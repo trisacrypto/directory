10c5f62eb3a2c9114f4c3d793e166873fb8452ea
Benjamin Bengfort <benjamin@bengfort.com>
2021-05-19 16:31:24 -0400

    TRIXO form implementation

--- web/src/components/Registration.js
+++ web/src/components/Registration.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import TRIXO from './TRIXO';
 import Contact from './Contact';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -16,11 +17,33 @@ class Registration extends React.Component {
     tabKey: "introduction",
     validated: false,
     formData: {
+      entity: {},
       contacts: {
         technical: {name: "", email: "", phone: ""},
         legal: {name: "", email: "", phone: ""},
         administrative: {name: "", email: "", phone: ""},
         billing: {name: "", email: "", phone: ""},
+      },
+      trisa_endpoint: "",
+      common_name: "",
+      website: "",
+      business_category: "",
+      established_on: "",
+      trixo: {
+        primary_national_jurisdiction: "",
+        primary_regulator: "",
+        other_jurisdictions: [],
+        financial_transfers_permitted: "",
+        has_required_regulatory_program: "",
+        conducts_customer_kyc: false,
+        kyc_threshold: 0.0,
+        kyc_threshold_currency: "USD",
+        must_comply_travel_rule: false,
+        applicable_regulations: ["FATF Recommendation 16"],
+        compliance_threshold: 0.0,
+        compliance_threshold_currency: "",
+        must_safeguard_pii: false,
+        safegaurds_pii: false,
       }
     },
   }
@@ -57,7 +80,9 @@ class Registration extends React.Component {
                 <Nav.Item>
                   <Nav.Link eventKey="introduction">Introduction</Nav.Link>
                   <Nav.Link eventKey="entity-details">Entity Details</Nav.Link>
+                  <Nav.Link eventKey="trisa-implementation">TRISA Implementation</Nav.Link>
                   <Nav.Link eventKey="contacts">Contacts</Nav.Link>
+                  <Nav.Link eventKey="trixo">TRIXO Questionnaire</Nav.Link>
                   <Nav.Link eventKey="summary">Summary</Nav.Link>
                 </Nav.Item>
               </Nav>
@@ -86,14 +111,30 @@ class Registration extends React.Component {
                       registration interface. The registration form will be completed when the TRISA
                       v1beta1 protocol is released.
                     </p>
-                    <Button disabled variant="secondary">Reset</Button>{' '}
-                    <Button variant="primary" onClick={(e) => this.setState({tabKey:"entity-details"})}>Next</Button>
+                    <Form.Group>
+                      <Button type="reset" disabled variant="secondary">Reset</Button>{' '}
+                      <Button variant="primary" onClick={(e) => this.setState({tabKey:"entity-details"})}>Next</Button>
+                    </Form.Group>
                   </fieldset>
                 </Tab.Pane>
                 <Tab.Pane eventKey="entity-details">
                   <fieldset>
                     <legend>Entity Details</legend>
                     <p></p>
+                    <Form.Group>
+                      <Button variant="secondary" onClick={(e) => this.setState({tabKey:"introduction"})}>Back</Button>{' '}
+                      <Button variant="primary" onClick={(e) => this.setState({tabKey:"trisa-implementation"})}>Next</Button>
+                    </Form.Group>
+                  </fieldset>
+                </Tab.Pane>
+                <Tab.Pane eventKey="trisa-implementation">
+                  <fieldset>
+                    <legend>TRISA Implementation</legend>
+                    <p></p>
+                    <Form.Group>
+                      <Button variant="secondary" onClick={(e) => this.setState({tabKey:"entity-details"})}>Back</Button>{' '}
+                      <Button variant="primary" onClick={(e) => this.setState({tabKey:"contacts"})}>Next</Button>
+                    </Form.Group>
                   </fieldset>
                 </Tab.Pane>
                 <Tab.Pane eventKey="contacts">
@@ -105,7 +146,7 @@ class Registration extends React.Component {
                       must be verified before the registration can proceed.
                     </p>
 
-                    <Accordion defaultActiveKey="technical">
+                    <Accordion defaultActiveKey="technical" className="pb-3">
                       <Card>
                         <Accordion.Toggle as={Card.Header} eventKey="technical">
                           Technical Contact
@@ -130,6 +171,10 @@ class Registration extends React.Component {
                         </Accordion.Toggle>
                         <Accordion.Collapse eventKey="legal">
                           <Card.Body>
+                            <p>
+                              Compliance officer or legal contact for requests about the compliance
+                              requirements and legal status of your organization. (Strongly recommended).
+                            </p>
                             <Contact
                               contact={this.state.formData.contacts.legal}
                               onChange={this.createChangeHandler("legal", "contacts")}
@@ -143,6 +188,10 @@ class Registration extends React.Component {
                         </Accordion.Toggle>
                         <Accordion.Collapse eventKey="administrative">
                           <Card.Body>
+                            <p>
+                              Administrative or executive contact for your organization to field
+                              high-level requests or queries. (Optional).
+                            </p>
                             <Contact
                               contact={this.state.formData.contacts.administrative}
                               onChange={this.createChangeHandler("administrative", "contacts")}
@@ -156,6 +205,10 @@ class Registration extends React.Component {
                         </Accordion.Toggle>
                         <Accordion.Collapse eventKey="billing">
                           <Card.Body>
+                            <p>
+                              Billing contact for your organization to handle account and invoice
+                              requests or queries relating to the operation of the TRISA network. (Optional).
+                            </p>
                             <Contact
                               contact={this.state.formData.contacts.billing}
                               onChange={this.createChangeHandler("billing", "contacts")}
@@ -165,12 +218,38 @@ class Registration extends React.Component {
                       </Card>
                     </Accordion>
 
+                    <Form.Group>
+                      <Button variant="secondary" onClick={(e) => this.setState({tabKey:"trisa-implementation"})}>Back</Button>{' '}
+                      <Button variant="primary" onClick={(e) => this.setState({tabKey:"trixo"})}>Next</Button>
+                    </Form.Group>
+                  </fieldset>
+                </Tab.Pane>
+                <Tab.Pane eventKey="trixo">
+                  <fieldset>
+                    <legend>TRIXO Questionnaire</legend>
+                    <p>
+                      This questionnaire is designed to help the TRISA working group and other
+                      TRISA members understand the regulatory regime of your organization to ensure
+                      that required compliance information exchanges are conducted correctly and
+                      safely. All verified TRISA members will have access to this information.
+                    </p>
+                    <TRIXO
+                      data={this.state.formData.trixo}
+                      onChange={this.createChangeHandler("trixo")}
+                    />
+                    <Form.Group>
+                      <Button variant="secondary" onClick={(e) => this.setState({tabKey:"contacts"})}>Back</Button>{' '}
+                      <Button variant="primary" onClick={(e) => this.setState({tabKey:"summary"})}>Next</Button>
+                    </Form.Group>
                   </fieldset>
                 </Tab.Pane>
                 <Tab.Pane eventKey="summary">
                   <fieldset>
                     <legend>Summary</legend>
-                    <Button type="submit">Submit</Button>
+                    <Form.Group>
+                      <Button type="reset" disabled variant="secondary">Reset</Button>{' '}
+                      <Button type="submit">Download</Button>
+                    </Form.Group>
                   </fieldset>
                 </Tab.Pane>
               </Tab.Content>
