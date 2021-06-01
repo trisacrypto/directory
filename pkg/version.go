fe7fb11d1025502ae69ed0c4f1b1e97e1e264cf2
Benjamin Bengfort <benjamin@bengfort.com>
2021-06-01 09:27:12 -0500

    version bump

--- pkg/version.go
+++ pkg/version.go
@@ -7,15 +7,27 @@ import "fmt"
 
 // Version component constants for the current build.
 const (
-	VersionMajor = 0
-	VersionMinor = 2
-	VersionPatch = 1
+	VersionMajor = 1
+	VersionMinor = 0
+	VersionPatch = 0
+    VersionReleaseLevel = "rc"
+    VersionReleaseNumber = 1
 )
 
 // Version returns the semantic version for the current build.
 func Version() string {
+    var versionCore string
 	if VersionPatch > 0 {
-		return fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
-	}
-	return fmt.Sprintf("%d.%d", VersionMajor, VersionMinor)
+		versionCore = fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
+	} else {
+    	versionCore = fmt.Sprintf("%d.%d", VersionMajor, VersionMinor)
+    }
+
+    if VersionReleaseLevel != "" {
+        if VersionReleaseNumber > 0 {
+            return fmt.Sprintf("%s-%s.%d", versionCore, VersionReleaseLevel, VersionReleaseNumber)
+        }
+        return fmt.Sprintf("%s-%s", versionCore, VersionReleaseLevel)
+    }
+    return versionCore
 }
