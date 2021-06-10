d97c173e1af37066334c7025ce4ba5a57c26407f
Benjamin Bengfort <benjamin@bengfort.com>
2021-06-09 23:25:44 -0500

    Implements Resend Admin RPC
    There have been a couple issues with email deliveries over the first few
    registrations, requiring that emails be resent. This RPC allows an admin
    to trigger a resend of different email types, based on the condition of
    the VASP record. This commit also tracks the number of emails that are
    sent for logging purposes.

--- pkg/gds/emails/client_test.go
+++ pkg/gds/emails/client_test.go
@@ -56,8 +56,19 @@ func TestClientSendEmails(t *testing.T) {
 	err = models.SetContactVerification(vasp.Contacts.Technical, "", true)
 	require.NoError(t, err)
 
-	require.NoError(t, email.SendVerifyContacts(vasp))
-	require.NoError(t, email.SendReviewRequest(vasp))
-	require.NoError(t, email.SendRejectRegistration(vasp, "this is a test rejection from the test runner"))
-	require.NoError(t, email.SendDeliverCertificates(vasp, "testdata/foo.zip"))
+	sent, err := email.SendVerifyContacts(vasp)
+	require.NoError(t, err)
+	require.Equal(t, 1, sent)
+
+	sent, err = email.SendReviewRequest(vasp)
+	require.NoError(t, err)
+	require.Equal(t, 1, sent)
+
+	sent, err = email.SendRejectRegistration(vasp, "this is a test rejection from the test runner")
+	require.NoError(t, err)
+	require.Equal(t, 1, sent)
+
+	sent, err = email.SendDeliverCertificates(vasp, "testdata/foo.zip")
+	require.NoError(t, err)
+	require.Equal(t, 1, sent)
 }
