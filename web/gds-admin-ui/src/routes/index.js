1cf1040d10fbdddfab82906cef65a29a91c38e9d
elysee15 <elyseebleu1@gmail.com>
2021-08-31 23:58:08 +0000

    feat: add dashboard page

--- web/gds-admin-ui/src/routes/index.js
+++ web/gds-admin-ui/src/routes/index.js
@@ -41,12 +41,15 @@ const dashboardRoutes = {
             component: Dashboard,
             route: Route,
         },
+<<<<<<< HEAD
         {
             path: '/vasps-summary/vasps',
             name: 'List',
             component: VaspsList,
             route: Route,
         }
+=======
+>>>>>>> feat: add dashboard page
     ],
 };
 
