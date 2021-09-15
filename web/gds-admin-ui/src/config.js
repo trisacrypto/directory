d3bb3201496581fe311dddff6af9f93ffee9ee90
elysee15 <54010836+elysee15@users.noreply.github.com>
2021-09-15 16:11:45 +0000

    Sign-In with Google Button (#92)
    Implements Sign-In with Google login button in the GDS Admin UI but does not wire it up to the rest of the app.

--- web/gds-admin-ui/src/config.js
+++ web/gds-admin-ui/src/config.js
@@ -1,5 +1,6 @@
 const config = {
     API_URL: process.env.REACT_APP_API_URL,
+    GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID
 };
 
 export default config;
--- web/gds-admin-ui/src/redux/reducers.js
+++ web/gds-admin-ui/src/redux/reducers.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux';
 
 import Layout from './layout/reducers';
 import { vaspsReducers as Vasps, certificatesReducers as Certificates, summaryReducers as Summary } from "./dashboard/reducers"
+import Auth from './auth/reducers'
 
 export default (combineReducers({
     Layout,
     Vasps,
     Certificates,
-    Summary
+    Summary,
+    Auth
 }): any);
