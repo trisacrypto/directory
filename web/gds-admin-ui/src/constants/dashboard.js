a7155fffa378d4773aac97118784c839d9360eb4
elysee15 <54010836+elysee15@users.noreply.github.com>
2021-09-20 19:02:52 +0000

    GDS Admin UI: add reviews timeline graph (#99)

--- web/gds-admin-ui/src/constants/dashboard.js
+++ web/gds-admin-ui/src/constants/dashboard.js
@@ -1,6 +1,23 @@
-
 export const Status = {
-    PENDING_REVIEW: "PENDING REVIEW",
+    SUBMITTED: "SUBMITTED",
+    EMAIL_VERIFIED: "EMAIL_VERIFIED",
+    PENDING_REVIEW: "PENDING_REVIEW",
+    REVIEWED: "REVIEWED",
+    ISSUING_CERTIFICATE: "ISSUING_CERTIFICATE",
     VERIFIED: "VERIFIED",
-    SUBMITTED: "SUBMITTED"
+    REJECTED: "REJECTED",
+    APPEALED: "APPEALED",
+    ERRORED: "ERRORED"
+}
+
+export const StatusLabel = {
+    SUBMITTED: "Submitted",
+    EMAIL_VERIFIED: "Email Verified",
+    PENDING_REVIEW: "Pending Review",
+    REVIEWED: "Reviewed",
+    ISSUING_CERTIFICATE: "Issuing Certificate",
+    VERIFIED: "Verified",
+    REJECTED: "Rejected",
+    APPEALED: "Appealed",
+    ERRORED: "Errored"
 }
---
+++ web/gds-admin-ui/src/pages/app/dashboard/TasksChart.js
@@ -0,0 +1,181 @@
+// @flow
+import React from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Card } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { Status, StatusLabel } from '../../../constants/dashboard';
+
+
+const barChartOpts = {
+    maintainAspectRatio: false,
+    legend: {
+        display: true,
+    },
+    tooltips: {
+        intersect: false,
+    },
+    hover: {
+        intersect: true,
+    },
+    plugins: {
+        filler: {
+            propagate: false,
+        },
+    },
+    scales: {
+        xAxes: [
+            {
+                reverse: true,
+                gridLines: {
+                    color: 'rgba(0,0,0,0.05)',
+                },
+                stacked: true
+            },
+        ],
+        yAxes: [
+            {
+                ticks: {
+                    stepSize: 10,
+                    display: false,
+                },
+                min: 10,
+                max: 100,
+                display: true,
+                borderDash: [5, 5],
+                gridLines: {
+                    color: 'rgba(0,0,0,0)',
+                    fontColor: '#fff',
+                },
+                stacked: true
+            },
+        ],
+    },
+};
+
+
+const TasksChart = (): React$Element<any> => {
+    const { reviews, isLoading } = useSelector(state => ({
+        reviews: state.Reviews.data,
+        isLoading: state.Reviews.loading
+    }))
+
+    const getWeeks = () => {
+        if (reviews && Array.isArray(reviews)) {
+            return reviews.map(review => review.week)
+        }
+
+        return []
+    }
+
+    const getData = (key = '') => {
+        if (reviews && Array.isArray(reviews)) {
+            return reviews.map(review => {
+                return review.registrations[key];
+            })
+        }
+
+        return []
+    }
+
+    const barChartData = {
+        labels: [
+            ...getWeeks()
+        ],
+        datasets: [
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.SUBMITTED,
+                backgroundColor: '#727cf5',
+                borderColor: '#727cf5',
+                data: getData(Status.SUBMITTED),
+            },
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.EMAIL_VERIFIED,
+                backgroundColor: '#1abc9c',
+                borderColor: '#1abc9c',
+                data: getData(Status.EMAIL_VERIFIED),
+            },
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.PENDING_REVIEW,
+                backgroundColor: '#3498db',
+                borderColor: '#3498db',
+                data: getData(Status.PENDING_REVIEW),
+            },
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.REVIEWED,
+                backgroundColor: '#9b59b6',
+                borderColor: '#9b59b6',
+                data: getData(Status.REVIEWED),
+            },
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.ISSUING_CERTIFICATE,
+                backgroundColor: '#f1c40f',
+                borderColor: '#f1c40f',
+                data: getData(Status.ISSUING_CERTIFICATE),
+            },
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.VERIFIED,
+                backgroundColor: '#e74c3c',
+                borderColor: '#e74c3c',
+                data: getData(Status.VERIFIED),
+            },
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.REJECTED,
+                backgroundColor: '#3B3B98',
+                borderColor: '#3B3B98',
+                data: getData(Status.REJECTED),
+            },
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.APPEALED,
+                backgroundColor: '#182C61',
+                borderColor: '#182C61',
+                data: getData(Status.APPEALED),
+            },
+            {
+                barPercentage: 0.7,
+                categoryPercentage: 0.7,
+                label: StatusLabel.ERRORED,
+                backgroundColor: '#9AECDB',
+                borderColor: '#9AECDB',
+                data: getData(Status.ERRORED),
+            }
+        ],
+    };
+
+
+    return (
+        <Card>
+            <Card.Body>
+                <h4 className="header-title mb-4">REVIEWS TIMELINE</h4>
+
+                <div dir="ltr">
+                    <div style={{ height: '320px' }} className="mt-3 chartjs-chart">
+                        {
+                            !isLoading && <Bar data={barChartData} options={barChartOpts} />
+                        }
+                    </div>
+                </div>
+                <small>
+                    ⋆ click on the elements of the legend to filter accordingly
+                </small>
+            </Card.Body>
+        </Card>
+    );
+};
+
+export default TasksChart;
