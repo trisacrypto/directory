80f894f422eaca7fc6d085eb0346e2c25aac5679
elysee15 <elyseebleu1@gmail.com>
2021-10-18 22:29:11 +0000

    fix: Show 404 page on VASP not found.

--- web/gds-admin-ui/src/constants/menu.js
+++ web/gds-admin-ui/src/constants/menu.js
@@ -1,6 +1,6 @@
 const MENU_ITEMS = [
     { key: 'registration', label: 'Review Registration', isTitle: true },
-    { key: 'dashboard', icon: 'uil-home-alt', label: 'Dashboard', url: '/dashboard' },
+    { key: 'dashboard', icon: 'uil-home-alt', label: 'Dashboard', url: '/' },
     { key: 'vasps', label: 'VASPs Summary', isTitle: true },
     { key: 'vs-list', icon: 'uil-list-ul', label: 'List', url: '/vasps' },
 ];
