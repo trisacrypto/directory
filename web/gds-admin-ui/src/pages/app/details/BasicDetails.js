956128adca040baf244b97104e42b2a76e425578
elysee15 <elyseebleu1@gmail.com>
2021-10-20 16:54:27 +0000

    fix: Update VASP Detail Layout

--- web/gds-admin-ui/src/pages/app/details/BasicDetails.js
+++ web/gds-admin-ui/src/pages/app/details/BasicDetails.js
@@ -1,10 +1,16 @@
 
 import React from 'react'
 import { Card, Col, Dropdown, Row } from 'react-bootstrap';
-import { formatDisplayedData } from '../../../utils';
+import { Status, StatusLabel } from '../../../constants';
+import { formatDisplayedData, isValidHttpUrl } from '../../../utils';
+import dayjs from 'dayjs';
 
 
 function BasicDetails({ data }) {
+    console.log('data', data)
+
+    const formatDate = (date) => date ? dayjs(date).format('DD-MM-YYYY') : 'N/A';
+    const isNotPendingReview = () => data?.vasp?.verification_status !== Status.PENDING_REVIEW
 
     return (
         <Card className="d-block">
@@ -17,41 +23,58 @@ function BasicDetails({ data }) {
                         <i className="dripicons-dots-3"></i>
                     </Dropdown.Toggle>
                     <Dropdown.Menu>
-                        <Dropdown.Item>
-                            <i className="mdi mdi-pencil me-1"></i>Edit Details
+                        <Dropdown.Item disabled={isNotPendingReview()}>
+                            <i className="mdi mdi-card-search me-1"></i>Review
                         </Dropdown.Item>
                         <Dropdown.Item>
-                            <i className="mdi mdi-pencil me-1"></i>Edit TRIXO Implementation
+                            <i className="mdi mdi-square-edit-outline me-1"></i>Edit
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            <i className="mdi mdi-printer me-1"></i>Print
+                        </Dropdown.Item>
+                        <Dropdown.Item>
+                            <i className="mdi mdi-email me-1"></i>Resend
                         </Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
 
-                <h4 className="mt-0 mb-3">Basic Details</h4>
+                <div className='mb-3'>
+                    <div className='d-flex align-items-lg-start gap-2'>
+                        <h4 className="mt-0">{data?.name}</h4>
+                        {data?.traveler ? <span className='badge bg-primary rounded-pill px-1'>traveler</span> : null}
+                        {data?.vasp?.verification_status ? <span className='badge bg-warning rounded-pill px-1'>{StatusLabel[data?.vasp?.verification_status]}</span> : null}
+                    </div>
+                    {isValidHttpUrl(data?.vasp?.website) ? <a target="_blank" href={`${data?.vasp?.website}`} rel="noreferrer">{data?.vasp?.website}</a> : null}
+                </div>
                 <Row>
-                    <Col xl={6}>
-                        <p className="fw-bold mb-2">ID: <span className="fw-normal">{formatDisplayedData(data?.vasp?.id)}</span></p>
-                        <p className="fw-bold mb-2">Name: <span className="fw-normal">{formatDisplayedData(data?.name)}</span></p>
-                        <p className="fw-bold mb-2">Common Name: <span className="fw-normal">{formatDisplayedData(data?.vasp?.common_name)}</span></p>
-                        <p className="fw-bold mb-2">Verification Status: <span className="fw-normal">{formatDisplayedData(data?.vasp?.verification_status)}</span></p>
+                    <Col>
+                        <div>
+                            <h5>Business details <button className='mdi mdi-arrow-down-bold-circle-outline border-0 bg-transparent' title="Download as JSON"></button></h5>
+                            <hr className='my-1' />
+                            <p className="mb-2">Business categorie(s): <span className="fw-normal">{formatDisplayedData(data?.vasp?.business_category)}</span></p>
+                            <p className="mb-2">Established on: <span className="fw-normal">{formatDisplayedData(data?.vasp?.established_on)}</span></p>
+                            <p className="mb-2">VASP {data?.vasp?.vasp_categories.length > 1 ? 'categories' : 'category'}: <span className="fw-normal">{formatDisplayedData(data?.vasp?.vasp_categories)}</span></p>
+                        </div>
+                        <div className='mt-4'>
+                            <h5>TRISA Details <button className='mdi mdi-arrow-down-bold-circle-outline border-0 bg-transparent' title="Download as JSON"></button></h5>
+                            <hr className='my-1' />
+                            <p className="mb-2">ID: <span className="fw-normal">{formatDisplayedData(data?.vasp?.id)}</span></p>
+                            <p className="mb-2">Common name: <span className="fw-normal">{formatDisplayedData(data?.vasp?.common_name)}</span></p>
+                            <p className="mb-2">TRISA Endpoint: <span className="fw-normal">{formatDisplayedData(data?.vasp?.trisa_endpoint)}</span></p>
+                            <p className="mb-2">Registered Directory: <span className="fw-normal">{formatDisplayedData(data?.vasp?.registered_directory)}</span></p>
+                        </div>
                     </Col>
                     <Col xl={6}>
-                        <p className="fw-bold mb-2">TRISA Endpoint: <span className="fw-normal">{formatDisplayedData(data?.vasp?.trisa_endpoint)}</span></p>
-                        <p className="fw-bold mb-2">Website: <span className="fw-normal">{formatDisplayedData(data?.vasp?.website)}</span></p>
-                        <p className="fw-bold mb-2">Established On: <span className="fw-normal">{formatDisplayedData(data?.vasp?.established_on)}</span></p>
-                        <p className="fw-bold mb-2">Verified On: <span className="fw-normal">{formatDisplayedData(data?.vasp?.verified_on)}</span></p>
+
                     </Col>
-                    <Col xl={6}>
-                        <p className="fw-bold mb-2">Business categories: <span className="fw-normal">{formatDisplayedData(data?.vasp?.business_category)}</span></p>
-                        <p className="fw-bold mb-2">First Listed: <span className="fw-normal">{formatDisplayedData(data?.vasp?.first_listed)}</span></p>
-                        <p className="fw-bold mb-2">Last Update: <span className="fw-normal">{formatDisplayedData(data?.vasp?.last_updated)}</span></p>
-                        <p className="fw-bold mb-2">Registered Directory: <span className="fw-normal">{formatDisplayedData(data?.vasp?.registered_directory)}</span></p>
-                        <p className="fw-bold mb-2">VASP Category: <span className="fw-normal">{formatDisplayedData(data?.vasp?.vasp_categories)}</span></p>
+                    <Col sm={12} className='d-flex justify-content-around flex-sm-wrap flex-md-nowrap text-center'>
+                        <p className="fw-bold mb-2"> <span className='d-block'>First Listed</span> <span className="fw-normal">{formatDate(data?.vasp?.first_listed)}</span></p>
+                        <p className="fw-bold mb-2"> <span className='d-block'>Last Updated</span> <span className="fw-normal">{formatDate(data?.vasp?.last_updated)}</span></p>
+                        <p className="fw-bold mb-2"> <span className='d-block'>Verified On</span> <span className="fw-normal">{formatDate(data?.vasp?.verified_on)}</span></p>
                     </Col>
-                    <Col xl={6}>
-                        <p className="fw-bold mb-2 mt-3">Version</p>
+                    <Col sm={12}>
                         <hr />
-                        <p className="fw-bold mb-2">PID: <span className="fw-normal">{formatDisplayedData(data?.vasp?.version?.pid)}</span></p>
-                        <p className="fw-bold mb-2">Version: <span className="fw-normal">{formatDisplayedData(data?.vasp?.version?.version)}</span></p>
+                        <p className="mb-2 text-center text-muted">Version: {`${formatDisplayedData(data?.vasp?.version?.version)}.${formatDisplayedData(data?.vasp?.version?.pid)}`}</p>
                     </Col>
                 </Row>
 
