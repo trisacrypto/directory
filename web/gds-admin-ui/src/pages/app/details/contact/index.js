cefb7ff74b23fd878a4397511ea582cf82f6c561
elysee15 <elyseebleu1@gmail.com>
2021-10-29 11:17:51 +0000

    fix: update contact card

--- web/gds-admin-ui/src/pages/app/details/contact/index.js
+++ web/gds-admin-ui/src/pages/app/details/contact/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { Card } from 'react-bootstrap';
 import Contact from './Contact';
 
-export default function ContactList({ data }) {
+export default function ContactList({ data, verifiedContact }) {
 
     return (
         <Card>
             <Card.Body>
                 <h4 className="mt-0 mb-3">Contacts</h4>
-                {data?.technical ? <Contact data={data?.technical} type="Technical" /> : null}
-                {data?.administrative ? <Contact data={data?.administrative} type="Administrative" /> : null}
-                {data?.legal ? <Contact data={data?.legal} type="Legal" /> : null}
-                {data?.billing ? <Contact data={data?.billing} type="Billing" /> : null}
+                {data?.technical ? <Contact data={data?.technical} verifiedContact={verifiedContact} type="Technical" /> : null}
+                {data?.administrative ? <Contact data={data?.administrative} verifiedContact={verifiedContact} type="Administrative" /> : null}
+                {data?.legal ? <Contact data={data?.legal} verifiedContact={verifiedContact} type="Legal" /> : null}
+                {data?.billing ? <Contact data={data?.billing} verifiedContact={verifiedContact} type="Billing" /> : null}
             </Card.Body>
         </Card>
     );
