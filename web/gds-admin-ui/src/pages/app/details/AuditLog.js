9c7e48758e52f4eeb41f0aeeead4b7ddde65f77e
elysee15 <elyseebleu1@gmail.com>
2021-10-29 16:20:05 +0000

    feat: add audit log

--- web/gds-admin-ui/src/pages/app/details/AuditLog.js
+++ web/gds-admin-ui/src/pages/app/details/AuditLog.js
@@ -3,9 +3,13 @@ import SimpleBar from 'simplebar-react';
 import TimelineItem from "../../../components/TimelineItem";
 import Timeline from '../../../components/Timeline'
 import { StatusLabel } from "../../../constants";
+import dayjs from "dayjs";
+import React from "react";
 
 
 const AuditLog = ({ data }) => {
+    const dataSorted = React.useMemo(() => data?.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()), [data])
+
     return data ? (
         <Card>
             <Card.Body>
@@ -14,17 +18,17 @@ const AuditLog = ({ data }) => {
                 <SimpleBar style={{ maxHeight: '330px', width: '100%' }}>
                     <Timeline>
                         {
-                            data.map((log, idx) => (
+                            Array.isArray(dataSorted) && dataSorted.map((log, idx) => (
                                 <TimelineItem key={idx}>
                                     <i className="mdi mdi-file-search-outline bg-info-lighten text-info timeline-icon"></i>
-                                    <div className="timeline-item-info">
-                                        <h5 data-testid="audit-log-desc" href="/" style={{ color: '#2e74d6' }}>{log.description}</h5>
+                                    <div className="timeline-item-info mb-3">
+                                        <h5 className="text-primary mb-1" data-testid="audit-log-desc" href="/">{log.description}</h5>
                                         <small className="d-block" data-testid="audit-log-state">
                                             <span className="fst-italic" data-testid="audit-log-source">{log.source}</span> from{' '}
                                             <span className="fst-italic" data-testid="audit-log-previous-state">{StatusLabel[log.previous_state]}</span> -{' '}
                                             <span className="fst-italic" data-testid="audit-log-current-state">{StatusLabel[log.current_state]}</span>
                                         </small>
-                                        <small className="d-block" data-testid="audit-log-timestamp">{new Date(log.timestamp).toLocaleString()}</small>
+                                        <small className="d-block" data-testid="audit-log-timestamp">{dayjs(log?.timestamp).toLocaleString()}</small>
                                     </div>
                                 </TimelineItem>
                             ))
