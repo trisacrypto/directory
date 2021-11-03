01ebf0b0a582131f5cd88e565748952333888310
elysee15 <elyseebleu1@gmail.com>
2021-11-03 01:34:32 +0000

    feat: add review notes list

--- web/gds-admin-ui/src/pages/app/details/ReviewNotes/index.js
+++ web/gds-admin-ui/src/pages/app/details/ReviewNotes/index.js
@@ -5,7 +5,10 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 import { useParams } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { fetchReviewNotesApiResponse } from '../../../../redux/review-notes/actions';
+<<<<<<< HEAD
 import useSafeDispatch from '../../../../hooks/useSafeDispatch';
+=======
+>>>>>>> feat: add review notes list
 dayjs.extend(relativeTime)
 
 const ReviewNotes = React.lazy(_ => import('./ReviewNotes'))
@@ -15,6 +18,7 @@ const ReviewNoteForm = React.lazy(_ => import('./ReviewNoteForm'))
 const Comments = () => {
     const params = useParams()
     const dispatch = useDispatch()
+<<<<<<< HEAD
     const safeDispatch = useSafeDispatch(dispatch)
 
     React.useEffect(() => {
@@ -23,13 +27,32 @@ const Comments = () => {
         }
 
     }, [params.id, safeDispatch])
+=======
+    const isMounted = React.useRef(true);
+
+
+    React.useEffect(() => {
+        if (isMounted.current) {
+            if (params?.id) {
+                dispatch(fetchReviewNotesApiResponse(params.id))
+            }
+        }
+
+        return () => isMounted.current = false;
+    }, [dispatch, params.id])
+>>>>>>> feat: add review notes list
 
     return (
         <Card>
             <Card.Body>
                 <h4 className="mt-0 mb-3">Reviewer Notes</h4>
+<<<<<<< HEAD
                 <ReviewNoteForm vaspId={params?.id} />
                 <ReviewNotes vaspId={params?.id} />
+=======
+                <ReviewNoteForm />
+                <ReviewNotes />
+>>>>>>> feat: add review notes list
             </Card.Body>
         </Card>
     );
