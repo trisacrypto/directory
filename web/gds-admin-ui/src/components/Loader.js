a74ae628157e0cebbed151821017432afb276472
elysee15 <54010836+elysee15@users.noreply.github.com>
2021-11-12 10:55:19 +0000

    GDS-Admin-UI: registrations statuses donut chart doesn't show proper colors (#202)

--- web/gds-admin-ui/src/components/Loader.js
+++ web/gds-admin-ui/src/components/Loader.js
@@ -1,6 +1,6 @@
-import React from 'react';
+// @flow
 import { ReactComponent as LoaderIcon } from '../assets/images/loader.svg';
 
 export default function Loader(props) {
     return <LoaderIcon {...props} />
-}
+} 
