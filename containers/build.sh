74351c2c95fc7977e25329e7d7338dc8cbe9a754
Benjamin Bengfort <benjamin@bengfort.com>
2021-11-15 22:21:57 -0600

    GDS Containers Update
    This PR adds the TRTL Dockerfile and updates the GDS Dockerfile to be a
    bit lighter-weight and share cache with the TRTL Dockerfile in order to
    reduce build times. The GDS container has decreased from 175MB to 132MB
    as a result of these changes.

--- containers/build.sh
+++ containers/build.sh
@@ -76,6 +76,7 @@ REPO=$(realpath "$DIR/..")
 
 # Build the primary backend images
 docker build -t trisa/gds:$TAG -f $DIR/gds/Dockerfile $REPO
+docker build -t trisa/trtl:$TAG -f $DIR/trtl/Dockerfile $REPO
 docker build -t trisa/grpc-proxy:$TAG -f $DIR/grpc-proxy/Dockerfile $REPO
 
 # Build the UI images for trisatest.net and vaspdirectory.net
@@ -110,14 +111,16 @@ docker build \
 
 # Retag the images to push to gcr.io
 docker tag trisa/gds:$TAG gcr.io/trisa-gds/gds:$TAG
-docker tag trisa/gds-ui:$TAG gcr.io/trisa-gds/gds-ui:$TAG
+docker tag trisa/trtl:$TAG gcr.io/trisa-gds/trtl:$TAG
 docker tag trisa/grpc-proxy:$TAG gcr.io/trisa-gds/grpc-proxy:$TAG
+docker tag trisa/gds-ui:$TAG gcr.io/trisa-gds/gds-ui:$TAG
 docker tag trisa/gds-testnet-ui:$TAG gcr.io/trisa-gds/gds-testnet-ui:$TAG
 docker tag trisa/gds-admin-ui:$TAG gcr.io/trisa-gds/gds-admin-ui:$TAG
 docker tag trisa/gds-testnet-admin-ui:$TAG gcr.io/trisa-gds/gds-testnet-admin-ui:$TAG
 
 # Push to DockerHub
 docker push trisa/gds:$TAG
+docker push trisa/trtl:$TAG
 docker push trisa/grpc-proxy:$TAG
 docker push trisa/gds-ui:$TAG
 docker push trisa/gds-testnet-ui:$TAG
@@ -126,6 +129,7 @@ docker push trisa/gds-testnet-admin-ui:$TAG
 
 # Push to GCR
 docker push gcr.io/trisa-gds/gds:$TAG
+docker push gcr.io/trisa-gds/trtl:$TAG
 docker push gcr.io/trisa-gds/grpc-proxy:$TAG
 docker push gcr.io/trisa-gds/gds-ui:$TAG
 docker push gcr.io/trisa-gds/gds-testnet-ui:$TAG
