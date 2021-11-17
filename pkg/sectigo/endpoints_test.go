3b2d3de1bc2f0b111d34d63cc87d3819e2fd81b7
Patrick Deziel <42919891+pdeziel@users.noreply.github.com>
2021-11-17 12:36:16 -0600

    Sectigo: Mock Server for Unit and Integration Tests (#205)
    Co-authored-by: Benjamin Bengfort <benjamin@bengfort.com>

--- pkg/sectigo/endpoints_test.go
+++ pkg/sectigo/endpoints_test.go
@@ -1,21 +1,58 @@
-package sectigo
+package sectigo_test
 
 import (
 	"testing"
 
 	"github.com/stretchr/testify/require"
+	. "github.com/trisacrypto/directory/pkg/sectigo"
+	"github.com/trisacrypto/directory/pkg/sectigo/mock"
 )
 
-func TestUrlFor(t *testing.T) {
+func TestEndpoint(t *testing.T) {
 	// No params
-	require.Equal(t, "https://iot.sectigo.com/api/v1/certificates/find", urlFor(findCertificateEP))
+	ep, err := Endpoint(FindCertificateEP)
+	require.NoError(t, err)
+	require.Equal(t, "https://iot.sectigo.com/api/v1/certificates/find", ep.String())
 
 	// Test params
-	require.Equal(t, "https://iot.sectigo.com/api/v1/organizations/42/authority/24", urlFor(authorityDetailEP, 42, 24))
+	ep, err = Endpoint(AuthorityDetailEP, 42, 24)
+	require.NoError(t, err)
+	require.Equal(t, "https://iot.sectigo.com/api/v1/organizations/42/authority/24", ep.String())
 
 	// Test copy
-	require.Equal(t, "https://iot.sectigo.com/api/v1/organizations/95/authority/14", urlFor(authorityDetailEP, 95, 14))
+	ep, err = Endpoint(AuthorityDetailEP, 95, 14)
+	require.NoError(t, err)
+	require.Equal(t, "https://iot.sectigo.com/api/v1/organizations/95/authority/14", ep.String())
 
-	// Test panic with unknown endpoint
-	require.Panics(t, func() { urlFor("foo") })
+	// Test panic with unknown endpoint'
+	ep, err = Endpoint("foo")
+	require.EqualError(t, err, `no endpoint named "foo"`)
+	require.Nil(t, ep)
+}
+
+func TestModifyBaseURL(t *testing.T) {
+	defer ResetBaseURL() // Ensure Base URL is always reset
+
+	// Check original url
+	ep, err := Endpoint(FindCertificateEP)
+	require.NoError(t, err)
+	require.Equal(t, "https://iot.sectigo.com/api/v1/certificates/find", ep.String())
+
+	// Create a mock Server
+	m, err := mock.New()
+	require.NoError(t, err)
+	defer m.Close()
+
+	// Check that mock Server updated the sectigo URL
+	ep, err = Endpoint(FindCertificateEP)
+	require.NoError(t, err)
+	require.Equal(t, "http", ep.Scheme)
+	require.Contains(t, ep.Host, "127.0.0.1")
+	require.Equal(t, "/api/v1/certificates/find", ep.Path)
+
+	// Reset Base URL
+	ResetBaseURL()
+	ep, err = Endpoint(FindCertificateEP)
+	require.NoError(t, err)
+	require.Equal(t, "https://iot.sectigo.com/api/v1/certificates/find", ep.String())
 }
