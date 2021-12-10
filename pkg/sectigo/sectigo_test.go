1bdb9a72ef88d23fb23860c4276ff93092e9499e
Patrick Deziel <deziel.patrick@gmail.com>
2021-12-10 14:12:12 -0600

    remove duplicate import

--- pkg/sectigo/sectigo_test.go
+++ pkg/sectigo/sectigo_test.go
@@ -12,7 +12,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 	"github.com/trisacrypto/directory/pkg/sectigo"
-	. "github.com/trisacrypto/directory/pkg/sectigo"
 	"github.com/trisacrypto/directory/pkg/sectigo/mock"
 )
 
@@ -22,7 +21,7 @@ func TestSectigo(t *testing.T) {
 
 type SectigoTestSuite struct {
 	suite.Suite
-	api *Sectigo
+	api *sectigo.Sectigo
 }
 
 func (s *SectigoTestSuite) BeforeTest(suiteName, testName string) {
@@ -35,7 +34,7 @@ func (s *SectigoTestSuite) BeforeTest(suiteName, testName string) {
 		Testing:  true,
 	}
 	require.NoError(mock.Start())
-	s.api, err = New(conf)
+	s.api, err = sectigo.New(conf)
 	require.NoError(err)
 }
 
@@ -71,22 +70,22 @@ func (s *SectigoTestSuite) TestSuccessfulCalls() {
 		name string
 		f    func(t *testing.T)
 	}{
-		{name: AuthenticateEP, f: s.authenticate},
-		{name: RefreshEP, f: s.refresh}, // must be called after authenticate
-		{name: CreateSingleCertBatchEP, f: s.createSingleCertBatch},
-		{name: UploadCSREP, f: s.uploadCSRBatch},
-		{name: BatchDetailEP, f: s.batchDetail},
-		{name: BatchProcessingInfoEP, f: s.processingInfo},
-		{name: DownloadEP, f: s.download},
-		{name: DevicesEP, f: s.licensesUsed},
-		{name: UserAuthoritiesEP, f: s.userAuthorities},
-		{name: AuthorityBalanceAvailableEP, f: s.authorityAvailableBalance},
-		{name: CurrentUserOrganizationEP, f: s.organization},
-		{name: ProfilesEP, f: s.profiles},
-		{name: ProfileParametersEP, f: s.profileParams},
-		{name: ProfileDetailEP, f: s.profileDetail},
-		{name: FindCertificateEP, f: s.findCertificate},
-		{name: RevokeCertificateEP, f: s.revokeCertificate},
+		{name: sectigo.AuthenticateEP, f: s.authenticate},
+		{name: sectigo.RefreshEP, f: s.refresh}, // must be called after authenticate
+		{name: sectigo.CreateSingleCertBatchEP, f: s.createSingleCertBatch},
+		{name: sectigo.UploadCSREP, f: s.uploadCSRBatch},
+		{name: sectigo.BatchDetailEP, f: s.batchDetail},
+		{name: sectigo.BatchProcessingInfoEP, f: s.processingInfo},
+		{name: sectigo.DownloadEP, f: s.download},
+		{name: sectigo.DevicesEP, f: s.licensesUsed},
+		{name: sectigo.UserAuthoritiesEP, f: s.userAuthorities},
+		{name: sectigo.AuthorityBalanceAvailableEP, f: s.authorityAvailableBalance},
+		{name: sectigo.CurrentUserOrganizationEP, f: s.organization},
+		{name: sectigo.ProfilesEP, f: s.profiles},
+		{name: sectigo.ProfileParametersEP, f: s.profileParams},
+		{name: sectigo.ProfileDetailEP, f: s.profileDetail},
+		{name: sectigo.FindCertificateEP, f: s.findCertificate},
+		{name: sectigo.RevokeCertificateEP, f: s.revokeCertificate},
 	}
 	for _, t := range tests {
 		s.T().Run(t.name, t.f)
@@ -204,9 +203,9 @@ func (s *SectigoTestSuite) revokeCertificate(t *testing.T) {
 func (s *SectigoTestSuite) TestAuthenticateInvalidCreds() {
 	require := s.Require()
 
-	mock.Handle(AuthenticateEP, func(c *gin.Context) {
+	mock.Handle(sectigo.AuthenticateEP, func(c *gin.Context) {
 		var (
-			in *AuthenticationRequest
+			in *sectigo.AuthenticationRequest
 		)
 		if err := c.BindJSON(&in); err != nil {
 			c.JSON(http.StatusBadRequest, err)
@@ -226,7 +225,7 @@ func (s *SectigoTestSuite) TestAuthenticateInvalidCreds() {
 		Testing:  true,
 	}
 	var err error
-	s.api, err = New(conf)
+	s.api, err = sectigo.New(conf)
 	require.NoError(err)
-	require.EqualError(s.api.Authenticate(), ErrInvalidCredentials.Error())
+	require.EqualError(s.api.Authenticate(), sectigo.ErrInvalidCredentials.Error())
 }
