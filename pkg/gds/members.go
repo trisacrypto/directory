80fbac3f2fab8d0f491aec576c000879d672130d
Benjamin Bengfort <benjamin@bengfort.com>
2021-12-13 17:36:49 -0600

    RPC implementation

--- pkg/gds/members.go
+++ pkg/gds/members.go
@@ -9,11 +9,17 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/trisacrypto/directory/pkg"
 	"github.com/trisacrypto/directory/pkg/gds/config"
-	pb "github.com/trisacrypto/directory/pkg/gds/members/v1alpha1"
+	api "github.com/trisacrypto/directory/pkg/gds/members/v1alpha1"
+	"github.com/trisacrypto/directory/pkg/gds/models/v1"
 	"github.com/trisacrypto/directory/pkg/gds/store"
+	pb "github.com/trisacrypto/trisa/pkg/trisa/gds/models/v1beta1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+)
+
+const (
+	defaultPageSize = 100
 )
 
 // NewMembers creates a new Member server derived from a parent Service.
@@ -26,7 +32,7 @@ func NewMembers(svc *Service) (members *Members, err error) {
 
 	// Initialize the gRPC server
 	members.srv = grpc.NewServer(grpc.UnaryInterceptor(svc.serverInterceptor))
-	pb.RegisterTRISAMembersServer(members.srv, members)
+	api.RegisterTRISAMembersServer(members.srv, members)
 	return members, nil
 }
 
@@ -38,7 +44,7 @@ func NewMembers(svc *Service) (members *Members, err error) {
 // NOTE: this is a prototype service, this service may eventually be moved into the GDS
 // specification in trisacrypto/trisa.
 type Members struct {
-	pb.UnimplementedTRISAMembersServer
+	api.UnimplementedTRISAMembersServer
 	svc  *Service              // The parent Service GDS uses to interact with other components
 	srv  *grpc.Server          // The gRPC server that listens on its own independent port
 	conf *config.MembersConfig // The GDS service specific configuration (helper alias to s.svc.conf.Members)
@@ -97,6 +103,109 @@ func (s *Members) Shutdown() (err error) {
 // page token. That token can be used to fetch the next page so long as the parameters
 // of the original request are not modified (e.g. any filters or pagination parameters).
 // See https://cloud.google.com/apis/design/design_patterns#list_pagination for more.
-func (s *Members) List(ctx context.Context, in *pb.ListRequest) (out *pb.ListReply, err error) {
-	return nil, status.Errorf(codes.Unimplemented, "not implemented quite yet")
+func (s *Members) List(ctx context.Context, in *api.ListRequest) (out *api.ListReply, err error) {
+	// Use default page size if one isn't specified
+	if in.PageSize == 0 {
+		in.PageSize = defaultPageSize
+	}
+
+	// If a page cursor is provided, load it - otherwise create a cursor for iteration
+	cursor := &models.PageCursor{}
+	if in.PageToken != "" {
+		if err = cursor.Load(in.PageToken); err != nil {
+			log.Warn().Err(err).Msg("invalid page token on list request")
+			return nil, status.Error(codes.InvalidArgument, "invalid page token")
+		}
+
+		// Validate the request has not changed
+		if cursor.PageSize != in.PageSize {
+			log.Debug().Int32("cursor", cursor.PageSize).Int32("opts", in.PageSize).Msg("invalid members list request: mismatched page size")
+			return nil, status.Error(codes.InvalidArgument, "page size cannot change between requests")
+		}
+
+	} else {
+		// Update the cursor with the input request
+		cursor.PageSize = in.PageSize
+	}
+
+	// Create response
+	out = &api.ListReply{
+		Vasps: make([]*api.VASPMember, 0, cursor.PageSize),
+	}
+
+	// Create the VASPs iterator to begin collecting validated VASPs data
+	iter := s.db.ListVASPs()
+	defer iter.Release()
+
+	// If necessary, seek to the next key specified by the cursor.
+	if cursor.NextVasp != "" {
+		// If iter.Seek returns false (e.g. seek did not find the specified key) then
+		// iter.Next() should also return false, so it isn't necessary to check the return.
+		// NOTE: next key must be deleted after it's used for seeking so that the last
+		// page doesn't retain the old key and loop forever.
+		iter.Seek(cursor.NextVasp)
+		cursor.NextVasp = ""
+
+		// Because we're going to be calling Next, we need to back up one key to ensure
+		// that we start on the right key in the for loop.
+		iter.Prev()
+	}
+
+	// Iterate over VASPs, collecting the one we're looking for.
+	for iter.Next() {
+		// Check if we're done iterating - if so and there is next data, there is
+		// another page, so create the page token to return it.
+		if len(out.Vasps) == int(cursor.PageSize) {
+			cursor.NextVasp = iter.Id()
+			break
+		}
+
+		// Collect the VASP from the iterator
+		vasp := iter.VASP()
+
+		// Skip any VASPs that are not verified yet
+		if vasp.VerificationStatus != pb.VerificationState_VERIFIED {
+			continue
+		}
+
+		// Build the directory information to return to the user
+		info := &api.VASPMember{
+			Id:                  vasp.Id,
+			RegisteredDirectory: vasp.RegisteredDirectory,
+			CommonName:          vasp.CommonName,
+			Endpoint:            vasp.TrisaEndpoint,
+			Website:             vasp.Website,
+			BusinessCategory:    vasp.BusinessCategory,
+			VaspCategories:      vasp.VaspCategories,
+			VerifiedOn:          vasp.VerifiedOn,
+		}
+
+		// Add other information to the VASP
+		if info.Name, err = vasp.Name(); err != nil {
+			log.Error().Err(err).Str("vasp_id", vasp.Id).Msg("could not retrieve VASP name from record")
+		}
+
+		if vasp.Entity != nil {
+			info.Country = vasp.Entity.CountryOfRegistration
+		}
+
+		out.Vasps = append(out.Vasps, info)
+	}
+
+	if err = iter.Error(); err != nil {
+		log.Error().Err(err).Msg("could not iterate over VASPs")
+		return nil, status.Error(codes.Internal, "could not iterate over directory service")
+	}
+
+	// Check if there is a next page cursor
+	if cursor.NextVasp != "" {
+		if out.NextPageToken, err = cursor.Dump(); err != nil {
+			log.Error().Err(err).Msg("could not serialize next page token on vasp member list")
+			return nil, status.Error(codes.Internal, "could not create next page token")
+		}
+	}
+
+	// Request Complete
+	log.Info().Int("count", len(out.Vasps)).Bool("has_next_page", out.NextPageToken != "").Msg("vasp member list complete")
+	return out, nil
 }
