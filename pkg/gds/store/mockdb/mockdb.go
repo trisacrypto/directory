a911f306044994281f7d7b0e1b60521b75545798
Patrick Deziel <42919891+pdeziel@users.noreply.github.com>
2021-12-17 08:49:08 -0600

    CI and Testing: CSV load test (#263)

--- pkg/gds/store/mockdb/mockdb.go
+++ pkg/gds/store/mockdb/mockdb.go
@@ -1,125 +1,159 @@
 package mockdb
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/trisacrypto/directory/pkg/gds/models/v1"
 	"github.com/trisacrypto/directory/pkg/gds/store"
 	"github.com/trisacrypto/directory/pkg/gds/store/iterator"
 	pb "github.com/trisacrypto/trisa/pkg/trisa/gds/models/v1beta1"
 )
 
-var _ store.Store = &MockDB{}
+var db store.Store = &MockDB{}
+var state = &MockState{
+	VASPs: make(map[string]pb.VASP),
+	Keys:  []string{},
+}
+
+// MockState contains the current state of the MockDB for test verification.
+type MockState struct {
+	// in-memory database store
+	VASPs map[string]pb.VASP
+	Keys  []string
+
+	// keep track of store interface calls
+	CloseInvoked           bool
+	CreateVASPInvoked      bool
+	RetrieveVASPInvoked    bool
+	UpdateVASPInvoked      bool
+	DeleteVASPInvoked      bool
+	ListVASPsInvoked       bool
+	SearchVASPsInvoked     bool
+	ListCertReqsInvoked    bool
+	CreateCertReqInvoked   bool
+	RetrieveCertReqInvoked bool
+	UpdateCertReqInvoked   bool
+	DeleteCertReqInvoked   bool
+	ReindexInvoked         bool
+	BackupInvoked          bool
+}
+
+func GetState() *MockState {
+	return state
+}
+
+func ResetState() {
+	state = &MockState{
+		VASPs: make(map[string]pb.VASP),
+		Keys:  []string{},
+	}
+}
 
 // MockDB fulfills the store interface for testing.
 type MockDB struct {
-	OnClose      func() error
-	CloseInvoked bool
-
+	OnClose           func() error
 	OnCreateVASP      func(v *pb.VASP) (string, error)
-	CreateVASPInvoked bool
-
-	OnRetrieveVASP      func(id string) (*pb.VASP, error)
-	RetrieveVASPInvoked bool
-
+	OnRetrieveVASP    func(id string) (*pb.VASP, error)
 	OnUpdateVASP      func(v *pb.VASP) error
-	UpdateVASPInvoked bool
-
 	OnDeleteVASP      func(id string) error
-	DeleteVASPInvoked bool
+	OnListVASPs       func() iterator.DirectoryIterator
+	OnSearchVASPs     func(query map[string]interface{}) ([]*pb.VASP, error)
+	OnListCertReqs    func() iterator.CertificateIterator
+	OnCreateCertReq   func(r *models.CertificateRequest) (string, error)
+	OnRetrieveCertReq func(id string) (*models.CertificateRequest, error)
+	OnUpdateCertReq   func(r *models.CertificateRequest) error
+	OnDeleteCertReq   func(id string) error
+	OnReindex         func() error
+	OnBackup          func(string) error
+}
 
-	OnListVASPs      func() iterator.DirectoryIterator
-	ListVASPsInvoked bool
-
-	OnSearchVASPs      func(query map[string]interface{}) ([]*pb.VASP, error)
-	SearchVASPsInvoked bool
-
-	OnListCertReqs      func() iterator.CertificateIterator
-	ListCertReqsInvoked bool
-
-	OnCreateCertReq      func(r *models.CertificateRequest) (string, error)
-	CreateCertReqInvoked bool
-
-	OnRetrieveCertReq      func(id string) (*models.CertificateRequest, error)
-	RetrieveCertReqInvoked bool
-
-	OnUpdateCertReq      func(r *models.CertificateRequest) error
-	UpdateCertReqInvoked bool
-
-	OnDeleteCertReq      func(id string) error
-	DeleteCertReqInvoked bool
-
-	OnReindex      func() error
-	ReindexInvoked bool
-
-	OnBackup      func(string) error
-	BackupInvoked bool
+func GetStore() store.Store {
+	return db
 }
 
 func (m *MockDB) Close() error {
-	m.CloseInvoked = true
+	state.CloseInvoked = true
 	return m.OnClose()
 }
 
 func (m *MockDB) CreateVASP(v *pb.VASP) (string, error) {
-	m.CreateVASPInvoked = true
-	return m.OnCreateVASP(v)
+	state.CreateVASPInvoked = true
+	if v.Id == "" {
+		return "", errors.New("VASP must contain an ID")
+	}
+	if _, ok := state.VASPs[v.Id]; ok {
+		return "", fmt.Errorf("VASP with ID %s already exists", v.Id)
+	}
+	state.VASPs[v.Id] = *v
+	state.Keys = append(state.Keys, v.Id)
+	return v.Id, nil
 }
 
 func (m *MockDB) RetrieveVASP(id string) (*pb.VASP, error) {
-	m.RetrieveVASPInvoked = true
-	return m.OnRetrieveVASP(id)
+	state.RetrieveVASPInvoked = true
+	if id == "" {
+		return nil, errors.New("missing VASP ID")
+	}
+	var v pb.VASP
+	var ok bool
+	if v, ok = state.VASPs[id]; !ok {
+		return nil, fmt.Errorf("VASP with ID %s not found", id)
+	}
+	return &v, nil
 }
 
 func (m *MockDB) UpdateVASP(v *pb.VASP) error {
-	m.UpdateVASPInvoked = true
+	state.UpdateVASPInvoked = true
 	return m.OnUpdateVASP(v)
 }
 
 func (m *MockDB) DeleteVASP(id string) error {
-	m.DeleteVASPInvoked = true
+	state.DeleteVASPInvoked = true
 	return m.OnDeleteVASP(id)
 }
 
 func (m *MockDB) ListVASPs() iterator.DirectoryIterator {
-	m.ListVASPsInvoked = true
+	state.ListVASPsInvoked = true
 	return m.OnListVASPs()
 }
 
 func (m *MockDB) SearchVASPs(query map[string]interface{}) ([]*pb.VASP, error) {
-	m.SearchVASPsInvoked = true
+	state.SearchVASPsInvoked = true
 	return m.OnSearchVASPs(query)
 }
 
 func (m *MockDB) ListCertReqs() iterator.CertificateIterator {
-	m.ListCertReqsInvoked = true
+	state.ListCertReqsInvoked = true
 	return m.OnListCertReqs()
 }
 
 func (m *MockDB) CreateCertReq(r *models.CertificateRequest) (string, error) {
-	m.CreateCertReqInvoked = true
+	state.CreateCertReqInvoked = true
 	return m.OnCreateCertReq(r)
 }
 
 func (m *MockDB) RetrieveCertReq(id string) (*models.CertificateRequest, error) {
-	m.RetrieveCertReqInvoked = true
+	state.RetrieveCertReqInvoked = true
 	return m.OnRetrieveCertReq(id)
 }
 
 func (m *MockDB) UpdateCertReq(r *models.CertificateRequest) error {
-	m.UpdateCertReqInvoked = true
+	state.UpdateCertReqInvoked = true
 	return m.OnUpdateCertReq(r)
 }
 
 func (m *MockDB) DeleteCertReq(id string) error {
-	m.DeleteCertReqInvoked = true
+	state.DeleteCertReqInvoked = true
 	return m.OnDeleteCertReq(id)
 }
 
 func (m *MockDB) Reindex() error {
-	m.ReindexInvoked = true
+	state.ReindexInvoked = true
 	return m.OnReindex()
 }
 
 func (m *MockDB) Backup(path string) error {
-	m.BackupInvoked = true
+	state.BackupInvoked = true
 	return m.OnBackup(path)
 }
