8375a5f65eaaa8f8e075d4393bd75c35b4c27f9c
Patrick Deziel <42919891+pdeziel@users.noreply.github.com>
2021-12-21 15:31:50 -0600

    CI and Testing: config validation tests (#276)

---
+++ pkg/sectigo/config_test.go
@@ -0,0 +1,20 @@
+package sectigo_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/trisacrypto/directory/pkg/sectigo"
+)
+
+func TestConfigValidation(t *testing.T) {
+	conf := sectigo.Config{
+		Profile: "invalid profile",
+	}
+	require.EqualError(t, conf.Validate(), fmt.Sprintf("%q is not a valid Sectigo profile name, specify one of %s", conf.Profile, strings.Join(sectigo.AllProfiles[:], ", ")))
+
+	conf.Profile = sectigo.AllProfiles[0]
+	require.NoError(t, conf.Validate())
+}
