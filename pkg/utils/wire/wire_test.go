df91fd66032e35e3102ac988e82c7f2fc041043d
Benjamin Bengfort <benjamin@bengfort.com>
2021-12-21 09:12:51 -0600

    Quick fixes: testdata and docker compose
    * removes double colons from wire package testdata filenames
    * adds GDS members service environment variables to docker compose

--- pkg/utils/wire/wire_test.go
+++ pkg/utils/wire/wire_test.go
@@ -25,7 +25,7 @@ func TestWire(t *testing.T) {
 	// The basic test pattern here is to load data from disk, then remarshal JSON
 	// and testing unmarshal of the specific namespace.
 	// Test unmarshal VASP records
-	in, err := ioutil.ReadFile("testdata/vasps::838b1f57-1646-488d-a231-d71d88681cfa.json")
+	in, err := ioutil.ReadFile("testdata/vasps/838b1f57-1646-488d-a231-d71d88681cfa.json")
 	require.NoError(t, err)
 	out, err := RemarshalJSON(NamespaceVASPs, in)
 	require.NoError(t, err)
@@ -39,7 +39,7 @@ func TestWire(t *testing.T) {
 	require.NoError(t, vasp.Validate(false))
 
 	// Test unmarshal certificate requests
-	in, err = ioutil.ReadFile("testdata/certreqs::87657b4d-e72c-4526-9332-c8fc56adb367.json")
+	in, err = ioutil.ReadFile("testdata/certreqs/87657b4d-e72c-4526-9332-c8fc56adb367.json")
 	require.NoError(t, err)
 	out, err = RemarshalJSON(NamespaceCertReqs, in)
 	require.NoError(t, err)
@@ -54,7 +54,7 @@ func TestWire(t *testing.T) {
 	require.Equal(t, models.CertificateRequestState_COMPLETED, certreq.Status)
 
 	// Test Unmarshal Peers
-	in, err = ioutil.ReadFile("testdata/peers::8.json")
+	in, err = ioutil.ReadFile("testdata/peers/8.json")
 	require.NoError(t, err)
 	out, err = RemarshalJSON(NamespaceReplicas, in)
 	require.NoError(t, err)
@@ -68,7 +68,7 @@ func TestWire(t *testing.T) {
 	require.Equal(t, "localhost:4435", peer.Addr)
 
 	// Test Unmarshal category index
-	in, err = ioutil.ReadFile("testdata/index::categories.json")
+	in, err = ioutil.ReadFile("testdata/index/categories.json")
 	require.NoError(t, err)
 	out, err = RemarshalJSON(NamespaceIndices, in)
 	require.NoError(t, err)
@@ -77,7 +77,7 @@ func TestWire(t *testing.T) {
 	require.Equal(t, 6, len(index))
 
 	// Test Unmarshal names index
-	in, err = ioutil.ReadFile("testdata/index::names.json")
+	in, err = ioutil.ReadFile("testdata/index/names.json")
 	require.NoError(t, err)
 	out, err = RemarshalJSON(NamespaceIndices, in)
 	require.NoError(t, err)
