b050a7b3ae2d285fdd061f01e326ec65c866c495
Benjamin Bengfort <benjamin@bengfort.com>
2021-12-29 19:07:21 -0600

    final cleanup

--- pkg/gds/certs.go
+++ pkg/gds/certs.go
@@ -151,7 +151,7 @@ func (s *Service) submitCertificateRequest(r *models.CertificateRequest) (err er
 	profile := s.certs.Profile()
 	if profile == sectigo.ProfileCipherTraceEndEntityCertificate || profile == sectigo.ProfileIDCipherTraceEndEntityCertificate {
 		// Default to TRISA Production locality since none has been provided.
-		// TODO: make this part of the certificate request.
+		// TODO: make this part of the certificate request (See SC-2606).
 		params["organizationName"] = "TRISA Production"
 		params["localityName"] = "Menlo Park"
 		params["stateOrProvinceName"] = "California"
@@ -161,12 +161,13 @@ func (s *Service) submitCertificateRequest(r *models.CertificateRequest) (err er
 	// Step 3: submit the certificate
 	var rep *sectigo.BatchResponse
 
-	batchName := fmt.Sprintf("%s-certreq-%s-%s)", s.conf.DirectoryID, r.CommonName, r.Id)
+	batchName := fmt.Sprintf("%s-certreq-%s)", s.conf.DirectoryID, r.Id)
 	if rep, err = s.certs.CreateSingleCertBatch(authority, batchName, params); err != nil {
 		// Although the error may be logged again by the calling function, log the error
 		// here as well to provide debugging information about why the Sectigo request failed.
 		dict := zerolog.Dict()
 		for key, value := range params {
+			// NOTE: Do not log any passwords or secrets!
 			if key == "pkcs12Password" {
 				value = strings.Repeat("*", len(value))
 			}
