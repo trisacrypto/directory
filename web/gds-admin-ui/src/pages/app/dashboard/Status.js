a9fcd609da2672d1ff023e15ff56829f4a6cd618
Cletus Razakou <masskoder@gmail.com>
2022-02-04 16:50:47 -0500

    [refactor/gds-admin-ui]: avoid getting Nan  as value when the object is empty

--- web/gds-admin-ui/src/pages/app/dashboard/Status.js
+++ web/gds-admin-ui/src/pages/app/dashboard/Status.js
@@ -10,51 +10,52 @@ const Status = ({ statuses }) => {
     const colors = ['#0d6efd', '#dc3545', '#ffc107'];
 
     const getStatusesCounts = React.useCallback(() => {
-        const initialValue = { 'VERIFIED': 0, 'REJECTED': 0, 'PENDING_REVIEW': 0 }
+        const initialValue = { VERIFIED: 0, REJECTED: 0, PENDING_REVIEW: 0 };
         const reducer = (counts, status) => {
             switch (status[0]) {
                 case STATUS.VERIFIED:
-                    counts[STATUS.VERIFIED] += status[1]
+                    counts[STATUS.VERIFIED] += status[1];
                     break;
                 case STATUS.ERRORED:
                 case STATUS.REJECTED:
-                    counts[STATUS.REJECTED] += status[1]
+                    counts[STATUS.REJECTED] += status[1];
                     break;
                 default:
-                    counts[STATUS.PENDING_REVIEW] += status[1]
+                    counts[STATUS.PENDING_REVIEW] += status[1];
                     break;
             }
-            return counts
-        }
+            return counts;
+        };
         try {
             return Object.entries(statuses).reduce(reducer, initialValue);
         } catch (error) {
-            throw error
+            throw error;
         }
-    }, [statuses])
+    }, [statuses]);
 
     const statusRatios = () => {
-        if (statuses && typeof statuses === "object") {
-            const statusesCounts = getStatusesCounts(statuses)
-            return getRatios(statusesCounts)
+        if (statuses && typeof statuses === 'object') {
+            const statusesCounts = getStatusesCounts(statuses);
+            return getRatios(statusesCounts);
         }
 
-        return {}
-    }
+        return {};
+    };
 
-    const getDonutChartData = () => statuses ? Object.values(getStatusesCounts()) : []
-    const statusPercents = () => Object.fromEntries(Object.entries(statusRatios()).map(([key, val]) => [key, val * 100.0]))
+    const getDonutChartData = () => (statuses ? Object.values(getStatusesCounts()) : []);
+    const statusPercents = () =>
+        Object.fromEntries(Object.entries(statusRatios()).map(([key, val]) => [key, val * 100.0]));
 
     const getDonutChartLabels = () => {
-        if (statuses && typeof statuses === "object") {
-            return Object.keys(getStatusesCounts()).map(status => {
-                const status_ = capitalizeFirstLetter(status).split('_')
-                return status_.join(' ')
-            })
+        if (statuses && typeof statuses === 'object') {
+            return Object.keys(getStatusesCounts()).map((status) => {
+                const status_ = capitalizeFirstLetter(status).split('_');
+                return status_.join(' ');
+            });
         }
 
-        return []
-    }
+        return [];
+    };
 
     const donutChartData = {
         labels: getDonutChartLabels(),
@@ -81,8 +82,10 @@ const Status = ({ statuses }) => {
             <Card.Body>
                 <h4 className="header-title mb-4">Registration Statuses</h4>
 
-                {
-                    !statuses ? <OvalLoader /> : <>
+                {!statuses ? (
+                    <OvalLoader />
+                ) : (
+                    <>
                         <div className="my-4 chartjs-chart" style={{ height: '202px' }}>
                             <Doughnut data={donutChartData} options={donutChartOpts} />
                         </div>
@@ -91,7 +94,7 @@ const Status = ({ statuses }) => {
                             <Col xs={4}>
                                 <i className="mdi mdi-progress-question text-warning mt-3 h4"></i>
                                 <h4 className="fw-normal">
-                                    <span>{statusPercents()[STATUS.PENDING_REVIEW] + '%'}</span>
+                                    <span>{statusPercents()[STATUS.PENDING_REVIEW] || 0 + '%'}</span>
                                 </h4>
                                 <p className="text-muted mb-0 fs-6">Pending</p>
                             </Col>
@@ -99,29 +102,27 @@ const Status = ({ statuses }) => {
                             <Col xs={4}>
                                 <i className="mdi mdi-alert-octagram text-danger mt-3 h4"></i>
                                 <h4 className="fw-normal">
-                                    <span>{statusPercents()[STATUS.REJECTED] + '%'}</span>
+                                    <span>{statusPercents()[STATUS.REJECTED] || 0 + '%'}</span>
                                 </h4>
                                 <p className="text-muted mb-0 fs-6">Rejected</p>
                             </Col>
                             <Col xs={4}>
                                 <i className="mdi mdi-shield-check text-primary mt-3 h4"></i>
                                 <h4 className="fw-normal">
-                                    <span>{statusPercents()[STATUS.VERIFIED] + '%'}</span>
+                                    <span>{statusPercents()[STATUS.VERIFIED] || 0 + '%'}</span>
                                 </h4>
                                 <p className="text-muted mb-0 fs-6"> Verified</p>
                             </Col>
-
                         </Row>
                     </>
-                }
-
+                )}
             </Card.Body>
         </Card>
     );
 };
 
 Status.propTypes = {
-    statuses: PropTypes.object
-}
+    statuses: PropTypes.object,
+};
 
 export default Status;
