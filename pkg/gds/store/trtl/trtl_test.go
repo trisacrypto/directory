212728c5b7f95664f6df09c61bd5e124f5d78e44
Patrick Deziel <deziel.patrick@gmail.com>
2022-02-09 21:21:51 -0600

    implement CertificateStore methods

--- pkg/gds/store/trtl/trtl_test.go
+++ pkg/gds/store/trtl/trtl_test.go
@@ -7,8 +7,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 	"github.com/stretchr/testify/suite"
+	"github.com/trisacrypto/directory/pkg/gds/models/v1"
 	store "github.com/trisacrypto/directory/pkg/gds/store/trtl"
 	"github.com/trisacrypto/directory/pkg/trtl"
 	"github.com/trisacrypto/directory/pkg/trtl/config"
@@ -265,4 +267,107 @@ func (s *trtlStoreTestSuite) TestDirectoryStore() {
 	reqs, err = db.ListVASPs().All()
 	require.NoError(err)
 	require.Len(reqs, 110)
+}
+
+func (s *trtlStoreTestSuite) TestCertificateStore() {
+	require := s.Require()
+
+	// Load the VASP record from testdata
+	data, err := ioutil.ReadFile("../testdata/certreq.json")
+	s.NoError(err)
+
+	certreq := &models.CertificateRequest{}
+	err = protojson.Unmarshal(data, certreq)
+	s.NoError(err)
+
+	// Verify the certificate request is loaded correctly
+	s.Empty(certreq.Id)
+	s.NotEmpty(certreq.Vasp)
+	s.NotEmpty(certreq.CommonName)
+	s.Equal(models.CertificateRequestState_INITIALIZED, certreq.Status)
+	s.Empty(certreq.Created)
+	s.Empty(certreq.Modified)
+
+	// Inject bufconn connection into the store
+	require.NoError(s.grpc.Connect())
+	defer s.grpc.Close()
+
+	db, err := store.NewMock(s.grpc.Conn)
+	require.NoError(err)
+
+	// Attempt to Create the CertReq
+	id, err := db.CreateCertReq(certreq)
+	s.NoError(err)
+
+	// Attempt to Retrieve the CertReq
+	crr, err := db.RetrieveCertReq(id)
+	s.NoError(err)
+	s.Equal(id, crr.Id)
+	s.NotEmpty(crr.Created)
+	s.Equal(crr.Modified, crr.Created)
+	s.Equal(certreq.Vasp, crr.Vasp)
+	s.Equal(certreq.CommonName, crr.CommonName)
+
+	// Attempt to save a certificate request with an ID on it
+	icrr := &models.CertificateRequest{
+		Id:         uuid.New().String(),
+		Vasp:       crr.Vasp,
+		CommonName: crr.CommonName,
+		Status:     models.CertificateRequestState_INITIALIZED,
+	}
+	_, err = db.CreateCertReq(icrr)
+	s.ErrorIs(err, storeerrors.ErrIDAlreadySet)
+
+	// Sleep for a second to roll over the clock for the modified time stamp
+	time.Sleep(1 * time.Second)
+
+	// Update the CertReq
+	crr.Status = models.CertificateRequestState_COMPLETED
+	err = db.UpdateCertReq(crr)
+	s.NoError(err)
+
+	crr, err = db.RetrieveCertReq(id)
+	s.NoError(err)
+	s.Equal(id, crr.Id)
+	s.Equal(models.CertificateRequestState_COMPLETED, crr.Status)
+	s.NotEmpty(crr.Modified)
+	s.NotEqual(crr.Modified, crr.Created)
+
+	// Attempt to update a certificate request with no Id on it
+	certreq.Id = ""
+	s.ErrorIs(db.UpdateCertReq(certreq), storeerrors.ErrIncompleteRecord)
+
+	// Delete the CertReq
+	err = db.DeleteCertReq(id)
+	s.NoError(err)
+	crr, err = db.RetrieveCertReq(id)
+	s.ErrorIs(err, storeerrors.ErrEntityNotFound)
+	s.Empty(crr)
+
+	// Add a few more certificate requests
+	for i := 0; i < 10; i++ {
+		crr := &models.CertificateRequest{
+			Vasp:       uuid.New().String(),
+			CommonName: fmt.Sprintf("trisa%d.example.com", i+1),
+			Status:     models.CertificateRequestState_COMPLETED,
+		}
+		_, err := db.CreateCertReq(crr)
+		s.NoError(err)
+	}
+
+	// Test listing all of the certificates
+	reqs, err := db.ListCertReqs().All()
+	s.NoError(err)
+	s.Len(reqs, 10)
+
+	// Test iterating over all the certificates
+	var niters int
+	iter := db.ListCertReqs()
+	for iter.Next() {
+		s.NotEmpty(iter.CertReq())
+		niters++
+	}
+	s.NoError(iter.Error())
+	iter.Release()
+	s.Equal(10, niters)
 }
