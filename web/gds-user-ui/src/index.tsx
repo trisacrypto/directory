cce005465da967a1df9b03acbef02d170f6d6353
Cletus Razakou <masskoder@gmail.com>
2022-02-17 10:58:32 -0500

    feat: set up chackra ui (#343)

--- web/gds-user-ui/src/index.tsx
+++ web/gds-user-ui/src/index.tsx
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { ChakraProvider } from '@chakra-ui/react'
 ReactDOM.render(
+<ChakraProvider> 
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+    </React.StrictMode>
+    </ChakraProvider>,
   document.getElementById('root')
 );
 
