85e4b951c9b521b64f15f959e3e8f8929f3190b7
Cletus Razakou <masskoder@gmail.com>
2022-03-01 09:27:55 -0500

    sc-3585: create account component (#356)
    * feat: adds head and about section components
    
    * fix: add manually color style to storybook
    
    * feat: add join us section component
    
    * feat: adds GoogleIcon && CreateAccount component
    
    * [refactor/style]: update btn hover color && spacing adjustment
    
    * feat: add Login component

--- web/gds-user-ui/src/application/routes/index.ts
+++ web/gds-user-ui/src/application/routes/index.ts
@@ -1,26 +1,25 @@
-
 // import commponents
 const routes = [
-    {
-        path: "/dashboard",
-        name: "Dashboard",
-        icon: "bar-chart ",
-        //component: <Dashboard>,
-        layout: "/admin",
-    },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "bar-chart ",
+    //component: <Dashboard>,
+    layout: "/admin",
+  },
 
-    {
-        path: "/login",
-        name: "Login",
-        //component: Login,
-        layout: "/landing",
-    },
-    {
-        path: "/register",
-        name: "Register",
-        //component: Register,
-        layout: "/landing",
-    },
+  {
+    path: "/login",
+    name: "Login",
+    //component: Login,
+    layout: "/landing",
+  },
+  {
+    path: "/register",
+    name: "Register",
+    //component: Register,
+    layout: "/landing",
+  },
 ];
 
 export default routes;
