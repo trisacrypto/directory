959d5abcf61e820374db8c7c31e7b194f3caccc9
Benjamin Bengfort <benjamin@bengfort.com>
2022-03-02 22:30:58 -0600

    object sampling

--- pkg/trtl/config/config.go
+++ pkg/trtl/config/config.go
@@ -43,6 +43,7 @@ type ReplicaConfig struct {
 	Name           string        `split_words:"true" required:"false" json:"name"`
 	GossipInterval time.Duration `split_words:"true" default:"1m" json:"gossip_interval"`
 	GossipSigma    time.Duration `split_words:"true" default:"5s" json:"gossip_sigma"`
+	ObjectSampling bool          `split_words:"true" default:"false"`
 }
 
 type ReplicaStrategyConfig struct {
