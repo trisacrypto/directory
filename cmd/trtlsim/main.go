8f265859c4687881353f74b0fcb67fbe78ed5480
Benjamin Bengfort <benjamin@bengfort.com>
2022-03-02 21:11:34 -0600

    Add trtl and remove sectigo from GDS pod (#361)

--- cmd/trtlsim/main.go
+++ cmd/trtlsim/main.go
@@ -32,15 +32,15 @@ func init() {
 }
 
 const (
-	interval  = 5 * time.Second                                        // the ticker interval, default to 5 seconds
-	sigma     = 100 * time.Millisecond                                 // the amount of jitter, default to 100 ms
-	accesses  = 200                                                    // desired accesses per interval, default to 15
-	regions   = 7                                                      // number of regions simultaneously using the accessor
+	interval  = 10 * time.Second                                       // the ticker interval, default to 5 seconds
+	sigma     = 200 * time.Millisecond                                 // the amount of jitter, default to 100 ms
+	accesses  = 1000                                                   // desired accesses per interval, default to 15
+	regions   = 1                                                      // number of regions simultaneously using the accessor
 	endpoint  = "localhost:4436"                                       // the endpoint of the running trtl server
 	insecure  = true                                                   // connect without mTLS
 	certPath  = "fixtures/certs/mtls.client.dev/client.pem"            // path to file on disk with certificate key-pair if using mTLS
 	poolPath  = "fixtures/certs/mtls.client.dev/certificate.chain.pem" // path to file on disk with trust pool if using mTLS
-	keyspace  = 1000                                                   // the number of keys the simulator operates on
+	keyspace  = 6000                                                   // the number of keys the simulator operates on
 	chunkSize = 512                                                    // size of each write
 	strategy  = "weighted_random"                                      // simulation strategy
 )
@@ -50,8 +50,8 @@ var namespaces = []string{"catchfireBarons", "falselightCutters", "fullCrowns", 
 
 // probabilities for reads/writes/deletes
 var probabilities = map[string]uint{
-	"read":   60,
-	"write":  38,
+	"read":   80,
+	"write":  18,
 	"delete": 2,
 }
 
