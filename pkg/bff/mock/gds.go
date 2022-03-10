26bc3cee0c287a93f6f655585e41638a4ee0637c
Benjamin Bengfort <benjamin@bengfort.com>
2022-03-10 11:27:59 -0600

    BFF mock fixtures

--- pkg/bff/mock/gds.go
+++ pkg/bff/mock/gds.go
@@ -2,14 +2,27 @@ package mock
 
 import (
 	"context"
+	"fmt"
+	"io/ioutil"
 
 	"github.com/trisacrypto/directory/pkg/bff/config"
 	"github.com/trisacrypto/directory/pkg/utils/bufconn"
 	gds "github.com/trisacrypto/trisa/pkg/trisa/gds/api/v1beta1"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/encoding/protojson"
 )
 
-const bufSize = 1024 * 1024
+const (
+	bufSize          = 1024 * 1024
+	RegisterRPC      = "Register"
+	LookupRPC        = "Lookup"
+	SearchRPC        = "Search"
+	VerificationRPC  = "Verification"
+	VerifyContactRPC = "VerifyContact"
+	StatusRPC        = "Status"
+)
 
 func NewGDS(conf config.DirectoryConfig) (g *GDS, err error) {
 	g = &GDS{
@@ -94,32 +107,135 @@ func (g *GDS) Reset() {
 	g.OnStatus = nil
 }
 
+// UseFixture allows you to specify a JSON fixture that is loaded from disk as the
+// protocol buffer response for the specified RPC, simplifying the handler mocking.
+func (g *GDS) UseFixture(rpc, path string) (err error) {
+	// Read the fixture data from disk
+	var data []byte
+	if data, err = ioutil.ReadFile(path); err != nil {
+		return fmt.Errorf("could not read fixture data: %s", err)
+	}
+
+	// Create a protobuf JSON unmarshaler
+	jsonpb := &protojson.UnmarshalOptions{
+		AllowPartial:   true,
+		DiscardUnknown: true,
+	}
+
+	switch rpc {
+	case RegisterRPC:
+		out := &gds.RegisterReply{}
+		if err = jsonpb.Unmarshal(data, out); err != nil {
+			return fmt.Errorf("could not unmarshal json into %T: %s", out, err)
+		}
+		g.OnRegister = func(context.Context, *gds.RegisterRequest) (*gds.RegisterReply, error) {
+			return out, nil
+		}
+	case LookupRPC:
+		out := &gds.LookupReply{}
+		if err = jsonpb.Unmarshal(data, out); err != nil {
+			return fmt.Errorf("could not unmarshal json into %T: %s", out, err)
+		}
+		g.OnLookup = func(context.Context, *gds.LookupRequest) (*gds.LookupReply, error) {
+			return out, nil
+		}
+	case SearchRPC:
+		out := &gds.SearchReply{}
+		if err = jsonpb.Unmarshal(data, out); err != nil {
+			return fmt.Errorf("could not unmarshal json into %T: %s", out, err)
+		}
+		g.OnSearch = func(context.Context, *gds.SearchRequest) (*gds.SearchReply, error) {
+			return out, nil
+		}
+	case VerificationRPC:
+		out := &gds.VerificationReply{}
+		if err = jsonpb.Unmarshal(data, out); err != nil {
+			return fmt.Errorf("could not unmarshal json into %T: %s", out, err)
+		}
+		g.OnVerification = func(context.Context, *gds.VerificationRequest) (*gds.VerificationReply, error) {
+			return out, nil
+		}
+	case VerifyContactRPC:
+		out := &gds.VerifyContactReply{}
+		if err = jsonpb.Unmarshal(data, out); err != nil {
+			return fmt.Errorf("could not unmarshal json into %T: %s", out, err)
+		}
+		g.OnVerifyContact = func(context.Context, *gds.VerifyContactRequest) (*gds.VerifyContactReply, error) {
+			return out, nil
+		}
+	case StatusRPC:
+		out := &gds.ServiceState{}
+		if err = jsonpb.Unmarshal(data, out); err != nil {
+			return fmt.Errorf("could not unmarshal json into %T: %s", out, err)
+		}
+		g.OnStatus = func(context.Context, *gds.HealthCheck) (*gds.ServiceState, error) {
+			return out, nil
+		}
+	default:
+		return fmt.Errorf("unknown rpc %q", rpc)
+	}
+	return nil
+}
+
+// UseError allows you to specify a gRPC status error to return from the specified RPC.
+func (g *GDS) UseError(rpc string, code codes.Code, msg string) error {
+	switch rpc {
+	case RegisterRPC:
+		g.OnRegister = func(context.Context, *gds.RegisterRequest) (*gds.RegisterReply, error) {
+			return nil, status.Error(code, msg)
+		}
+	case LookupRPC:
+		g.OnLookup = func(context.Context, *gds.LookupRequest) (*gds.LookupReply, error) {
+			return nil, status.Error(code, msg)
+		}
+	case SearchRPC:
+		g.OnSearch = func(context.Context, *gds.SearchRequest) (*gds.SearchReply, error) {
+			return nil, status.Error(code, msg)
+		}
+	case VerificationRPC:
+		g.OnVerification = func(context.Context, *gds.VerificationRequest) (*gds.VerificationReply, error) {
+			return nil, status.Error(code, msg)
+		}
+	case VerifyContactRPC:
+		g.OnVerifyContact = func(context.Context, *gds.VerifyContactRequest) (*gds.VerifyContactReply, error) {
+			return nil, status.Error(code, msg)
+		}
+	case StatusRPC:
+		g.OnStatus = func(context.Context, *gds.HealthCheck) (*gds.ServiceState, error) {
+			return nil, status.Error(code, msg)
+		}
+	default:
+		return fmt.Errorf("unknown rpc %q", rpc)
+	}
+	return nil
+}
+
 func (g *GDS) Register(ctx context.Context, in *gds.RegisterRequest) (*gds.RegisterReply, error) {
-	g.Calls["Register"]++
+	g.Calls[RegisterRPC]++
 	return g.OnRegister(ctx, in)
 }
 
 func (g *GDS) Lookup(ctx context.Context, in *gds.LookupRequest) (*gds.LookupReply, error) {
-	g.Calls["Lookup"]++
+	g.Calls[LookupRPC]++
 	return g.OnLookup(ctx, in)
 }
 
 func (g *GDS) Search(ctx context.Context, in *gds.SearchRequest) (*gds.SearchReply, error) {
-	g.Calls["Search"]++
+	g.Calls[SearchRPC]++
 	return g.OnSearch(ctx, in)
 }
 
 func (g *GDS) Verification(ctx context.Context, in *gds.VerificationRequest) (*gds.VerificationReply, error) {
-	g.Calls["Verification"]++
+	g.Calls[VerificationRPC]++
 	return g.OnVerification(ctx, in)
 }
 
 func (g *GDS) VerifyContact(ctx context.Context, in *gds.VerifyContactRequest) (*gds.VerifyContactReply, error) {
-	g.Calls["VerifyContact"]++
+	g.Calls[VerifyContactRPC]++
 	return g.OnVerifyContact(ctx, in)
 }
 
 func (g *GDS) Status(ctx context.Context, in *gds.HealthCheck) (*gds.ServiceState, error) {
-	g.Calls["Status"]++
+	g.Calls[StatusRPC]++
 	return g.OnStatus(ctx, in)
 }
