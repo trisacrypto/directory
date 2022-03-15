912ac7077fb1d0e3da996ccb32e1edad253724ea
Cletus Razakou <masskoder@gmail.com>
2022-03-15 13:17:27 -0400

    Sc-3529 : automatically lint and format code (#371)
    * refactor: remove unuse component
    
    * config: set lint && eslint
    
    * feat: vasp verification component

--- web/gds-user-ui/src/utils/theme.ts
+++ web/gds-user-ui/src/utils/theme.ts
@@ -1,27 +1,27 @@
-import { extendTheme, ThemeConfig } from "@chakra-ui/react";
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 
 const config: ThemeConfig = {
-  initialColorMode: "light",
-  useSystemColorMode: false,
+  initialColorMode: 'light',
+  useSystemColorMode: false
 };
 
 export const colors = {
-  transparent: "transparent",
-  black: "#000",
-  white: "#fff",
-  font: ["Open Sans", "sans-serif"],
+  transparent: 'transparent',
+  black: '#000',
+  white: '#fff',
+  font: ['Open Sans', 'sans-serif'],
   gray: {
-    50: "#f7fafc",
-    900: "#171923",
+    50: '#f7fafc',
+    900: '#171923'
   },
   system: {
-    blue: "#23A7E0",
-    cyan: "#1BCE9F",
-    orange: "#FF7A59",
-    white: "#E3EBEF",
-    gray: "#5B5858",
-    green: "#0A864F",
-  },
+    blue: '#23A7E0',
+    cyan: '#1BCE9F',
+    orange: '#FF7A59',
+    white: '#E3EBEF',
+    gray: '#5B5858',
+    green: '#0A864F'
+  }
 };
 
 const customTheme = extendTheme({ colors, config });
