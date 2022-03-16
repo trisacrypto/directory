085974bc3280fe316ea764cf47828a1affc1cf91
Cletus Razakou <masskoder@gmail.com>
2022-03-16 16:35:47 -0400

    [gds-ui/feat]:refactor some landing components && implement home page component (#413)

--- web/gds-user-ui/src/layouts/DashboardLayout.tsx
+++ web/gds-user-ui/src/layouts/DashboardLayout.tsx
@@ -1,11 +1,9 @@
-import Sidebar from "components/sidebar";
+import Sidebar from 'components/Sidebar';
 
 type DashboardLayoutProp = {
   children: React.ReactNode;
 };
 
-const DashboardLayout: React.FC<DashboardLayoutProp> = (props) => (
-  <Sidebar {...props} />
-);
+const DashboardLayout: React.FC<DashboardLayoutProp> = (props) => <Sidebar {...props} />;
 
 export default DashboardLayout;
--- web/gds-user-ui/src/layouts/LandingLayout.tsx
+++ web/gds-user-ui/src/layouts/LandingLayout.tsx
@@ -1,20 +1,17 @@
-import React from "react";
-import { Flex, FlexProps } from "@chakra-ui/react";
-import LandingHeader from "components/Header/LandingHeader";
-import LandingHead from "components/Head/LandingHead";
-import AboutTrisaSection from "components/Section/AboutUs";
-import JoinUsSection from "components/Section/JoinUs";
-import LandingFooter from "components/Footer/LandingFooter";
+import React from 'react';
+import { Flex, FlexProps } from '@chakra-ui/react';
+import LandingHeader from 'components/Header/LandingHeader';
+import LandingFooter from 'components/Footer/LandingFooter';
 
-export default function LandingLayout(props: FlexProps): JSX.Element {
+type LandingLayoutProp = {
+  children: React.ReactNode;
+};
+
+export default function LandingLayout(props: LandingLayoutProp): JSX.Element {
   return (
-    <Flex direction="column" align="center" maxW={"100%"} m="0 auto" {...props}>
+    <Flex direction="column" align="center" maxW={'100%'} m="0 auto" fontFamily={'Open Sans'}>
       <LandingHeader />
-
       {props.children}
-      <LandingHead />
-      <AboutTrisaSection />
-      <JoinUsSection />
       <LandingFooter />
     </Flex>
   );
