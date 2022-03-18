a81b9306da2286d2c3a0692dea3e2afc79794c87
elysee15 <54010836+elysee15@users.noreply.github.com>
2022-03-18 21:03:06 +0000

    GDS UI Refresh: implement react hook form on legal person addresse (#417)

--- web/gds-user-ui/src/components/AddressForm/index.tsx
+++ web/gds-user-ui/src/components/AddressForm/index.tsx
@@ -1,11 +1,12 @@
-import { Grid, GridItem, VStack, Text } from "@chakra-ui/react";
-import InputFormControl from "components/ui/InputFormControl";
-import SelectFormControl from "components/ui/SelectFormControl";
+import { Grid, GridItem, VStack, Text } from '@chakra-ui/react';
+import InputFormControl from 'components/ui/InputFormControl';
+import SelectFormControl from 'components/ui/SelectFormControl';
+import { addressTypeOptions } from 'constants/address';
+import { getCountriesOptions } from 'constants/countries';
 
 const AddressForm: React.FC<{}> = () => {
   return (
     <>
-      <Text>Address 1</Text>
       <VStack spacing={3.5} align="start">
         <InputFormControl
           formHelperText="Address line 1 e.g. building name/number, street name"
@@ -22,16 +23,17 @@ const AddressForm: React.FC<{}> = () => {
           controlId="address_3"
         />
 
-        <Grid
-          templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
-          gap={6}
-          width="100%"
-        >
+        <Grid templateColumns={{ base: '1fr', md: 'repeat(2, 1fr)' }} gap={6} width="100%">
           <GridItem>
-            <InputFormControl formHelperText="Country" controlId="country" />
+            <SelectFormControl
+              options={getCountriesOptions()}
+              formHelperText="Country"
+              controlId="country"
+            />
           </GridItem>
           <GridItem>
             <SelectFormControl
+              options={addressTypeOptions()}
               formHelperText="Address Type"
               controlId="address_type"
             />
