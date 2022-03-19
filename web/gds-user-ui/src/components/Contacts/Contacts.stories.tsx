860353d94bb815ee4aae3d625b7872b1ba697d17
elysee15 <elyseebleu1@gmail.com>
2022-03-19 08:13:40 +0000

    sc-4257 GDS UI Refresh: implement react hook form on other juridictions

--- web/gds-user-ui/src/components/Contacts/Contacts.stories.tsx
+++ web/gds-user-ui/src/components/Contacts/Contacts.stories.tsx
@@ -1,9 +1,9 @@
-import { Meta, Story } from "@storybook/react";
-import Contacts from ".";
+import { Meta, Story } from '@storybook/react';
+import Contacts from '.';
 
 export default {
-  title: "components/Contacts",
-  component: Contacts,
+  title: 'components/Contacts',
+  component: Contacts
 } as Meta;
 
 const Template: Story = (args) => <Contacts {...args} />;
--- web/gds-user-ui/src/components/TrixoQuestionnaireForm/TrixoQuestionnaire.stories.tsx
+++ web/gds-user-ui/src/components/TrixoQuestionnaireForm/TrixoQuestionnaire.stories.tsx
@@ -1,9 +1,11 @@
-import { Meta, Story } from "@storybook/react";
-import TrixoQuestionnaireForm from ".";
+import { Meta, Story } from '@storybook/react';
+import { withRHF } from 'hoc/withRHF';
+import TrixoQuestionnaireForm from '.';
 
 export default {
-  title: "components/TrixoQuestionnaireForm",
+  title: 'components/TrixoQuestionnaireForm',
   component: TrixoQuestionnaireForm,
+  decorators: [withRHF(false)]
 } as Meta;
 
 const Template: Story = (args) => <TrixoQuestionnaireForm {...args} />;
