f77f55ebc542558db7941c7d536c8033bc61936d
Cletus Razakou <masskoder@gmail.com>
2022-03-24 23:25:21 -0400

    [gds-ui/refact]: adds localstorage help && improve stepper hook

--- web/gds-user-ui/src/hooks/useCertificateStepper.ts
+++ web/gds-user-ui/src/hooks/useCertificateStepper.ts
@@ -1,29 +1,54 @@
 import React, { FC, useEffect } from 'react';
-import { useCertificateSteps } from 'contexts/certificateStepsContext';
-import { useDispatch, useSelector } from 'react-redux';
-import { setSteps, setCurrentStep } from 'application/store/stepper.slice';
+import { useDispatch, useSelector, RootStateOrAny } from 'react-redux';
+import { addStep, setCurrentStep, setStepStatus, TStep } from 'application/store/stepper.slice';
+import {
+  addStepToLocalStorage,
+  updateStepFromLocalStorage,
+  setCurrentStepFromLocalStorage
+} from 'utils/localStorageHelper';
+import { findStepKey } from 'utils/utils';
 
+// 'todo' comment: this hook should be improve
 const useCertificateStepper = () => {
-  const [certificateSteps] = useCertificateSteps();
-  const next = (step: number, status: string) => {
-    // set the localstorage data
-    // set the current state  to previous + 1
-    const steps = {
-      key: step,
-      status
-    };
+  const dispatch = useDispatch();
+  const CurrentStep: number = useSelector((state: RootStateOrAny) => state.stepper.currentStep);
+  const Steps: TStep[] = useSelector((state: RootStateOrAny) => state.stepper.steps);
+  const nextStep = (state?: any) => {
+    if (state) {
+      // user can go to the next with doing anything
+      const found = findStepKey(Steps, CurrentStep);
+      if (found.length === 1) {
+        updateStepFromLocalStorage(state, CurrentStep);
+        setCurrentStepFromLocalStorage(CurrentStep + 1);
+        dispatch(setStepStatus(state));
+        dispatch(setCurrentStep({ currentStep: CurrentStep + 1 }));
+        const foundNext = findStepKey(Steps, CurrentStep + 1);
+        if (foundNext.length === 0) {
+          addStepToLocalStorage({ key: CurrentStep + 1, status: 'progress' });
+          dispatch(addStep({ key: CurrentStep + 1, status: 'progress' }));
+        }
+      } else {
+        addStepToLocalStorage({ key: CurrentStep, status: 'progress' });
+        setCurrentStepFromLocalStorage(CurrentStep);
+        dispatch(addStep({ key: CurrentStep, status: state.status }));
+        dispatch(setCurrentStep({ currentStep: CurrentStep + 1 }));
+      }
+
+      // user can go to the next by updating a status
+    } else {
+      setCurrentStepFromLocalStorage(CurrentStep + 1);
+      dispatch(setCurrentStep({ currentStep: CurrentStep + 1 }));
+    }
   };
-  const previous = (step: number) => {
+  const previousStep = () => {
     // all set the previous state
-  };
-  const saveAndNext = (step: number, datas: any) => {
-    // save form data and call next function
+
+    dispatch(setCurrentStep({ currentStep: CurrentStep - 1 }));
   };
 
   return {
-    next,
-    previous,
-    saveAndNext
+    nextStep,
+    previousStep
   };
 };
 
