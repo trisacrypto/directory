58a544f5e0e9ebe5ac8855c1f8f74cd18e51b566
elysee15 <elyseebleu1@gmail.com>
2022-03-28 08:08:58 +0000

    sc-4420 GDS UI Refresh: Submit and Review

---
+++ web/gds-user-ui/src/components/ReviewSubmit/index.tsx
@@ -0,0 +1,79 @@
+import { Button, Heading, VStack, Stack, Text } from '@chakra-ui/react';
+import FormLayout from 'layouts/FormLayout';
+
+const ReviewSubmit: React.FC = () => {
+  return (
+    <VStack align="start" mt="2rem">
+      <Heading size="md">Registration Submission</Heading>
+      <FormLayout>
+        <Text>
+          You must submit your registration for TestNet and MainNet separately.{' '}
+          <Text as="span" fontWeight="bold">
+            Note:
+          </Text>{' '}
+          You will receive two separate emails with confirmation links for each registration. You
+          must click on each confirmation link to complete the registration process.
+          <Text as="span" fontWeight="bold">
+            Failure to click either confirmation will result in an incomplete registration.
+          </Text>
+        </Text>
+      </FormLayout>
+      <Stack direction={['column', 'row']} justifyContent="space-around" py={14} width="100%">
+        <Button
+          bgColor="#ff7a59f0"
+          color="#fff"
+          size="lg"
+          py="2.5rem"
+          whiteSpace="normal"
+          maxW="200px"
+          width="100%"
+          boxShadow="lg"
+          _hover={{
+            bgColor: '#f55c35'
+          }}>
+          Submit TestNet Registration
+        </Button>
+        <Button
+          bgColor="#23a7e0e8"
+          color="#fff"
+          size="lg"
+          py="2.5rem"
+          whiteSpace="normal"
+          boxShadow="lg"
+          maxW="200px"
+          width="100%"
+          _hover={{
+            bgColor: '#189fda'
+          }}>
+          Submit MainNet Registration
+        </Button>
+      </Stack>
+      <Stack spacing={10}>
+        <FormLayout>
+          <Text>
+            Your{' '}
+            <Text as="span" fontWeight="bold">
+              TestNet
+            </Text>{' '}
+            registration form has been successfully submitted. You will receive a confirmation email
+            from admin@trisa.io. In the email, you will receive instructions on next
+          </Text>
+          steps.
+        </FormLayout>
+        <FormLayout>
+          <Text>
+            Your{' '}
+            <Text as="span" fontWeight="bold">
+              MainNet
+            </Text>{' '}
+            registration form has been successfully submitted. You will receive a confirmation email
+            from admin@trisa.io. In the email, you will receive instructions on next
+          </Text>
+          steps.
+        </FormLayout>
+      </Stack>
+    </VStack>
+  );
+};
+
+export default ReviewSubmit;
--- web/gds-user-ui/src/components/testnetProgress/TestNetCertificateProgressBar.component.tsx
+++ web/gds-user-ui/src/components/testnetProgress/TestNetCertificateProgressBar.component.tsx
@@ -1,4 +1,3 @@
-import { Collapse } from '@chakra-ui/transition';
 import useCertificateStepper from 'hooks/useCertificateStepper';
 import {
   CertificateStepContainer,
@@ -10,6 +9,7 @@ import LegalPerson from 'components/LegalPerson';
 import { FormProvider, useForm } from 'react-hook-form';
 import Contacts from 'components/Contacts';
 import TrixoQuestionnaire from 'components/TrixoQuestionnaire';
+import ReviewSubmit from 'components/ReviewSubmit';
 
 const ProgressBar = () => {
   const { nextStep, previousStep } = useCertificateStepper();
@@ -24,6 +24,7 @@ const ProgressBar = () => {
           <CertificateStepContainer key="2" status="complete" component={<LegalPerson />} />
           <CertificateStepContainer key="3" status="progress" component={<Contacts />} />
           <CertificateStepContainer key="4" status="progress" component={<TrixoQuestionnaire />} />
+          <CertificateStepContainer key="5" status="progress" component={<ReviewSubmit />} />
         </CertificateSteps>
       </form>
     </FormProvider>
