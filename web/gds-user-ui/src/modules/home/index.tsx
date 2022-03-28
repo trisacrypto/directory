4dc171239d7b7ce128d13bfc9abadf8a5164aff2
Cletus Razakou <masskoder@gmail.com>
2022-03-28 00:01:21 -0400

    feat: set axios & implement lookup service

--- web/gds-user-ui/src/modules/home/index.tsx
+++ web/gds-user-ui/src/modules/home/index.tsx
@@ -1,3 +1,4 @@
+import React, { FormEvent, useEffect, useState } from 'react';
 import { Heading, Stack } from '@chakra-ui/react';
 import CollaboratorsSection from 'components/CollaboratorsSection';
 
@@ -6,14 +7,37 @@ import Head from 'components/Head/LandingHead';
 import JoinUsSection from 'components/Section/JoinUs';
 import SearchDirectory from 'components/Section/SearchDirectory';
 import AboutTrisaSection from 'components/Section/AboutUs';
-
+import { lookup } from './service';
+import { isValidUuid } from 'utils/utils';
 const HomePage: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [result, setResult] = useState(false);
+  const [error, setError] = useState('');
+
+  const handleSearchSubmit = async (evt: FormEvent, searchQuery: string) => {
+    setIsLoading(true);
+    evt.preventDefault();
+    const query = isValidUuid(searchQuery) ? `uuid=${searchQuery}` : `common_name=${searchQuery}`;
+
+    try {
+      const res = await lookup(query);
+      setResult(res.data);
+      setIsLoading(false);
+    } catch (e) {
+      setIsLoading(false);
+    }
+  };
   return (
     <LandingLayout>
-      <Head hasBtn />
+      <Head hasBtn isHomePage />
       <AboutTrisaSection />
       <JoinUsSection />
-      <SearchDirectory />
+      <SearchDirectory
+        handleSubmit={handleSearchSubmit}
+        isLoading={isLoading}
+        result={result}
+        error={error}
+      />
     </LandingLayout>
   );
 };
