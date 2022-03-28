7efad98aadfa614d3e78b85e73dcd91c4e44306f
elysee15 <elyseebleu1@gmail.com>
2022-03-28 07:17:19 +0000

    sc-4419 GDS UI refresh: TRISA implementation MainNet form

--- web/gds-user-ui/src/components/TrisaImplementationForm/TrisaImplementationForm.stories.tsx
+++ web/gds-user-ui/src/components/TrisaImplementationForm/TrisaImplementationForm.stories.tsx
@@ -1,12 +1,20 @@
-import { Meta, Story } from "@storybook/react";
-import TrisaImplementationForm from ".";
+import { Meta, Story } from '@storybook/react';
+import TrisaImplementationForm from '.';
+
+type TrisaImplementationFormProps = {
+  headerText: string;
+};
 
 export default {
-  title: "components/Trisa Implementation Form",
-  component: TrisaImplementationForm,
+  title: 'components/Trisa Implementation Form',
+  component: TrisaImplementationForm
 } as Meta;
 
-const Template: Story = (args) => <TrisaImplementationForm {...args} />;
+const Template: Story<TrisaImplementationFormProps> = (args) => (
+  <TrisaImplementationForm {...args} />
+);
 
 export const Default = Template.bind({});
-Default.args = {};
+Default.args = {
+  headerText: 'TRISA Endpoint: MainNet'
+};
