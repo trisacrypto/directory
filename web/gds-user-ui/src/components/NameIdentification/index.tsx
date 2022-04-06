8c3728de01b88e8e3cf0c58f7b15bcc5efcd7ef7
Cletus Razakou <masskoder@gmail.com>
2022-04-06 10:49:17 -0400

    fix: identification form control

--- web/gds-user-ui/src/components/NameIdentification/index.tsx
+++ web/gds-user-ui/src/components/NameIdentification/index.tsx
@@ -64,8 +64,8 @@ const NationalIdentification: React.FC<NationalIdentificationProps> = () => {
       <InputFormControl
         label="Registration Authority"
         controlId="registration_authority"
-        isRequired={NationalIdentificationType !== 'NATIONAL_IDENTIFIER_TYPE_CODE_LEIX'}
         isDisabled={NationalIdentificationType === 'NATIONAL_IDENTIFIER_TYPE_CODE_LEIX'}
+        isRequired={NationalIdentificationType !== 'NATIONAL_IDENTIFIER_TYPE_CODE_LEIX' && false}
         formHelperText="If the identifier is an LEI number, enter the ID used in the GLEIF Registration Authorities List."
         {...register('entity.national_identification.registration_authority')}
       />
