7163f2a59e004f27723ea996adde258994ef4e5e
Cletus Razakou <masskoder@gmail.com>
2022-04-06 09:57:34 -0400

    refact/style: keep the footer at the bottom

--- web/gds-user-ui/src/components/Footer/LandingFooter.tsx
+++ web/gds-user-ui/src/components/Footer/LandingFooter.tsx
@@ -1,40 +1,41 @@
-import React from "react";
-import { Flex, Text, Link, useColorModeValue } from "@chakra-ui/react";
-import { colors } from "utils/theme";
+import React from 'react';
+import { Flex, Text, Link, useColorModeValue } from '@chakra-ui/react';
+import { colors } from 'utils/theme';
 const Footer = (): React.ReactElement => {
   return (
     <Flex
-      bg={useColorModeValue(colors.system.gray, "transparent")}
+      bg={useColorModeValue(colors.system.gray, 'transparent')}
       color="white"
       width="100%"
       justifyContent="center"
       alignItems="center"
       direction="column"
       padding={4}
-    >
+      position={'absolute'}
+      bottom={0}>
       <Flex width="100%" wrap="wrap">
         <Text width="100%" textAlign="center" color="white" fontSize="sm">
-          A component of{" "}
+          A component of{' '}
           <Link href="https://trisa.io" color={colors.system.cyan}>
             the TRISA architecture
-          </Link>{" "}
+          </Link>{' '}
           for Cryptocurrency Travel Rule compliance.
         </Text>
         <Text width="100%" textAlign="center" color="white" fontSize="sm">
-          Created and maintained by{" "}
+          Created and maintained by{' '}
           <Link href="https://rotational.io" color={colors.system.cyan}>
-            {" "}
+            {' '}
             Rotational Labs
-          </Link>{" "}
-          in partnership with{" "}
+          </Link>{' '}
+          in partnership with{' '}
           <Link href="https://cyphertrace.com" color={colors.system.cyan}>
-            {" "}
+            {' '}
             CipherTrace
-          </Link>{" "}
-          CipherTrace on behalf of{" "}
+          </Link>{' '}
+          CipherTrace on behalf of{' '}
           <Link href="https://trisa.io" color={colors.system.cyan}>
             TRISA
-          </Link>{" "}
+          </Link>{' '}
           .
         </Text>
       </Flex>
