91151c0470a9a1dc0dc636fdb31d226f45b226e3
Cletus Razakou <masskoder@gmail.com>
2022-04-06 20:52:38 -0400

    [feat/gds-ui]: set up google analytics

--- web/gds-user-ui/src/application/routes/index.tsx
+++ web/gds-user-ui/src/application/routes/index.tsx
@@ -1,22 +1,26 @@
 import React, { Suspense } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
-
+import GoogleAnalyticsWrapper from 'components/GaWrapper';
+import useAnalytics from 'hooks/useAnalytics';
 const Home = React.lazy(() => import('modules/home'));
 const StartPage = React.lazy(() => import('modules/start'));
 const CertificatePage = React.lazy(() => import('modules/dashboard/certificate/registration'));
 const VerifyPage = React.lazy(() => import('modules/verify'));
 
 const AppRouter: React.FC = () => {
+  const { isInitialized } = useAnalytics();
   return (
     <Suspense fallback="loading...">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/getting-started" element={<StartPage />} />
-        <Route path="/certificate/registration" element={<CertificatePage />} />
-        <Route path="/verify" element={<VerifyPage />} />
+      <GoogleAnalyticsWrapper isInitialized={isInitialized}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/getting-started" element={<StartPage />} />
+          <Route path="/certificate/registration" element={<CertificatePage />} />
+          <Route path="/verify" element={<VerifyPage />} />
 
-        <Route element={<Home />} />
-      </Routes>
+          <Route element={<Home />} />
+        </Routes>
+      </GoogleAnalyticsWrapper>
     </Suspense>
   );
 };
