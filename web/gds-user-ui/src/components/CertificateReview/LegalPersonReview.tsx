6ed5bece5ef5950cea693eaac7da2a8af38a5d72
elysee15 <elyseebleu1@gmail.com>
2022-04-14 15:25:09 +0000

    Revert "Merge branch 'sc-4846' into main"
    This reverts commit 2ead14418ba3847c47526eec49c69af238a34583, reversing
    changes made to 0910b43394501760dc3ee4ceceb7e9b85427c161.

--- web/gds-user-ui/src/components/CertificateReview/LegalPersonReview.tsx
+++ web/gds-user-ui/src/components/CertificateReview/LegalPersonReview.tsx
@@ -111,7 +111,6 @@ const LegalPersonReview: React.FC<LegalReviewProps> = (props) => {
 
   useEffect(() => {
     const getStepperData = loadDefaultValueFromLocalStorage();
-    console.log('[getStepperData]', getStepperData);
     const stepData = {
       ...getStepperData.entity
     };
