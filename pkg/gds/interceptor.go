bfeae3ac1f397de60d2808f4dcceb3cd0685e39c
Benjamin Bengfort <benjamin@bengfort.com>
2022-04-19 17:02:17 -0500

    Panic recovery and panic bug fix (#495)

--- pkg/gds/interceptor.go
+++ pkg/gds/interceptor.go
@@ -2,8 +2,11 @@ package gds
 
 import (
 	"context"
+	"fmt"
+	"runtime/debug"
 	"time"
 
+	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -13,6 +16,19 @@ import (
 func (s *Service) serverInterceptor(ctx context.Context, in interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (out interface{}, err error) {
 	// Track how long the method takes to execute.
 	start := time.Now()
+	panicked := true
+
+	// Recover from panics in the handler.
+	// See: https://github.com/grpc-ecosystem/go-grpc-middleware/blob/4705cb37b9857ad51b4c96ff5a2f3c60afe442cf/recovery/interceptors.go#L21-L37
+	defer func() {
+		if r := recover(); r != nil || panicked {
+			log.WithLevel(zerolog.PanicLevel).
+				Err(fmt.Errorf("%v", r)).
+				Str("stack_trace", string(debug.Stack())).
+				Msg("grpc server has recovered from a panic")
+			err = status.Error(codes.Internal, "an unhandled exception occurred")
+		}
+	}()
 
 	// Check if we're in maintenance mode - status method should still return a full response.
 	if s.conf.Maintenance && info.FullMethod != "/trisa.gds.api.v1beta1.TRISADirectory/Status" {
@@ -21,6 +37,7 @@ func (s *Service) serverInterceptor(ctx context.Context, in interface{}, info *g
 
 	// Call the handler to finalize the request and get the response.
 	out, err = handler(ctx, in)
+	panicked = false
 
 	// Log with zerolog - checkout grpclog.LoggerV2 for default logging.
 	// TODO: add remote peer information if using mTLS
