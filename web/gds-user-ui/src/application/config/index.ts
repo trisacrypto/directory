2c838a17cdeff1bf01af6672999e9f6012151670
Cletus Razakou <masskoder@gmail.com>
2022-04-21 04:05:52 -0400

    Sc-4861 : Add Version information to User ui  (#501)
    * wip
    
    * feat: added version information of user ui
    
    * refactor: add try catch to prevent error
    
    * clean up && checkpoint

--- web/gds-user-ui/src/application/config/index.ts
+++ web/gds-user-ui/src/application/config/index.ts
@@ -1,1 +1,27 @@
+import axiosInstance from 'utils/axios';
+
 export const isProdEnv = process.env.NODE_ENV === 'production';
+
+export const getAppVersionNumber = () => {
+  if (isProdEnv) {
+    return process.env.REACT_APP_VERSION_NUMBER;
+  }
+};
+export const getAppGitVersion = () => {
+  if (isProdEnv) {
+    return process.env.REACT_APP_GIT_REVISION;
+  }
+};
+
+export const getBffAndGdsVersion = async () => {
+  if (isProdEnv) {
+    try {
+      const res = await axiosInstance.get('/status');
+      return res.data;
+    } catch (e) {
+      // log error in sentry or console
+      console.warn('Error while fetching BFF and GDS version', e);
+      return false;
+    }
+  }
+};
