ca4e25d00e7d715220930f3e4c59844fcdce4271
Cletus Razakou <masskoder@gmail.com>
2022-05-03 19:07:20 -0400

    sc-5041: sentry implementation on frontend side (#510)
    * gds-user-ui(feat): set up sentry
    
    * refactor: improve && clean up sentry init

---
+++ web/gds-user-ui/src/application/config/sentry.ts
@@ -0,0 +1,17 @@
+import * as Sentry from '@sentry/react';
+import { BrowserTracing } from '@sentry/tracing';
+
+const initSentry = () => {
+  Sentry.init({
+    dsn: process.env.REACT_APP_SENTRY_DSN,
+    environment: process.env.NODE_ENV,
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0
+  });
+};
+
+export default initSentry;
--- web/gds-user-ui/src/modules/home/index.tsx
+++ web/gds-user-ui/src/modules/home/index.tsx
@@ -1,11 +1,11 @@
 import React, { FormEvent, useEffect, useState } from 'react';
-import { Heading, Stack } from '@chakra-ui/react';
+
 import LandingLayout from 'layouts/LandingLayout';
 import Head from 'components/Head/LandingHead';
 import JoinUsSection from 'components/Section/JoinUs';
 import SearchDirectory from 'components/Section/SearchDirectory';
 import AboutTrisaSection from 'components/Section/AboutUs';
-
+import * as Sentry from '@sentry/react';
 import { lookup } from './service';
 import { isValidUuid } from 'utils/utils';
 const HomePage: React.FC = () => {
@@ -15,6 +15,7 @@ const HomePage: React.FC = () => {
   const [search, setSearch] = useState('');
   const handleSearchSubmit = async (evt: FormEvent, searchQuery: string) => {
     evt.preventDefault();
+    // throw new Error('Sentry Frontend Error');
     setIsLoading(true);
     const query = isValidUuid(searchQuery) ? `uuid=${searchQuery}` : `common_name=${searchQuery}`;
 
@@ -30,12 +31,14 @@ const HomePage: React.FC = () => {
       }
     } catch (e: any) {
       setIsLoading(false);
+
       if (!e.response.data.success) {
         setResult(false);
         setError(e.response.data.error);
         setResult(false);
       } else {
-        console.log('sorry something went wrong , please try again');
+        Sentry.captureMessage('Something wrong happen when we trying to call lookup api');
+        Sentry.captureException(e);
       }
     }
   };
@@ -44,6 +47,7 @@ const HomePage: React.FC = () => {
       <Head hasBtn isHomePage />
       <AboutTrisaSection />
       <JoinUsSection />
+
       <SearchDirectory
         handleSubmit={handleSearchSubmit}
         isLoading={isLoading}
