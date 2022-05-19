6ea6a0f94366dd29b3b23cc934ea79a7bc781e38
elysee15 <54010836+elysee15@users.noreply.github.com>
2022-05-19 14:42:50 +0000

    sc-5245 GDS User UI: implement  unit tests for review page (#543)
    Co-authored-by: Benjamin Bengfort <benjamin@bengfort.com>

---
+++ web/gds-user-ui/src/components/CertificateReview/ContactsReview.spec.tsx
@@ -0,0 +1,32 @@
+import { render } from 'utils/test-utils';
+import ContactsReview from './ContactsReview';
+
+const defaultValues = {
+  contacts: {
+    administrative: { name: '', email: '', phone: '' },
+    technical: { name: 'Test', email: 'test@test.com', phone: '+4535557757' },
+    billing: { name: '', email: '' },
+    legal: { name: 'Info ', email: 'info@gmail.com', phone: '+93400583484' }
+  }
+};
+
+describe('<ContactsReview />', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it('should match snapshot', () => {
+    localStorage.setItem('certificateForm', JSON.stringify(defaultValues));
+
+    const { container } = render(<ContactsReview />);
+
+    expect(localStorage.getItem).toHaveBeenCalledWith('certificateForm');
+    expect(Object.keys(localStorage.__STORE__).length).toBe(1);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  afterAll(() => {
+    jest.clearAllMocks();
+  });
+});
