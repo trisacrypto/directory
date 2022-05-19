13d5fbf2887f0c26b135288de214c54104e57a17
elysee15 <elyseebleu1@gmail.com>
2022-05-19 10:13:30 +0000

    sc-3491 setup Lingui for internalization

---
+++ web/gds-user-ui/src/locales/fr/messages.js
@@ -0,0 +1,1 @@
+/*eslint-disable*/module.exports = { messages: { "About TRISA": "A propos de TRISA" } };
---
+++ web/gds-user-ui/src/utils/i18nLoaderHelper.ts
@@ -0,0 +1,19 @@
+import { i18n } from '@lingui/core';
+import { en, fr, de, zh, ja } from 'make-plural/plurals';
+import { Locales } from 'types/type';
+
+export const DEFAULT_LOCALE: Locales = 'en';
+
+export async function dynamicActivate(locale: string) {
+  const { messages } = await import(`../locales/${locale}/messages`);
+
+  i18n.load(locale, messages);
+  i18n.loadLocaleData({
+    en: { plurals: en },
+    de: { plurals: de },
+    fr: { plurals: fr },
+    zh: { plurals: zh },
+    ja: { plurals: ja }
+  });
+  i18n.activate(locale);
+}
