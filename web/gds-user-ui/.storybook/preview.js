2195497a524aadccbd6a2b7a388a46c338c2cb46
elysee15 <54010836+elysee15@users.noreply.github.com>
2022-06-07 14:49:38 +0000

    sc-6068 GDS User UI : implement comply page (#565)

--- web/gds-user-ui/.storybook/preview.js
+++ web/gds-user-ui/.storybook/preview.js
@@ -1,7 +1,16 @@
 import customTheme from "../src/utils/theme";
+import { LanguageProvider } from '../src/contexts/LanguageContext'
 
 export const parameters = {
   chakra: {
     customTheme,
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <LanguageProvider>
+      <Story />
+    </LanguageProvider>
+  ),
+];
