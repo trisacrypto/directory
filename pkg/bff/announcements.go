518e2faad938f6887a182b4b9cb26ae2ff869b05
Benjamin Bengfort <benjamin@bengfort.com>
2022-07-04 21:22:02 -0500

    announcements endpoint tests

--- pkg/bff/announcements.go
+++ pkg/bff/announcements.go
@@ -64,7 +64,7 @@ func (s *Server) MakeAnnouncement(c *gin.Context) {
 
 	if claims.Email == "" {
 		log.Warn().Msg("missing email on claims, cannot set author of network announcement")
-		c.JSON(http.StatusInternalServerError, api.ErrorResponse("user claims are not correctly configured"))
+		c.JSON(http.StatusBadRequest, api.ErrorResponse("user claims are not correctly configured"))
 		return
 	}
 
