88b5ac525cf5621345e08b54f1ef9ad06301d3e9
elysee15 <54010836+elysee15@users.noreply.github.com>
2022-07-06 13:06:49 +0000

    sc-6889 GDS UI ADMIN : add release to sentry config (#622)

--- web/gds-admin-ui/src/sentry.js
+++ web/gds-admin-ui/src/sentry.js
@@ -1,9 +1,11 @@
 import * as Sentry from '@sentry/react'
 import { BrowserTracing } from '@sentry/tracing';
+import config from './config';
 
 const defaultTracingOrigins = ['localhost', /^\//];
 
 const initSentry = () => {
+    console.log(config.appVersion)
 
     if (process.env.REACT_APP_SENTRY_DSN) {
         let tracingOrigins = defaultTracingOrigins;
@@ -17,6 +19,7 @@ const initSentry = () => {
             integrations: [new BrowserTracing({ tracingOrigins })],
             environment: process.env.NODE_ENV,
             tracesSampleRate: 1.0,
+            release: config.appVersion
         });
 
         // eslint-disable-next-line no-console
