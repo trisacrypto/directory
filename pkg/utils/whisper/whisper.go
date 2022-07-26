f5f0329b3b49146d933eb350d2450935e4b947b4
DanielSollis <daniel@rotational.io>
2022-07-26 13:56:55 -0700

    added documentation and additional argument checks

--- pkg/utils/whisper/whisper.go
+++ pkg/utils/whisper/whisper.go
@@ -8,28 +8,43 @@ import (
 	whisper "github.com/rotationalio/whisper/pkg/api/v1"
 )
 
-// Takes in a PKCS12 secret and
+const accessDefault int = 3
+
+// Takes in a PKCS12 secret and whisper password and returns the Whisper URL
+// used to retrieve the secret.
 func CreateWhisperLink(secret string, password string, accesses int, expirationDate time.Time) (link string, err error) {
+	// Ensure the secret is not empty
 	if secret == "" {
 		return "", errors.New("a secret is required to generate a Whisper link")
 	}
 
+	// Ensure the password is not empty
 	if password == "" {
 		return "", errors.New("a password is required to generate a Whisper link")
 	}
+
+	// If accesses if not set, set it to the default
 	if accesses <= 0 {
-		accesses = 3
+		accesses = accessDefault
 	}
 
+	// Ensure the expiration date is valid
+	if expirationDate.Before(time.Now()) {
+		return "", errors.New("the expiration date for the secret must be in the future")
+	}
+
+	// Create the whisper client
 	var client whisper.Service
 	endpoint := "https://api.whisper.rotational.dev"
 	if client, err = whisper.New(endpoint); err != nil {
 		return "", err
 	}
 
+	// Convert the expiration date into a whisper.Duration
 	until := time.Until(expirationDate)
 	lifetime := whisper.Duration(until)
 
+	// Create the secret request
 	request := &whisper.CreateSecretRequest{
 		Secret:   secret,
 		Password: password,
@@ -38,14 +53,17 @@ func CreateWhisperLink(secret string, password string, accesses int, expirationD
 		IsBase64: false,
 	}
 
+	// Create a 30 second context timeout
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	defer cancel()
 
+	// Call CreateSecretReply
 	var reply *whisper.CreateSecretReply
 	if reply, err = client.CreateSecret(ctx, request); err != nil {
 		return "", err
 	}
 
+	// Create and return the Whisper link with the returned
 	link = endpoint + "/secret/" + reply.Token
 	return link, nil
 }
--- pkg/utils/whisper/whisper_test.go
+++ pkg/utils/whisper/whisper_test.go
@@ -12,31 +12,42 @@ import (
 )
 
 func TestCreateWhisperLink(t *testing.T) {
+	// CreateWhisperLink should throw an error when passed an empty secret
 	accesses := 3
 	oneWeek := time.Now().AddDate(0, 0, 7)
 	link, err := whisper.CreateWhisperLink("", "not empty", accesses, oneWeek)
 	require.Equal(t, link, "")
 	require.EqualError(t, err, "a secret is required to generate a Whisper link")
 
+	// CreateWhisperLink should throw an error when passed an empty password
 	link, err = whisper.CreateWhisperLink("not empty", "", accesses, oneWeek)
 	require.Equal(t, link, "")
 	require.EqualError(t, err, "a password is required to generate a Whisper link")
 
+	// CreateWhisperLink should throw an error when passed an expiration date in the past
+	yesterday := time.Now().Add(-time.Hour * 24)
+	link, err = whisper.CreateWhisperLink("not empty", "not empty", accesses, yesterday)
+	require.Equal(t, link, "")
+	require.EqualError(t, err, "the expiration date for the secret must be in the future")
+
+	// Pass in valid arguments and check that the returned URL is valid
 	link, err = whisper.CreateWhisperLink("this is a secret", "password", accesses, oneWeek)
-	slashIndex := strings.LastIndex(link, "/")
-	url := link[:slashIndex+1]
-	token := link[slashIndex+1:]
+	lastSlash := strings.LastIndex(link, "/")
+	url := link[:lastSlash+1]
+	token := link[lastSlash+1:]
 	require.Equal(t, url, "https://api.whisper.rotational.dev/secret/")
 	require.NotNil(t, token)
 
+	// Create a Whisper client
 	whisperClient, err := api.New("https://api.whisper.rotational.dev")
 	require.NoError(t, err)
 
-	for i := 1; i <= 4; i++ {
+	// Make sure that the returned token can be used to fetch the secret for the number of
+	// times that the 'accesses' argument was set
+	for i := 1; i <= accesses; i++ {
 		fetchReply, err := whisperClient.FetchSecret(context.Background(), token, "password")
-		require.NoError(t, err)
+		require.NoError(t, err, token)
 		require.Equal(t, fetchReply.Secret, "this is a secret")
-		require.Equal(t, fetchReply.Accesses, i)
 		if i == accesses {
 			require.True(t, fetchReply.Destroyed)
 		}
