029ee2403a371d34aecaa67345e5a359e4cbd5b1
kbelita <karen@rotational.io>
2022-08-05 09:36:09 -0400

    function to check if an email has been sent to the contact in some time window

---
+++ pkg/gds/emails/sentlogs.go
@@ -0,0 +1,37 @@
+// This contains helper functions to help confirm if emails were sent.
+package emails
+
+import (
+	"time"
+
+	"github.com/trisacrypto/directory/pkg/gds/models/v1"
+	pb "github.com/trisacrypto/trisa/pkg/trisa/gds/models/v1beta1"
+)
+
+// VerifyEmailSentVASPContact checks if an email has been sent to the VASP contacts in some time window (e.g. the past 30 days).
+// Returns false for either email has not been sent or an error occurred.
+func VerifyEmailSentVASPContact(vasp *pb.VASP, kind string, timeWindowDays int) (status bool, err error) {
+
+	contact := models.ContactFromType(vasp.Contacts, kind)
+	emailLog, err := models.GetEmailLog(contact)
+	if err != nil {
+		status = false
+		return status, err
+	} else {
+		// get latest timestamp
+		strTimestamp := emailLog[len(emailLog)-1].Timestamp
+
+		//format emailTimestamp
+		timestamp, _ := time.Parse(time.RFC3339, strTimestamp)
+
+		//check if an email has been send within time window.
+		WithinTimeWindow := timestamp.After(time.Now().AddDate(0, 0, -timeWindowDays))
+		if WithinTimeWindow {
+			status = true
+			return status, nil
+		} else {
+			status = false
+			return status, nil
+		}
+	}
+}
