60668e5f29b83414b34b30e17c9f8953c65a0a32
elysee15 <elyseebleu1@gmail.com>
2022-08-05 03:12:23 +0000

    sc-7858 Refactor password reset module component to reach 60% in average for code coverage

---
+++ web/gds-user-ui/src/components/Section/PasswordResetForm.tsx
@@ -0,0 +1,56 @@
+import { Stack, Button, useColorModeValue, Box } from '@chakra-ui/react';
+import { t } from '@lingui/macro';
+import { Trans } from '@lingui/react';
+import InputFormControl from 'components/ui/InputFormControl';
+import { SubmitHandler, useFormContext } from 'react-hook-form';
+import { getValueByPathname } from 'utils/utils';
+import { ResetPasswordFormValues } from './PasswordReset';
+
+type PasswordResetFormProps = {
+  onSubmit: SubmitHandler<ResetPasswordFormValues>;
+};
+
+function PasswordResetForm({ onSubmit }: PasswordResetFormProps) {
+  const { register, formState, handleSubmit } = useFormContext<ResetPasswordFormValues>();
+  const { isSubmitting, errors } = formState;
+
+  return (
+    <Box
+      rounded={'lg'}
+      bg={useColorModeValue('white', 'transparent')}
+      position={'relative'}
+      bottom={5}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Stack spacing={4}>
+          <InputFormControl
+            placeholder={t`Email Address`}
+            type="email"
+            size="lg"
+            isInvalid={getValueByPathname(errors, 'email')}
+            formHelperText={getValueByPathname(errors, 'email')?.message}
+            controlId={''}
+            {...register('email')}
+          />
+          <Button
+            display="block"
+            alignSelf="start"
+            px={16}
+            bg="blue"
+            color={'white'}
+            isLoading={isSubmitting}
+            type="submit"
+            _hover={{
+              bg: '#10aaed'
+            }}
+            _focus={{
+              borderColor: 'transparent'
+            }}>
+            <Trans id="Submit">Submit</Trans>
+          </Button>
+        </Stack>
+      </form>
+    </Box>
+  );
+}
+
+export default PasswordResetForm;
