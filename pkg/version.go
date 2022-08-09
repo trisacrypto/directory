514113e92ef1e8da394e88f402f391c4bf905a02
Benjamin Bengfort <benjamin@bengfort.com>
2022-08-09 10:43:44 -0500

    version bump v1.5.0-rc.22

--- pkg/version.go
+++ pkg/version.go
@@ -8,10 +8,10 @@ import "fmt"
 // Version component constants for the current build.
 const (
 	VersionMajor         = 1
-	VersionMinor         = 4
-	VersionPatch         = 1
-	VersionReleaseLevel  = ""
-	VersionReleaseNumber = 21
+	VersionMinor         = 5
+	VersionPatch         = 0
+	VersionReleaseLevel  = "rc"
+	VersionReleaseNumber = 22
 )
 
 // Set the GitVersion via -ldflags="-X 'github.com/trisacrypto/directory/pkg.GitVersion=$(git rev-parse --short HEAD)'"
@@ -20,7 +20,7 @@ var GitVersion string
 // Version returns the semantic version for the current build.
 func Version() string {
 	var versionCore string
-	if VersionPatch > 0 {
+	if VersionPatch > 0 || VersionReleaseLevel != "" {
 		versionCore = fmt.Sprintf("%d.%d.%d", VersionMajor, VersionMinor, VersionPatch)
 	} else {
 		versionCore = fmt.Sprintf("%d.%d", VersionMajor, VersionMinor)
@@ -29,8 +29,9 @@ func Version() string {
 	if VersionReleaseLevel != "" {
 		if VersionReleaseNumber > 0 {
 			versionCore = fmt.Sprintf("%s-%s.%d", versionCore, VersionReleaseLevel, VersionReleaseNumber)
+		} else {
+			versionCore = fmt.Sprintf("%s-%s", versionCore, VersionReleaseLevel)
 		}
-		versionCore = fmt.Sprintf("%s-%s", versionCore, VersionReleaseLevel)
 	}
 
 	if GitVersion != "" {
