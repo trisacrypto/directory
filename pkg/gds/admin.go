c9e02fb0a2d4daa988268638f9ef5f13345d2295
Benjamin Bengfort <benjamin@bengfort.com>
2022-08-09 10:43:44 -0500

    version bump v1.5.0-rc.22

--- pkg/gds/admin.go
+++ pkg/gds/admin.go
@@ -179,7 +179,7 @@ func (s *Admin) setupRoutes() (err error) {
 		cors.New(cors.Config{
 			AllowOrigins:     s.conf.AllowOrigins,
 			AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD"},
-			AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "X-CSRF-TOKEN", "sentry-trace"},
+			AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization", "X-CSRF-TOKEN", "sentry-trace", "baggage"},
 			AllowCredentials: true,
 			MaxAge:           12 * time.Hour,
 		}),
