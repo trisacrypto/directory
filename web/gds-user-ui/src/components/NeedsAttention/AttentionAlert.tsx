0557439cbc1f5a169c3e45405c6f8415a8d55019
Cletus Razakou <masskoder@gmail.com>
2022-08-09 11:59:29 -0400

    fix: updated btn text for complete registation action

--- web/gds-user-ui/src/components/NeedsAttention/AttentionAlert.tsx
+++ web/gds-user-ui/src/components/NeedsAttention/AttentionAlert.tsx
@@ -45,59 +45,68 @@ const AttentionAlert = ({
   onClick,
   buttonText
 }: AttentionAlertProps) => {
-  switch (action as AttentionAction) {
-    case AttentionAction.START_REGISTRATION:
-      return (
-        <>
-          <Alert status={severity.toLowerCase()} borderRadius={'10px'}>
-            <AlertIcon />
-            <HStack justifyContent={'space-between'}>
-              <Text> {message}</Text>
-              <Button
-                onClick={onClick}
-                width={142}
-                as={'a'}
-                borderRadius={0}
-                background="transparent"
-                color="#fff"
-                cursor="pointer"
-                _hover={{ background: 'blue' }}>
-                {buttonText}
-              </Button>
-            </HStack>
-          </Alert>{' '}
-        </>
-      );
-    case AttentionAction.COMPLETE_REGISTRATION:
-      return (
-        <>
-          <Alert status={severity.toLowerCase()} borderRadius={'10px'}>
-            <AlertIcon />
-            <HStack justifyContent={'space-between'}>
-              <Text> {message}</Text>
-              <Button
-                onClick={onClick}
-                width={142}
-                as={'a'}
-                borderRadius={0}
-                background="transparent"
-                color="#fff"
-                cursor="pointer"
-                _hover={{ background: 'blue' }}>
-                {buttonText}
-              </Button>
-            </HStack>
-          </Alert>{' '}
-        </>
-      );
+  if (severity === AttentionSeverity.INFO.toUpperCase()) {
+    switch (action as AttentionAction) {
+      case AttentionAction.START_REGISTRATION:
+        return (
+          <>
+            <Alert status={severity.toLowerCase()} borderRadius={'10px'}>
+              <AlertIcon />
+              <HStack justifyContent={'space-between'}>
+                <Text> {message}</Text>
+                <Button
+                  onClick={onClick}
+                  width={142}
+                  as={'a'}
+                  borderRadius={0}
+                  background="transparent"
+                  color="#fff"
+                  cursor="pointer"
+                  _hover={{ background: 'blue' }}>
+                  Start
+                </Button>
+              </HStack>
+            </Alert>{' '}
+          </>
+        );
+      case AttentionAction.COMPLETE_REGISTRATION:
+        return (
+          <>
+            <Alert status={severity.toLowerCase()} borderRadius={'10px'}>
+              <AlertIcon />
+              <HStack justifyContent={'space-between'}>
+                <Text> {message}</Text>
+                <Button
+                  onClick={onClick}
+                  width={142}
+                  as={'a'}
+                  borderRadius={0}
+                  background="transparent"
+                  color="#fff"
+                  cursor="pointer"
+                  _hover={{ background: 'blue' }}>
+                  Complete
+                </Button>
+              </HStack>
+            </Alert>{' '}
+          </>
+        );
 
-    default:
-      return (
-        <Alert status={severity.toLowerCase()} borderRadius={'10px'}>
-          <AlertIcon />
-          {message}
-        </Alert>
-      );
+      default:
+        return (
+          <Alert status={severity.toLowerCase()} borderRadius={'10px'}>
+            <AlertIcon />
+            {message}
+          </Alert>
+        );
+    }
+  } else {
+    return (
+      <Alert status={severity.toLowerCase()} borderRadius={'10px'}>
+        <AlertIcon />
+        {message}
+      </Alert>
+    );
   }
 };
 
