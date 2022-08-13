405388cb2010f3435661ddda34ba76ee7d5e875d
Cletus Razakou <masskoder@gmail.com>
2022-08-13 17:57:55 -0400

    refact: display only error message

--- web/gds-user-ui/src/modules/auth/callback/index.tsx
+++ web/gds-user-ui/src/modules/auth/callback/index.tsx
@@ -10,24 +10,43 @@ import { useNavigate } from 'react-router-dom';
 import Loader from 'components/Loader';
 import { t } from '@lingui/macro';
 import { useSelector, useDispatch } from 'react-redux';
-
+import * as Sentry from '@sentry/browser';
+import ErrorMessage from 'components/ui/ErrorMessage';
 const CallbackPage: React.FC = () => {
   const [isLoading, setIsloading] = useState(false);
+
   const query = useHashQuery();
   const accessToken = query.access_token;
+  const callbackError = query.error;
   const { isFetching, isLoggedIn, isError, errorMessage } = useSelector(userSelector);
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const toast = useToast();
+  const getErrorMessage = () => {
+    if (errorMessage?.error) {
+      return errorMessage.error;
+    } else {
+      return ErrorMessage;
+    }
+  };
   useEffect(() => {
-    clearCookies();
     dispatch(getAuth0User(accessToken));
-  }, [accessToken, dispatch]);
+  }, [accessToken]);
 
   useEffect(() => {
+    if (callbackError) {
+      toast({
+        description: query.error_description,
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+        position: 'top-right'
+      });
+    }
     if (isError) {
       toast({
-        description: errorMessage,
+        description: getErrorMessage(),
         status: 'error',
         duration: 5000,
         isClosable: true,
@@ -38,7 +57,7 @@ const CallbackPage: React.FC = () => {
       navigate('/dashboard/overview');
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isError, isLoggedIn]);
+  }, [isError, isLoggedIn, callbackError]);
 
   useEffect(() => {
     if (isFetching) {
@@ -52,7 +71,13 @@ const CallbackPage: React.FC = () => {
     <Box height={'100%'}>
       {isLoading && <Loader />}
       {isFetching && <Loader text="Loading Dashboard ..." />}
-      {isError && <AlertMessage title={t`Token not valid`} message={errorMessage} status="error" />}
+      {isError && (
+        <AlertMessage
+          title={callbackError || t`Token not valid`}
+          message={query.error_description || getErrorMessage()}
+          status="error"
+        />
+      )}
     </Box>
   );
 };
--- web/gds-user-ui/src/utils/utils.ts
+++ web/gds-user-ui/src/utils/utils.ts
@@ -147,16 +147,16 @@ export const handleError = (error: any, customMessage?: string) => {
   Sentry.captureMessage(customMessage || error);
   Sentry.captureException(error);
   console.log(error.response);
-  if (error.response.status === 401 || error.response.status === 403) {
-    clearCookies();
-    const origin = window.location.origin;
-    if (error.response.status === 401) {
-      window.location.href = `/auth/login?from=${origin}&q=unauthorized`;
-    }
-    if (error.response.status === 403) {
-      window.location.href = `/auth/login?from=${origin}&q=token_expired`;
-    }
-  }
+  // if (error.response.status === 401 || error.response.status === 403) {
+  //   clearCookies();
+  //   const origin = window.location.origin;
+  //   if (error.response.status === 401) {
+  //     window.location.href = `/auth/login?from=${origin}&q=unauthorized`;
+  //   }
+  //   if (error.response.status === 403) {
+  //     window.location.href = `/auth/login?from=${origin}&q=token_expired`;
+  //   }
+  // }
 };
 
 // uppercased first letter
