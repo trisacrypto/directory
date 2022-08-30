5f21e4272e02dd7b869fa6c1479a735ccb5f5a85
DanielSollis <daniel@rotational.io>
2022-08-30 12:11:47 -0700

    fixed testing issue by lengthening reissuance interval

--- pkg/gds/certman/certs_test.go
+++ pkg/gds/certman/certs_test.go
@@ -903,12 +903,6 @@ func (s *certTestSuite) TestCertManagerRequestLoop() {
 	s.runCertManager(s.conf.CertMan.RequestInterval)
 }
 
-func (s *certTestSuite) TestCertManagerReissuanceLoop() {
-	s.setupCertManager(sectigo.ProfileCipherTraceEE, fixtures.Full)
-	defer s.teardownCertManager()
-	s.runCertManager(s.conf.CertMan.ReissuenceInterval)
-}
-
 func (s *certTestSuite) setupCertManager(profile string, fType fixtures.FixtureType) (certPath string) {
 	require := s.Require()
 
@@ -924,7 +918,6 @@ func (s *certTestSuite) setupCertManager(profile string, fType fixtures.FixtureT
 	require.NoError(err, "could not create cert storage")
 	s.conf.CertMan.Storage = certPath
 	s.conf.CertMan.RequestInterval = time.Millisecond
-	s.conf.CertMan.ReissuenceInterval = 10 * time.Millisecond
 	s.conf.CertMan.Sectigo.Profile = profile
 
 	// Initialize the trtl store
