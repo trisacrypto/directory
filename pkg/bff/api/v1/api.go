86848c926aeab5ba53d91f03f1ad069bdb0e9f08
Patrick Deziel <42919891+pdeziel@users.noreply.github.com>
2022-09-07 12:56:47 -0500

    Return registration form on PUT (#742)
    Co-authored-by: Benjamin Bengfort <benjamin@bengfort.com>

--- pkg/bff/api/v1/api.go
+++ pkg/bff/api/v1/api.go
@@ -22,7 +22,7 @@ type BFFClient interface {
 
 	// Authenticated Endpoints
 	LoadRegistrationForm(context.Context) (*models.RegistrationForm, error)
-	SaveRegistrationForm(context.Context, *models.RegistrationForm) error
+	SaveRegistrationForm(context.Context, *models.RegistrationForm) (*models.RegistrationForm, error)
 	SubmitRegistration(_ context.Context, network string) (*RegisterReply, error)
 	RegistrationStatus(context.Context) (*RegistrationStatus, error)
 	Overview(context.Context) (*OverviewReply, error)
