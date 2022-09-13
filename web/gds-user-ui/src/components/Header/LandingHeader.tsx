e0635733574094303b6fa8274f5105da3a14aecb
Elysee Bleu <elyseebleu@192.168.1.4>
2022-09-13 15:29:24 +0000

    sc-9182 GDS USER UI : fix mobile nav menu on small screen

--- web/gds-user-ui/src/components/Header/LandingHeader.tsx
+++ web/gds-user-ui/src/components/Header/LandingHeader.tsx
@@ -8,16 +8,12 @@ import {
   Link,
   Container,
   Stack,
-  VStack,
   Drawer,
   DrawerOverlay,
   DrawerContent,
-  DrawerHeader,
   useDisclosure,
   DrawerCloseButton,
-  Text,
   Button,
-  IconButton,
   useColorMode
 } from '@chakra-ui/react';
 import { MenuIcon, CloseIcon } from '../Icon';
@@ -29,7 +25,6 @@ import LanguagesDropdown from 'components/LanguagesDropdown';
 import { NavLink } from 'react-router-dom';
 import { useLanguageProvider } from 'contexts/LanguageContext';
 import { TRISA_BASE_URL } from 'constants/trisa-base-url';
-import { MdModeNight, MdOutlineWbSunny } from 'react-icons/md';
 import useAuth from 'hooks/useAuth';
 const LandingHeader = (props: FlexProps): JSX.Element => {
   const [show, setShow] = React.useState(false);
@@ -72,12 +67,6 @@ const LandingHeader = (props: FlexProps): JSX.Element => {
               alignItems={'center'}
               display={{ base: 'none', sm: 'flex' }}
               direction={['column', 'row']}>
-              {/* <IconButton
-                aria-label={`swith to ${isLight ? 'light' : 'dark'}`}
-                icon={isLight ? <MdOutlineWbSunny /> : <MdModeNight />}
-                variant="outline"
-                onClick={toggleColorMode}
-              /> */}
               <Stack pr={2}>
                 <LanguagesDropdown />
               </Stack>
@@ -117,34 +106,16 @@ const LandingHeader = (props: FlexProps): JSX.Element => {
                     }
                   }}
                 />
-                <DrawerBody mt="50px" px={0}>
-                  <VStack
-                    alignItems="start"
-                    sx={{
-                      p: {
-                        color: '#fff',
-                        paddingY: 2,
-                        m: '0 !important',
-                        w: '100%',
-                        pl: '25px'
-                      }
-                    }}>
-                    <MenuItem to="/#about">
-                      <Trans id="About TRISA">About TRISA</Trans>{' '}
-                    </MenuItem>
-                    <MenuItem to={`${TRISA_BASE_URL}/${locale}`}>
-                      <Trans id="Documentation">Documentation</Trans>
-                    </MenuItem>
-                    {!isLoggedIn ? (
-                      <MenuItem to="/auth/login">
-                        <Trans id="Login">Login</Trans>
-                      </MenuItem>
-                    ) : (
-                      <MenuItem to="/dashboard/overview">
-                        <Trans id="Your dashboard">Your dashboard</Trans>
-                      </MenuItem>
-                    )}{' '}
-                  </VStack>
+                <DrawerBody mt="50px" px={5}>
+                  <MenuItem to="/#about" color="white" pb={0}>
+                    <Trans id="About TRISA">About TRISA</Trans>
+                  </MenuItem>
+                  <MenuItem to={`${TRISA_BASE_URL}/${locale}`} color="white">
+                    <Trans id="Documentation">Documentation</Trans>
+                  </MenuItem>
+                  <MenuItem to="/auth/login" color="white">
+                    <Trans id="Login">Login</Trans>
+                  </MenuItem>
                 </DrawerBody>
               </DrawerContent>
             </Drawer>
