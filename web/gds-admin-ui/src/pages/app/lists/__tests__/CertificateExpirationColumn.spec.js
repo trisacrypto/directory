a31b71909a01fd81307d3e1b225e63e1c7ba7e28
elysee15 <54010836+elysee15@users.noreply.github.com>
2022-09-22 11:20:31 +0000

    sc-8715 GDS Admin UI: display N/A for empty value on vasps list (#778)
    Co-authored-by: elysee15 <elyseebleu@192.168.1.4>
    Co-authored-by: Cletus Razakou <masskoder@gmail.com>

---
+++ web/gds-admin-ui/src/pages/app/lists/__tests__/CertificateExpirationColumn.spec.js
@@ -0,0 +1,35 @@
+import { render, screen } from "utils/test-utils"
+import { CertificateExpirationColumn } from ".."
+import faker from 'faker'
+import dayjs from 'dayjs'
+
+describe('<CertificateExpirationColumn />', () => {
+
+    it('should display data correctly', () => {
+        const row = {
+            original: {
+                id: faker.datatype.uuid(),
+                certificate_expiration: faker.date.recent()
+            }
+        }
+        render(<CertificateExpirationColumn row={row} />)
+
+        expect(screen.getByTestId('certificate_expiration').textContent).toBe(
+            dayjs(row?.original?.certificate_expiration).format("MMM DD, YYYY h:mm:ss a")
+        )
+
+    })
+
+    it('should display N/A', () => {
+        const row = {
+            original: {
+                id: faker.datatype.uuid(),
+                certificate_expiration: null
+            }
+        }
+        render(<CertificateExpirationColumn row={row} />)
+
+        expect(screen.getByTestId('certificate_expiration').textContent).toBe('N/A')
+
+    })
+})
