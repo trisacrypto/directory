b020ed76a96e2e8360959ae190f63d84c2591bda
Cletus Razakou <masskoder@gmail.com>
2022-10-17 16:28:13 -0400

    sc-8099:  ensure env are set in sentry set up (#821)
    * refac: ensure env are set in sentry set up
    
    * minor fixes
    
    * clean up

--- web/gds-admin-ui/src/sentry.js
+++ web/gds-admin-ui/src/sentry.js
@@ -5,7 +5,14 @@ import config from './config';
 const defaultTracingOrigins = ['localhost', /^\//];
 
 const initSentry = () => {
-    console.log(config.appVersion)
+   // ensure environment variables app version and git revision are set
+    if (!config.appVersion) {
+        throw new Error('App version is not set in environment variables');
+    }
+    if (!config.gitVersion) {
+        throw new Error('Git revision is not set in environment variables');
+    }
+    console.log(`AppVersion: ${config.appVersion || 'not set'} - GitRevision: ${config.gitVersion || 'not set'}`); // eslint-disable-line no-console
 
     if (process.env.REACT_APP_SENTRY_DSN) {
         let tracingOrigins = defaultTracingOrigins;
