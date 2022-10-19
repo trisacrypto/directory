f72dc5d12adff0f6d729610e8c16cd2db40f5a77
elysee15 <elyseebleu1@gmail.com>
2022-10-19 12:11:20 +0000

    sc-10105 Fix Admin UI Footer

--- web/gds-admin-ui/src/layouts/Footer.js
+++ web/gds-admin-ui/src/layouts/Footer.js
@@ -23,17 +23,15 @@ const Footer = () => {
                 <div className="container-fluid">
                     <Row>
                         <Col md={12}>{currentYear} ©
-                            Created and maintained by <a href="https://rotational.io">Rotational</a> Labs in partnership with <a href="https://ciphertrace.com">CipherTrace</a> on behalf of <a href="https://trisa.io">TRISA</a>.
+                            Created and maintained by <a href="https://rotational.io">Rotational Labs</a> in partnership with <a href="https://ciphertrace.com">CipherTrace</a> on behalf of <a href="https://trisa.io">TRISA</a>.
                         </Col>
                         <Col className='d-flex'>
-                            <p data-testid="api-version">API version: {appVersion?.version}</p>
+                            {appVersion?.version ? <p data-testid="api-version">API version: {appVersion?.version}</p> : null}
                             {
-                                config.isProd ? (
-                                    <>
-                                        <p data-testid="app-version">App version: {config.appVersion}</p>
-                                        <p data-testid="git-version">GIT version: {config.gitVersion}</p>
-                                    </>
-                                ) : null
+                                <>
+                                    {config.appVersion ? <p data-testid="app-version">&nbsp;&middot;&nbsp;App version: {config.appVersion}</p> : null}
+                                    {config.gitVersion ? <p data-testid="git-version">&nbsp;&middot;&nbsp;GIT version: {config.gitVersion}</p> : null}
+                                </>
                             }
 
                         </Col>
