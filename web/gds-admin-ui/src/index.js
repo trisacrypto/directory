6212a6d0705db28e344a086f08555aee0f71db59
elysee15 <elyseebleu1@gmail.com>
2022-11-14 16:41:05 +0000

    sc-10745 upgrade to react 18 on admin UI

--- web/gds-admin-ui/src/index.js
+++ web/gds-admin-ui/src/index.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
 import './index.css';
 import "nprogress/nprogress.css";
 import App from './App';
-import { Provider } from 'react-redux';
-import { configureStore } from './redux/store';
-
 import reportWebVitals from './reportWebVitals';
 import initSentry from 'sentry';
+import { createRoot } from 'react-dom/client'
 
 
 initSentry()
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={configureStore({})}>
-      <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+const root = createRoot(container)
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
 
 reportWebVitals();
