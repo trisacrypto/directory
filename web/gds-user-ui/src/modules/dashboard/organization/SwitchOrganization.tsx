9e8b95c6d097024435cb034d0149b08d9ba68cc4
Cletus Razakou <masskoder@gmail.com>
2022-12-01 16:01:14 -0500

    sc-11626:  fix spinner issue & refactor switch org component (#927)
    * fix: add 1second timeout to load redirect to dashboard
    
    * clean up
    
    * wip : log
    
    * refact: added useSwitchOrganization hook & added 200 code response case
    
    * minor refact
    
    * refact: remove no content response case

--- web/gds-user-ui/src/modules/dashboard/organization/SwitchOrganization.tsx
+++ web/gds-user-ui/src/modules/dashboard/organization/SwitchOrganization.tsx
@@ -1,67 +1,31 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import React from 'react';
+import { useParams, Navigate } from 'react-router-dom';
 import useQuery from 'hooks/useQuery';
 import TransparentLoader from 'components/Loader/TransparentLoader';
-import { logUserInBff, getUserCurrentOrganizationService } from 'modules/auth/login/auth.service';
-import { refreshNewToken } from 'utils/auth0.helper';
-import { useDispatch } from 'react-redux';
+
 import { useToast, Text } from '@chakra-ui/react';
 import { colors } from 'utils/theme';
-import { setUserOrganization } from 'modules/auth/login/user.slice';
+import { useSwitchOrganization } from './useSwitchOrganization';
 // import { getAuth0User } from 'modules/auth/login/user.slice';
-import { APP_PATH, APP_STATUS_CODE } from 'utils/constants';
+import { APP_PATH } from 'utils/constants';
 const SwitchOrganization: React.FC = () => {
   const toast = useToast();
-  const { id } = useParams<{ id: string }>();
+  const { id } = useParams<{ id: string }>() as any;
   const { vaspName } = useQuery<{ vaspName: string }>();
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const isCalled = useRef(false);
-  const [isError, setIsError] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const { isLoading, isError } = useSwitchOrganization(id);
 
-  useEffect(() => {
-    const switchOrganization = async () => {
-      try {
-        setIsLoading(true);
-        const logged = await logUserInBff({
-          orgid: id
-        });
-        if (logged.status === APP_STATUS_CODE.NO_CONTENT) {
-          const token = (await refreshNewToken()) as any;
-          const user = token && (await getUserCurrentOrganizationService());
-          if (user?.status === APP_STATUS_CODE.OK) {
-            dispatch(setUserOrganization(user?.data));
-            setIsLoading(false);
-            navigate(APP_PATH.DASHBOARD);
-          }
-        }
-      } catch (error) {
-        setIsError(true);
-        toast({
-          title: 'Error',
-          description: 'Something went wrong or Organization not found',
-          status: 'error',
-          duration: 9000,
-          isClosable: true
-        });
-      }
-    };
+  if (isError) {
+    toast({
+      title: 'Organization not found',
+      status: 'error',
+      duration: 5000,
+      isClosable: true,
+      position: 'top-right'
+    });
+    return <Navigate to={APP_PATH.DASHBOARD} />;
+  }
 
-    if (!isCalled.current) {
-      switchOrganization();
-      isCalled.current = true;
-    }
-
-    return () => {
-      isCalled.current = false;
-    };
-  }, [id, navigate, dispatch, toast, vaspName]);
-
-  const renderTitle = () => {
-    if (isError) {
-      return 'Error';
-    }
+  const renderLoadingTitle = () => {
     return (
       <Text as={'span'}>
         Switching to{' '}
@@ -72,7 +36,15 @@ const SwitchOrganization: React.FC = () => {
     );
   };
 
-  return <>{isLoading && !isError && <TransparentLoader title={renderTitle()} opacity="full" />}</>;
+  return (
+    <>
+      {isLoading && !isError ? (
+        <TransparentLoader title={renderLoadingTitle()} opacity="full" />
+      ) : (
+        <Navigate to={APP_PATH.DASHBOARD} />
+      )}
+    </>
+  );
 };
 
 export default SwitchOrganization;
