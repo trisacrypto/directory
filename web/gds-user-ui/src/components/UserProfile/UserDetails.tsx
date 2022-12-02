6254ea6a711046d5089004c2a161c630b277c128
Cletus Razakou <masskoder@gmail.com>
2022-12-02 18:09:57 -0500

    test: should render all users details

--- web/gds-user-ui/src/components/UserProfile/UserDetails.tsx
+++ web/gds-user-ui/src/components/UserProfile/UserDetails.tsx
@@ -4,42 +4,47 @@ import { ProfileBlock } from './index';
 import { useSelector } from 'react-redux';
 import { userSelector } from 'modules/auth/login/user.slice';
 
-export default function UserDetails() {
+const UserDetails: React.FC = () => {
   const { user } = useSelector(userSelector);
+
   return (
-    <ProfileBlock title={<Trans>USER DETAILS</Trans>}>
-      <VStack align="start">
-        <Text fontWeight={700} textTransform="capitalize">
-          <Trans>Profile Created</Trans>
-        </Text>
-        <Text>{user?.createAt || '-'}</Text>
-      </VStack>
-      <VStack align="start">
-        <Text fontWeight={700} textTransform="capitalize">
-          <Trans>Role</Trans>
-        </Text>
-        <Text textTransform="capitalize">
-          <Trans>{user?.role || '-'}</Trans>
-        </Text>
-      </VStack>
-      <VStack align="start">
-        <Text fontWeight={700} textTransform="capitalize">
-          <Trans>Permissions</Trans>
-        </Text>
-        <Text>
-          {user?.permissions.map((permission: string, index: string) => (
-            <Tag key={index} bg={'blue'} color={'white'} ml={1}>
-              {permission}
-            </Tag>
-          ))}
-        </Text>
-      </VStack>
-      <VStack align="start">
-        <Text fontWeight={700} textTransform="capitalize">
-          <Trans>Last Login</Trans>
-        </Text>
-        <Text>{user?.lastLogin}</Text>
-      </VStack>
-    </ProfileBlock>
+    <>
+      <ProfileBlock title={<Trans>USER DETAILS</Trans>}>
+        <VStack align="start">
+          <Text fontWeight={700} textTransform="capitalize">
+            <Trans>Profile Created</Trans>
+          </Text>
+          <Text data-testid="user_created_At">{user?.createAt || '-'}</Text>
+        </VStack>
+        <VStack align="start">
+          <Text fontWeight={700} textTransform="capitalize">
+            <Trans>Role</Trans>
+          </Text>
+          <Text textTransform="capitalize" data-testid="user_role">
+            <Trans>{user?.role || '-'}</Trans>
+          </Text>
+        </VStack>
+        <VStack align="start">
+          <Text fontWeight={700} textTransform="capitalize">
+            <Trans>Permissions</Trans>
+          </Text>
+          <Text>
+            {user?.permissions.map((permission: string, index: string) => (
+              <Tag key={index} bg={'blue'} color={'white'} ml={1} data-testid={`user_permissions`}>
+                {permission}
+              </Tag>
+            ))}
+          </Text>
+        </VStack>
+        <VStack align="start">
+          <Text fontWeight={700} textTransform="capitalize">
+            <Trans>Last Login</Trans>
+          </Text>
+          <Text data-testid="user_last_login">{user?.lastLogin}</Text>
+        </VStack>
+      </ProfileBlock>
+    </>
   );
-}
+};
+
+export default UserDetails;
---
+++ web/gds-user-ui/src/components/UserProfile/__tests__/UserDetails.spec.tsx
@@ -0,0 +1,45 @@
+import { dynamicActivate } from 'utils/i18nLoaderHelper';
+import { act, render, screen } from 'utils/test-utils';
+import UserDetails from '../UserDetails';
+
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn().mockReturnValueOnce({
+    user: {
+      createAt: '2021-05-05T12:00:00.000Z',
+      role: 'admin',
+      permissions: ['admin', 'user'],
+      lastLogin: '2021-05-05T12:00:00.000Z'
+    }
+  })
+}));
+
+describe('<UserDetails />  ', () => {
+  beforeAll(() => {
+    act(() => {
+      dynamicActivate('en');
+    });
+  });
+
+  it('should render user details', () => {
+    const userDetailsMock = {
+      createAt: '2021-05-05T12:00:00.000Z',
+      role: 'admin',
+      permissions: ['admin', 'user'],
+      lastLogin: '2021-05-05T12:00:00.000Z'
+    };
+
+    render(<UserDetails />);
+
+    expect(screen.getByTestId('user_created_At').textContent).toBe(userDetailsMock.createAt);
+    expect(screen.getByTestId('user_role').textContent).toBe(userDetailsMock.role);
+    expect(screen.getByTestId('user_last_login').textContent).toBe(userDetailsMock.lastLogin);
+    // get all permissions by index and check if they are in the array
+    expect(screen.getAllByTestId('user_permissions')[0].textContent).toBe(
+      userDetailsMock.permissions[0]
+    );
+  });
+  // afterEach(() => {
+  //   useSelector.mockClear();
+  // });
+});
