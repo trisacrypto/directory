fd6e578500794decf6a7d522256e5f5fedd2d0b6
Benjamin Bengfort <benjamin@bengfort.com>
2022-12-07 10:20:44 -0600

    towards add collaborator feature

--- cmd/bffutil/main.go
+++ cmd/bffutil/main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/auth0/go-auth0/management"
 	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 	"github.com/trisacrypto/directory/pkg"
+	"github.com/trisacrypto/directory/pkg/bff/auth"
 	"github.com/trisacrypto/directory/pkg/bff/config"
 	"github.com/trisacrypto/directory/pkg/bff/models/v1"
 	"github.com/trisacrypto/directory/pkg/store"
@@ -17,8 +19,9 @@ import (
 )
 
 var (
-	db   store.Store
-	conf config.Config
+	db    store.Store
+	auth0 *management.Management
+	conf  config.Config
 )
 
 func main() {
@@ -32,16 +35,40 @@ func main() {
 	app.Flags = []cli.Flag{}
 	app.Commands = []*cli.Command{
 		{
-			Name:   "orgs",
-			Usage:  "list and view organization report",
-			Action: organizations,
+			Name:   "orgs:list",
+			Usage:  "list a summary of all organizations in the bff database",
+			Action: listOrgs,
 			Before: connectDB,
+			After:  closeDB,
+			Flags:  []cli.Flag{},
+		},
+		{
+			Name:      "orgs:detail",
+			Usage:     "list a summary of all organizations in the bff database",
+			Action:    detailOrgs,
+			ArgsUsage: "orgID [orgID ...]",
+			Before:    connectDB,
+			After:     closeDB,
+			Flags:     []cli.Flag{},
+		},
+		{
+			Name:   "collabs:add",
+			Usage:  "add a collaborator to an organization",
+			Action: addCollab,
+			Before: Before(loadConf, connectDB, connectAuth0),
 			After:  closeDB,
 			Flags: []cli.Flag{
 				&cli.StringFlag{
-					Name:    "id",
-					Aliases: []string{"i"},
-					Usage:   "specify an organization to get more detailed information for",
+					Name:     "org",
+					Aliases:  []string{"o"},
+					Usage:    "specify the organization id to add the collaborator to",
+					Required: true,
+				},
+				&cli.StringFlag{
+					Name:     "user",
+					Aliases:  []string{"u"},
+					Usage:    "specify the auth0 id of the user to make a collaborator",
+					Required: true,
 				},
 			},
 		},
@@ -53,7 +80,11 @@ func main() {
 	}
 }
 
-func connectDB(c *cli.Context) (err error) {
+//===========================================================================
+// Before/After CLI Commands
+//===========================================================================
+
+func loadConf(c *cli.Context) (err error) {
 	// suppress zerolog output from the store
 	logger.Discard()
 
@@ -63,6 +94,15 @@ func connectDB(c *cli.Context) (err error) {
 	}
 	conf.Database.ReindexOnBoot = false
 	conf.ConsoleLog = false
+	return nil
+}
+
+func connectDB(c *cli.Context) (err error) {
+	if conf.IsZero() {
+		if err = loadConf(c); err != nil {
+			return err
+		}
+	}
 
 	// Connect to the BFF main database
 	// TODO: do we need to connect to the mainnet and testnet databases?
@@ -76,6 +116,19 @@ func connectDB(c *cli.Context) (err error) {
 	return nil
 }
 
+func connectAuth0(c *cli.Context) (err error) {
+	if conf.IsZero() {
+		if err = loadConf(c); err != nil {
+			return err
+		}
+	}
+
+	if auth0, err = auth.NewManagementClient(conf.Auth0); err != nil {
+		return cli.Exit(err, 1)
+	}
+	return nil
+}
+
 func closeDB(c *cli.Context) (err error) {
 	if err = db.Close(); err != nil {
 		return cli.Exit(err, 1)
@@ -83,21 +136,11 @@ func closeDB(c *cli.Context) (err error) {
 	return nil
 }
 
-func organizations(c *cli.Context) (err error) {
-	if id := c.String("id"); id != "" {
-		// Print organization detail rather than list organizations
-		var orgID uuid.UUID
-		if orgID, err = models.ParseOrgID(id); err != nil {
-			return cli.Exit(err, 1)
-		}
+//===========================================================================
+// CLI Commands
+//===========================================================================
 
-		var org *models.Organization
-		if org, err = db.RetrieveOrganization(orgID); err != nil {
-			return cli.Exit(err, 1)
-		}
-		return printJSON(org)
-	}
-
+func listOrgs(c *cli.Context) (err error) {
 	// Create organizations report
 	orgs := make([]map[string]interface{}, 0)
 	iter := db.ListOrganizations()
@@ -126,6 +169,42 @@ func organizations(c *cli.Context) (err error) {
 	return printJSON(orgs)
 }
 
+func detailOrgs(c *cli.Context) (err error) {
+	if c.NArg() == 0 {
+		return cli.Exit("specify at least one organization ID", 1)
+	}
+
+	orgs := make([]*models.Organization, 0, c.NArg())
+	for i := 0; i < c.NArg(); i++ {
+		var org *models.Organization
+		if org, err = GetOrg(c.Args().Get(i)); err != nil {
+			return cli.Exit(err, 1)
+		}
+		orgs = append(orgs, org)
+	}
+
+	if len(orgs) == 1 {
+		return printJSON(orgs[0])
+	}
+	return printJSON(orgs)
+}
+
+func addCollab(c *cli.Context) (err error) {
+	var org *models.Organization
+	if org, err = GetOrg(c.String("org")); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	var user *management.User
+	if user, err = auth0.User.Read(c.String("user")); err != nil {
+		return cli.Exit(err, 1)
+	}
+
+	fmt.Println(org.ResolveName())
+	fmt.Println(auth.UserDisplayName(user))
+	return nil
+}
+
 //===========================================================================
 // Helper Functions
 //===========================================================================
@@ -139,4 +218,28 @@ func printJSON(msg interface{}) (err error) {
 	}
 	fmt.Println("")
 	return nil
+}
+
+func GetOrg(id string) (_ *models.Organization, err error) {
+	var orgID uuid.UUID
+	if orgID, err = models.ParseOrgID(id); err != nil {
+		return nil, err
+	}
+
+	var org *models.Organization
+	if org, err = db.RetrieveOrganization(orgID); err != nil {
+		return nil, err
+	}
+	return org, nil
+}
+
+func Before(funcs ...cli.BeforeFunc) cli.BeforeFunc {
+	return func(c *cli.Context) error {
+		for _, f := range funcs {
+			if err := f(c); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
 }
