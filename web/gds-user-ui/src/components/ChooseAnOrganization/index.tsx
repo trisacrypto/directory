0fcdb8024d1825c917da85368fb42f5b0b1731a0
Cletus Razakou <masskoder@gmail.com>
2022-12-22 16:12:03 -0500

    Sc 11879: VASP Modal Tab navigation refactoring (#965)
    * refact: improve transparentbackground
    
    * test: update test implementation
    
    * refactor: improve choose organization component
    
    * style: add overflow to vasp list

--- web/gds-user-ui/src/components/ChooseAnOrganization/index.tsx
+++ web/gds-user-ui/src/components/ChooseAnOrganization/index.tsx
@@ -1,4 +1,4 @@
-import { HStack, IconButton, Stack, StackDivider, Text, VStack } from '@chakra-ui/react';
+import { HStack, IconButton, Stack, StackDivider, Text, VStack, css } from '@chakra-ui/react';
 import { Account } from 'components/Account';
 import AddNewVaspModal from 'components/AddNewVaspModal/AddNewVaspModal';
 import { Trans } from '@lingui/macro';
@@ -6,17 +6,33 @@ import { useOrganizationListQuery } from 'modules/dashboard/organization/useOrga
 import { userSelector } from 'modules/auth/login/user.slice';
 import { useSelector } from 'react-redux';
 import { GrClose } from 'react-icons/gr';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import React from 'react';
 
+// import { TransparentBackground } from 'components/TransparentBackground';
 function ChooseAnOrganization() {
   const { organizations } = useOrganizationListQuery();
   const { user } = useSelector(userSelector);
+  const navigate = useNavigate();
+  const handleBack = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    navigate(-1);
+  };
 
   return (
-    <VStack spacing={3} mx="auto" h="100vh" pt="10vh" position="relative">
+    <VStack
+      position="absolute"
+      top="0"
+      left="0"
+      w="100%"
+      h="100%"
+      bg={'rgba(255,255,255,255)'}
+      spacing={3}
+      width={'full'}
+      mx="auto"
+      pt="10vh">
       <IconButton
-        as={Link}
-        to="/"
+        onClick={handleBack}
         icon={<GrClose />}
         aria-label="Get back to dashboard"
         position="absolute"
@@ -25,7 +41,14 @@ function ChooseAnOrganization() {
         variant="ghost"
         title="Get back to dashboard"
       />
-      <Stack maxW="700px" mx="auto">
+      <Stack
+        width={'50%'}
+        mx="auto"
+        overflowY={'scroll'}
+        css={css({
+          boxShadow: 'inset 0 -2px 0 rgba(0, 0, 0, 0.1)',
+          border: '0 none'
+        })}>
         <div>
           <HStack width="100%" justifyContent="end">
             <AddNewVaspModal />
@@ -34,20 +57,23 @@ function ChooseAnOrganization() {
             <Trans>Select a VASP from the Managed VASP List</Trans>
           </Text>
         </div>
-        <Stack divider={<StackDivider borderColor="#D9D9D9" />} p={2}>
-          {organizations && organizations?.length > 0 ? (
-            organizations?.map((organization) => (
-              <Account
-                key={organization.id}
-                id={organization.id}
-                name={organization?.name}
-                domain={organization?.domain}
-                isCurrent={organization.id === user?.vasp?.id}
-              />
-            ))
-          ) : (
-            <Text>No VASPs found</Text>
-          )}
+
+        <Stack>
+          <Stack divider={<StackDivider borderColor="#D9D9D9" />} p={2}>
+            {organizations && organizations?.length > 0 ? (
+              organizations?.map((organization) => (
+                <Account
+                  key={organization.id}
+                  id={organization.id}
+                  name={organization?.name}
+                  domain={organization?.domain}
+                  isCurrent={organization.id === user?.vasp?.id}
+                />
+              ))
+            ) : (
+              <Text>No VASPs found</Text>
+            )}
+          </Stack>
         </Stack>
       </Stack>
     </VStack>
