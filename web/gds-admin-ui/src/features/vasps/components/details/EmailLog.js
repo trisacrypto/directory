7bd088fb248e50e2c914512af1b628da9443585a
elysee15 <elyseebleu1@gmail.com>
2023-01-11 03:25:10 +0000

    sc-9368 Display email log in admin UI

--- web/gds-admin-ui/src/features/vasps/components/details/EmailLog.js
+++ web/gds-admin-ui/src/features/vasps/components/details/EmailLog.js
@@ -1,12 +1,12 @@
-import { Card } from "react-bootstrap";
+import { Card } from 'react-bootstrap';
 import SimpleBar from 'simplebar-react';
-import TimelineItem from "@/components/TimelineItem";
-import Timeline from '@/components/Timeline'
-import dayjs from "dayjs";
+import TimelineItem from '@/components/TimelineItem';
+import Timeline from '@/components/Timeline';
+import dayjs from 'dayjs';
 import PropTypes from 'prop-types';
 
 function sortByTimestamp(data = []) {
-    return data.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime())
+    return data.sort((a, b) => new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime());
 }
 
 const EmailLog = ({ data }) => {
@@ -15,42 +15,48 @@ const EmailLog = ({ data }) => {
             <Card.Body>
                 <h4 className="header-title mb-2">Email log</h4>
 
-                {
-                    Array.isArray(data) && data.length ? (
-
-                        <SimpleBar style={{ maxHeight: '330px', width: '100%' }}>
-                            <Timeline>
-                                {
-                                    sortByTimestamp(data).map((log, idx) => (
-                                        <TimelineItem key={idx}>
-                                            <i className="mdi mdi-file-search-outline bg-info-lighten text-info timeline-icon"></i>
-                                            <div className="timeline-item-info mb-3">
-                                                <h5 className="text-primary mb-1" data-testid="email-log-reason" href="/">{log.reason}</h5>
-                                                <small className="d-block" data-testid="email-log-contact-line">
-                                                    contact: <span className="fst-italic" data-testid="email-log-contact">{log.contact}</span>
-                                                </small>
-                                                <small data-testid="email-log-subject-line">
-                                                    subject: <span className="fst-italic" data-testid="email-log-subject">{log.subject}</span>
-                                                </small>
-                                                <small className="d-block" data-testid="email-log-timestamp">{dayjs(log?.timestamp).toLocaleString()}</small>
-                                            </div>
-                                        </TimelineItem>
-                                    ))
-                                }
-
-                            </Timeline>
-                        </SimpleBar>
-                    ) : (
-                        <p className="fst-italic fs-6" data-testid="no-data">No Email Log</p>
-                    )
-                }
+                {Array.isArray(data) && data.length ? (
+                    <SimpleBar style={{ maxHeight: '330px', width: '100%' }}>
+                        <Timeline>
+                            {sortByTimestamp(data).map((log, idx) => (
+                                <TimelineItem key={idx}>
+                                    <i className="mdi mdi-file-search-outline bg-info-lighten text-info timeline-icon"></i>
+                                    <div className="timeline-item-info mb-3">
+                                        <h5 className="text-primary mb-1" data-testid="email-log-reason" href="/">
+                                            {log?.reason || 'N/A'}
+                                        </h5>
+                                        <small className="d-block" data-testid="email-log-contact-line">
+                                            Recipient:{' '}
+                                            <span className="fst-italic" data-testid="email-log-contact">
+                                                {log?.recipient || 'No recipient'}
+                                            </span>
+                                        </small>
+                                        <small data-testid="email-log-subject-line">
+                                            Subject:{' '}
+                                            <span className="fst-italic" data-testid="email-log-subject">
+                                                {log?.subject || 'No subject'}
+                                            </span>
+                                        </small>
+                                        <small className="d-block" data-testid="email-log-timestamp">
+                                            Date: {dayjs(log?.timestamp).toLocaleString()}
+                                        </small>
+                                    </div>
+                                </TimelineItem>
+                            ))}
+                        </Timeline>
+                    </SimpleBar>
+                ) : (
+                    <p className="fst-italic fs-6" data-testid="no-data">
+                        No Email Log
+                    </p>
+                )}
             </Card.Body>
         </Card>
     ) : null;
 };
 
 EmailLog.propTypes = {
-    data: PropTypes.arrayOf(PropTypes.object)
-}
+    data: PropTypes.arrayOf(PropTypes.object),
+};
 
 export default EmailLog;
--- web/gds-admin-ui/src/features/vasps/components/details/__tests__/email-log.spec.js
+++ web/gds-admin-ui/src/features/vasps/components/details/__tests__/email-log.spec.js
@@ -3,39 +3,56 @@ import { render, screen } from '@testing-library/react';
 import EmailLog from '../EmailLog';
 
 describe('EmailLogo', () => {
-  let emailLogMock;
-  it('Should show email log data', () => {
-    emailLogMock = [
-      {
-        contact: 'technical',
-        reason: 'verify_contact',
-        subject: 'TRISA: Please verify your email address',
-        timestamp: '2021-11-08T20:28:36Z',
-      },
-    ];
+    let emailLogMock;
+    it('Should display email log data', () => {
+        emailLogMock = [
+            {
+                recipient: 'technical',
+                reason: 'verify_contact',
+                subject: 'TRISA: Please verify your email address',
+                timestamp: '2021-11-08T20:28:36Z',
+            },
+        ];
 
-    const { container } = render(<EmailLog data={emailLogMock} />);
+        const { container } = render(<EmailLog data={emailLogMock} />);
 
-    expect(container.childNodes.length).toEqual(1);
+        expect(container.childNodes.length).toEqual(1);
 
-    expect(screen.getByTestId('email-log-subject').textContent).toBe(emailLogMock[0].subject);
-    expect(screen.getByTestId('email-log-subject-line').textContent).toBe(
-      'subject: TRISA: Please verify your email address'
-    );
-    expect(screen.getByTestId('email-log-reason').textContent).toBe(emailLogMock[0].reason);
-    expect(screen.getByTestId('email-log-contact').textContent).toBe(emailLogMock[0].contact);
-    expect(screen.getByTestId('email-log-contact-line').textContent).toBe('contact: technical');
-    expect(screen.getByTestId('email-log-timestamp').textContent).toBe(
-      'Mon, 08 Nov 2021 20:28:36 GMT'
-    );
-  });
+        expect(screen.getByTestId('email-log-subject').textContent).toBe(emailLogMock[0].subject);
+        expect(screen.getByTestId('email-log-subject-line').textContent).toBe(
+            'Subject: TRISA: Please verify your email address'
+        );
+        expect(screen.getByTestId('email-log-reason').textContent).toBe(emailLogMock[0].reason);
+        expect(screen.getByTestId('email-log-contact').textContent).toBe(emailLogMock[0].recipient);
+        expect(screen.getByTestId('email-log-contact-line').textContent).toBe('Recipient: technical');
+        expect(screen.getByTestId('email-log-timestamp').textContent).toBe('Date: Mon, 08 Nov 2021 20:28:36 GMT');
+    });
 
-  it('should render no data when there are no email log', () => {
-    emailLogMock = [];
-    render(<EmailLog data={emailLogMock} />);
+    it('should display fallback text', () => {
+        emailLogMock = [
+            {
+                recipient: null,
+                reason: null,
+                subject: null,
+                timestamp: '2021-11-08T20:28:36Z',
+            },
+        ];
 
-    const noData = screen.getByTestId('no-data');
-    expect(noData).toBeInTheDocument();
-    expect(noData.textContent).toBe('No Email Log');
-  });
+        const { container } = render(<EmailLog data={emailLogMock} />);
+
+        expect(container.childNodes.length).toEqual(1);
+
+        expect(screen.getByTestId('email-log-subject').textContent).toBe('No subject');
+        expect(screen.getByTestId('email-log-reason').textContent).toBe('N/A');
+        expect(screen.getByTestId('email-log-contact').textContent).toBe('No recipient');
+    });
+
+    it('should render no data when there are no email log', () => {
+        emailLogMock = [];
+        render(<EmailLog data={emailLogMock} />);
+
+        const noData = screen.getByTestId('no-data');
+        expect(noData).toBeInTheDocument();
+        expect(noData.textContent).toBe('No Email Log');
+    });
 });
