a8a98dae4764cc020f2acab51c62e580062c4d8b
Benjamin Bengfort <benjamin@bengfort.com>
2023-02-17 12:16:14 -0600

    increase security of token generation

--- pkg/gds/secrets/token.go
+++ pkg/gds/secrets/token.go
@@ -1,36 +1,74 @@
 package secrets
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"encoding/binary"
+	"fmt"
 	"strings"
-	"time"
 )
 
-var (
-	chars        = []rune("ABCDEFGHIJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz1234567890")
-	specialChars = []rune("#%&()*+-<=>?@[]^_{}")
+const (
+	alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	alphanum = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+	idxbits  = 6
+	idxmask  = 1<<idxbits - 1
+	idxmax   = 63 / idxbits
 )
 
 // CreateToken creates a variable length random token that can be used for passwords or API keys.
 func CreateToken(length int) string {
-	random := rand.New(rand.NewSource(time.Now().UnixNano()))
-	var b strings.Builder
-	for i := 0; i < length; i++ {
-		if random.Float64() <= 0.90 {
-			b.WriteRune(chars[random.Intn(len(chars))])
-		} else {
-			b.WriteRune(specialChars[random.Intn(len(specialChars))])
-		}
-	}
-	return b.String()
+	return generate(length, alphanum)
 }
 
-// ValidateToken checks if a token contains any invalid characters.
-func ValidateToken(token string) bool {
-	for _, c := range token {
-		if !strings.ContainsRune(string(chars), c) && !strings.ContainsRune(string(specialChars), c) {
-			return false
-		}
+// Alpha generates a random string of n characters that only includes upper and
+// lowercase letters (no symbols or digits).
+func Alpha(n int) string {
+	return generate(n, alphabet)
+}
+
+// AlphaNumeric generates a random string of n characters that includes upper and
+// lowercase letters and the digits 0-9.
+func AlphaNumeric(n int) string {
+	return generate(n, alphanum)
+}
+
+// generate is a helper function to create a random string of n characters from the
+// character set defined by chars. It uses as efficient a method of generation as
+// possible, using a string builder to prevent multiple allocations and a 6 bit mask
+// to select 10 random letters at a time to add to the string. This method would be far
+// faster if it used math/rand src and the Int63() function, but for API key generation
+// it is important to use a cryptographically random generator.
+//
+// See: https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
+func generate(n int, chars string) string {
+	if n <= 0 {
+		return ""
 	}
-	return true
+
+	sb := strings.Builder{}
+	sb.Grow(n)
+
+	for i, cache, remain := n-1, CryptoRandInt(), idxmax; i >= 0; {
+		if remain == 0 {
+			cache, remain = CryptoRandInt(), idxmax
+		}
+
+		if idx := int(cache & idxmask); idx < len(chars) {
+			sb.WriteByte(chars[idx])
+			i--
+		}
+
+		cache >>= idxbits
+		remain--
+	}
+
+	return sb.String()
+}
+
+func CryptoRandInt() uint64 {
+	buf := make([]byte, 8)
+	if _, err := rand.Read(buf); err != nil {
+		panic(fmt.Errorf("cannot generate random number: %w", err))
+	}
+	return binary.BigEndian.Uint64(buf)
 }
--- pkg/gds/secrets/token_test.go
+++ pkg/gds/secrets/token_test.go
@@ -1,40 +1,63 @@
 package secrets_test
 
 import (
+	"math/rand"
+	"regexp"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 	"github.com/trisacrypto/directory/pkg/gds/secrets"
 )
 
-// Test that the CreateToken function creates a valid token of the given length.
 func TestCreateToken(t *testing.T) {
-	// Negative or zero length should return an empty string
-	require.Equal(t, "", secrets.CreateToken(-1))
-	require.Equal(t, "", secrets.CreateToken(0))
-
-	// Returned token should not contain any unexpected characters
-	token := secrets.CreateToken(20)
-	require.True(t, secrets.ValidateToken(token), "token %s contains invalid characters", token)
-
-	// Successive calls should return different tokens
-	nextToken := secrets.CreateToken(20)
-	require.True(t, secrets.ValidateToken(nextToken), "token %s contains invalid characters", nextToken)
-	require.NotEqual(t, token, nextToken, "CreateToken returned the same token twice: %s", token)
+	vals := make(map[string]struct{})
+	for i := 0; i < 5000; i++ {
+		val := secrets.CreateToken(8)
+		require.Len(t, val, 8)
+		vals[val] = struct{}{}
+	}
+	require.Len(t, vals, 5000, "there is a very small chance that a duplicate value is generated")
 }
 
-// Test that the ValidateToken function can determine if a token contains invalid
-// characters.
-func TestValidateToken(t *testing.T) {
-	// Valid tokens
-	require.True(t, secrets.ValidateToken(""))
-	require.True(t, secrets.ValidateToken("abcABC12345"))
-	require.True(t, secrets.ValidateToken("abc123#+-{}"))
-	require.True(t, secrets.ValidateToken("()&*[]#"))
-	require.False(t, secrets.ValidateToken("abc12345!"))
+func TestAlpha(t *testing.T) {
+	// Test creating different random strings at different lengths
+	for i := 0; i < 10000; i++ {
+		len := rand.Intn(512) + 1
+		alpha := secrets.Alpha(len)
+		require.Len(t, alpha, len)
+		require.Regexp(t, regexp.MustCompile(`[a-zA-Z]+`), alpha)
+	}
 
-	// Invalid tokens
-	require.False(t, secrets.ValidateToken("abc12345\n"))
-	require.False(t, secrets.ValidateToken("`~\t"))
-	require.False(t, secrets.ValidateToken(",."))
+	vals := make(map[string]struct{})
+	for i := 0; i < 10000; i++ {
+		val := secrets.Alpha(16)
+		vals[val] = struct{}{}
+	}
+	require.Len(t, vals, 10000, "there is a very low chance that a duplicate value was generated")
+}
+
+func TestAlphaNumeric(t *testing.T) {
+	// Test creating different random strings at different lengths
+	for i := 0; i < 10000; i++ {
+		len := rand.Intn(512) + 1
+		alpha := secrets.AlphaNumeric(len)
+		require.Len(t, alpha, len)
+		require.Regexp(t, regexp.MustCompile(`[a-zA-Z0-9]+`), alpha)
+	}
+
+	vals := make(map[string]struct{})
+	for i := 0; i < 10000; i++ {
+		val := secrets.AlphaNumeric(16)
+		vals[val] = struct{}{}
+	}
+	require.Len(t, vals, 10000, "there is a very low chance that a duplicate value was generated")
+}
+
+func TestCryptoRandInt(t *testing.T) {
+	nums := make(map[uint64]struct{})
+	for i := 0; i < 10000; i++ {
+		val := secrets.CryptoRandInt()
+		nums[val] = struct{}{}
+	}
+	require.Len(t, nums, 10000, "there is a very low chance that a duplicate value was generated")
 }
