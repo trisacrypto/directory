adb6520d2b4bc4a0f845e53acba391cecdeff105
daniellemaxwell <danielle@rotational.io>
2023-05-23 14:31:57 -0400

    Updates cypress version and adds cucumber

---
+++ web/gds-user-ui/cypress.config.ts
@@ -0,0 +1,43 @@
+import * as browserify from '@cypress/browserify-preprocessor';
+import { defineConfig } from 'cypress';
+import cucumber from 'cypress-cucumber-preprocessor';
+
+export default defineConfig({
+  numTestsKeptInMemory: 0,
+  env: {
+    uncaughtCypressException: false,
+    hideXhr: true,
+  },
+  chromeWebSecurity: false,
+  retries: {
+    runMode: 1,
+    openMode: 0,  
+  },
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    setupNodeEvents(on, config) {
+      const options = {
+        ...browserify.defaultOptions,
+        typescript: require.resolve('typescript'),
+      };
+
+      on('file:preprocessor', cucumber(options));
+
+      return config;
+    },
+    specPattern: 'cypress/e2e/**/*.{feature,features}',
+    env: {
+      API_URL: 'http://localhost:8080/v1',
+    },
+  },
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: ['mochawesome'],
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/results',
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+  },
+});
