056de80cbfab03d228a9f2f384a3583212ef4693
Patrick Deziel <42919891+pdeziel@users.noreply.github.com>
2023-05-23 13:59:47 -0500

    Contacts validation step 3 (#1017)

--- pkg/bff/models/v1/registration_test.go
+++ pkg/bff/models/v1/registration_test.go
@@ -2,7 +2,6 @@ package models_test
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"strings"
 	"testing"
@@ -56,34 +55,6 @@ func TestStepType(t *testing.T) {
 		require.Equal(t, models.StepNone, actual)
 	}
 
-}
-
-// Test that the registration form marshals and unmarshals correctly to and from JSON
-func TestMarshalRegistrationForm(t *testing.T) {
-	// Load the JSON fixture
-	fixtureData, err := os.ReadFile("testdata/default_registration_form.json")
-	require.NoError(t, err, "error reading default registration form fixture")
-
-	// Default form should be marshaled correctly
-	form := models.NewRegisterForm()
-	data, err := json.Marshal(form)
-	require.NoError(t, err, "error marshaling registration form to JSON")
-	require.JSONEq(t, string(fixtureData), string(data), "default registration form does not match fixture")
-
-	// Default form should be unmarshaled correctly
-	result := &models.RegistrationForm{}
-	require.NoError(t, json.Unmarshal(data, result), "error unmarshaling registration form from JSON")
-	require.True(t, proto.Equal(form, result), "registration form should be unmarshaled correctly")
-
-	// Modified form should be marshaled correctly
-	form.Contacts.Administrative.Email = "admin@example.com"
-	data, err = json.Marshal(form)
-	require.NoError(t, err, "error marshaling registration form to JSON")
-
-	// Modified form should be unmarshaled correctly
-	result = &models.RegistrationForm{}
-	require.NoError(t, json.Unmarshal(data, result), "error unmarshaling registration form from JSON")
-	require.True(t, proto.Equal(form, result), "registration form should be unmarshaled correctly")
 }
 
 // Test validating the basic details step of the registration form
@@ -144,10 +115,179 @@ func TestValidateBasicDetails(t *testing.T) {
 		form.OrganizationName = tc.orgName
 
 		verrs := form.ValidateBasicDetails()
-		fmt.Println(tc)
 		require.Equal(t, tc.errs, verrs)
 
 		errs := form.Validate(models.StepBasicDetails)
+		require.Equal(t, tc.errs, errs)
+	}
+}
+
+// Test that the registration form marshals and unmarshals correctly to and from JSON
+func TestMarshalRegistrationForm(t *testing.T) {
+	// Load the JSON fixture
+	fixtureData, err := os.ReadFile("testdata/default_registration_form.json")
+	require.NoError(t, err, "error reading default registration form fixture")
+
+	// Default form should be marshaled correctly
+	form := models.NewRegisterForm()
+	data, err := json.Marshal(form)
+	require.NoError(t, err, "error marshaling registration form to JSON")
+	require.JSONEq(t, string(fixtureData), string(data), "default registration form does not match fixture")
+
+	// Default form should be unmarshaled correctly
+	result := &models.RegistrationForm{}
+	require.NoError(t, json.Unmarshal(data, result), "error unmarshaling registration form from JSON")
+	require.True(t, proto.Equal(form, result), "registration form should be unmarshaled correctly")
+
+	// Modified form should be marshaled correctly
+	form.Contacts.Administrative.Email = "admin@example.com"
+	data, err = json.Marshal(form)
+	require.NoError(t, err, "error marshaling registration form to JSON")
+
+	// Modified form should be unmarshaled correctly
+	result = &models.RegistrationForm{}
+	require.NoError(t, json.Unmarshal(data, result), "error unmarshaling registration form from JSON")
+	require.True(t, proto.Equal(form, result), "registration form should be unmarshaled correctly")
+}
+
+// Test validating the contacts step of the registration form
+func TestValidateContacts(t *testing.T) {
+	// A single error should be returned for nil contacts
+	form := models.RegistrationForm{}
+	expected := models.ValidationErrors{
+		{Field: "contacts", Err: models.ErrMissingField.Error()},
+	}
+	verrs := form.ValidateContacts()
+	require.Equal(t, expected, verrs, "expected a single error for missing contacts field")
+
+	errs := form.Validate(models.StepContacts)
+	require.Equal(t, expected, errs, "expected a single error for missing contacts field")
+
+	contact := &pb.Contact{
+		Name:  "Leopold Wentzel",
+		Email: "leopold.wentzel@gmail.com",
+		Phone: "555-867-5309",
+	}
+
+	missingEmail := &pb.Contact{
+		Name:  contact.Name,
+		Phone: contact.Phone,
+	}
+
+	// Test that contacts are valided if not empty
+	testCases := []struct {
+		technical *pb.Contact
+		admin     *pb.Contact
+		legal     *pb.Contact
+		billing   *pb.Contact
+		errs      models.ValidationErrors
+	}{
+		// No contacts provided
+		{nil, nil, &pb.Contact{}, nil, models.ValidationErrors{
+			{Field: "contacts", Err: models.ErrNoContacts.Error()},
+		}},
+		// Only technical provided should nominate admin/legal contact to be populated
+		{contact, nil, nil, nil, models.ValidationErrors{
+			{Field: "contacts", Err: models.ErrMissingContact.Error()},
+			{Field: "contacts.administrative", Err: models.ErrMissingAdminOrLegal.Error()},
+			{Field: "contacts.legal", Err: models.ErrMissingAdminOrLegal.Error()},
+		}},
+		// Only legal provided should nominate admin/technical contact to be populated
+		{nil, nil, contact, nil, models.ValidationErrors{
+			{Field: "contacts", Err: models.ErrMissingContact.Error()},
+			{Field: "contacts.administrative", Err: models.ErrMissingAdminOrTechnical.Error()},
+			{Field: "contacts.technical", Err: models.ErrMissingAdminOrTechnical.Error()},
+		}},
+		// Only billing provided should nominate admin/technical/legal contact to be populated
+		{nil, nil, nil, contact, models.ValidationErrors{
+			{Field: "contacts", Err: models.ErrMissingContact.Error()},
+			{Field: "contacts.administrative", Err: models.ErrMissingContact.Error()},
+			{Field: "contacts.technical", Err: models.ErrMissingContact.Error()},
+			{Field: "contacts.legal", Err: models.ErrMissingContact.Error()},
+		}},
+		// Legal and billing provided should nominate admin/technical contact to be populated
+		{nil, nil, contact, contact, models.ValidationErrors{
+			{Field: "contacts", Err: models.ErrMissingContact.Error()},
+			{Field: "contacts.administrative", Err: models.ErrMissingAdminOrTechnical.Error()},
+			{Field: "contacts.technical", Err: models.ErrMissingAdminOrTechnical.Error()},
+		}},
+		// Technical and billing provided should nominate admin/legal contact to be populated
+		{contact, nil, nil, contact, models.ValidationErrors{
+			{Field: "contacts", Err: models.ErrMissingContact.Error()},
+			{Field: "contacts.administrative", Err: models.ErrMissingAdminOrLegal.Error()},
+			{Field: "contacts.legal", Err: models.ErrMissingAdminOrLegal.Error()},
+		}},
+		{missingEmail, contact, contact, contact, models.ValidationErrors{
+			{Field: "contacts.technical.email", Err: models.ErrMissingField.Error()},
+		}},
+		{missingEmail, missingEmail, contact, contact, models.ValidationErrors{
+			{Field: "contacts.technical.email", Err: models.ErrMissingField.Error()},
+			{Field: "contacts.administrative.email", Err: models.ErrMissingField.Error()},
+		}},
+		{missingEmail, missingEmail, missingEmail, contact, models.ValidationErrors{
+			{Field: "contacts.technical.email", Err: models.ErrMissingField.Error()},
+			{Field: "contacts.administrative.email", Err: models.ErrMissingField.Error()},
+			{Field: "contacts.legal.email", Err: models.ErrMissingField.Error()},
+		}},
+		{missingEmail, missingEmail, missingEmail, missingEmail, models.ValidationErrors{
+			{Field: "contacts.technical.email", Err: models.ErrMissingField.Error()},
+			{Field: "contacts.administrative.email", Err: models.ErrMissingField.Error()},
+			{Field: "contacts.legal.email", Err: models.ErrMissingField.Error()},
+			{Field: "contacts.billing.email", Err: models.ErrMissingField.Error()},
+		}},
+		// Only admin provided is valid
+		{nil, contact, nil, nil, nil},
+		// Admin and billing provided is valid
+		{nil, contact, nil, contact, nil},
+		// Technical and legal provided is valid
+		{contact, contact, contact, contact, nil},
+	}
+
+	for _, tc := range testCases {
+		form := models.RegistrationForm{
+			Contacts: &pb.Contacts{
+				Technical:      tc.technical,
+				Administrative: tc.admin,
+				Legal:          tc.legal,
+				Billing:        tc.billing,
+			},
+		}
+
+		verrs := form.ValidateContacts()
+		require.Equal(t, tc.errs, verrs)
+
+		errs := form.Validate(models.StepContacts)
+		require.Equal(t, tc.errs, errs)
+	}
+}
+
+// Test validating a single contact
+func TestValidateContact(t *testing.T) {
+	testCases := []struct {
+		contact *pb.Contact
+		errs    models.ValidationErrors
+	}{
+		{&pb.Contact{Name: "L", Email: " leopold.wentzel@gmail.com ", Phone: "555-867-5309"}, models.ValidationErrors{
+			{Field: "admin.name", Err: models.ErrTooShort.Error()},
+		}},
+		{&pb.Contact{Email: "not an email", Phone: " 555-867-5309 "}, models.ValidationErrors{
+			{Field: "admin.name", Err: models.ErrMissingField.Error()},
+			{Field: "admin.email", Err: models.ErrInvalidEmail.Error()},
+		}},
+		{&pb.Contact{Phone: "555-867-5309"}, models.ValidationErrors{
+			{Field: "admin.name", Err: models.ErrMissingField.Error()},
+			{Field: "admin.email", Err: models.ErrMissingField.Error()},
+		}},
+		{&pb.Contact{}, models.ValidationErrors{
+			{Field: "admin.name", Err: models.ErrMissingField.Error()},
+			{Field: "admin.email", Err: models.ErrMissingField.Error()},
+			{Field: "admin.phone", Err: models.ErrMissingField.Error()},
+		}},
+		{&pb.Contact{Name: "Leopold Wentzel", Email: "leopold.wentzel@gmail.com", Phone: "555-867-5309"}, nil},
+	}
+
+	for _, tc := range testCases {
+		errs := models.ValidateContact(tc.contact, "admin")
 		require.Equal(t, tc.errs, errs)
 	}
 }
