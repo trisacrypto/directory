be89cb681cd3f1caaecf12d1b1d127cda4d25891
Cletus Razakou <masskoder@gmail.com>
2023-05-26 23:06:57 -0400

    fix: update copy

--- web/gds-user-ui/src/modules/dashboard/certificate/registration.tsx
+++ web/gds-user-ui/src/modules/dashboard/certificate/registration.tsx
@@ -10,7 +10,7 @@ import { useSelector } from 'react-redux';
 
 import HomeButton from 'components/ui/HomeButton';
 
-import { Trans } from '@lingui/react';
+import { Trans } from '@lingui/macro';
 
 const Certificate: React.FC = () => {
   const textColor = useColorModeValue('black', '#EDF2F7');
@@ -31,25 +31,28 @@ const Certificate: React.FC = () => {
           <Card maxW="100%" bg={backgroundColor} color={textColor}>
             <Card.Body>
               <Text>
-                <Trans id="This multi-section form is an important step in the registration and certificate issuance process. The information you provide will be used to verify the legal entity that you represent and, where appropriate, will be available to verified TRISA members to facilitate compliance decisions. If you need guidance, see the">
+                <Trans>
                   This multi-section form is an important step in the registration and certificate
                   issuance process. The information you provide will be used to verify the legal
                   entity that you represent and, where appropriate, will be available to verified
-                  TRISA members to facilitate compliance decisions. If you need guidance, see the
-                </Trans>{' '}
-                <Link isExternal href="/getting-started" color={'link'} fontWeight={'bold'}>
-                  <Trans id="Getting Started Help Guide">Getting Started Help Guide</Trans>.{' '}
-                </Link>
+                  TRISA members to facilitate compliance decisions. If you need guidance, see the{' '}
+                  <Link isExternal href="/getting-started" color={'link'} fontWeight={'bold'}>
+                    <Trans>Getting Started</Trans>.{' '}
+                  </Link>
+                  Help Guide.
+                </Trans>
               </Text>
               <Text pt={4}>
-                <Trans id="To assist in completing the registration form, the form is divided into multiple sections">
+                <Trans>
                   To assist in completing the registration form, the form is divided into multiple
-                  sections
+                  sections. Navigate through the form by clicking on each section of the progress
+                  bar or the buttons at the bottom of each section.
                 </Trans>
                 .{' '}
-                <Text as={'span'} fontWeight={'bold'}>
-                  <Trans id="No information is sent until you complete Section 6 - Review & Submit">
-                    No information is sent until you complete Section 6 - Review & Submit
+                <Text fontWeight={'bold'} pt={4}>
+                  <Trans>
+                    No information is sent until you submit for a TestNet and/ or MainNet
+                    Certificate after the Review Section.
                   </Trans>
                   .{' '}
                 </Text>
