d82e43672bfacdd5951aa07c58b867a46f4ddab7
Cletus Razakou <masskoder@gmail.com>
2023-07-03 13:10:34 -0400

    clean up

--- web/gds-user-ui/src/modules/dashboard/member/index.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/index.tsx
@@ -1,12 +1,14 @@
 import { Suspense } from 'react';
 import { Heading } from '@chakra-ui/react';
 import MemberTable from './MemberTable';
-
+import { Trans } from '@lingui/macro';
 import Loader from 'components/Loader';
 const MemberPage: React.FC = () => {
   return (
     <>
-      <Heading marginBottom="69px">TRISA Member Directory</Heading>
+      <Heading marginBottom="69px">
+        <Trans>TRISA Member Directory</Trans>
+      </Heading>
 
       <Suspense fallback={<Loader />}>
         <MemberTable />
