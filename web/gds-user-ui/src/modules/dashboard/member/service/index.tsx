d4d069c2b6557f0ff227810f4a22d9802b79dc3e
Cletus Razakou <masskoder@gmail.com>
2023-07-04 14:30:44 -0400

    fix: typo

--- web/gds-user-ui/src/modules/dashboard/member/service/index.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/service/index.tsx
@@ -1,13 +1,13 @@
 import axiosInstance from 'utils/axios';
 import { DirectoryType } from '../memberType';
 
-const getDirectoryUrl = (dir: DirectoryType) => {
+const getVaspDirectory = (dir: DirectoryType) => {
   return dir === 'mainnet' ? 'vaspdirectory.net' : 'trisatest.net';
 };
 
 export const getMembersService = async (directory = 'mainnet') => {
   const response = await axiosInstance.get(
-    `/members?registered_directory=${getDirectoryUrl(directory as DirectoryType)}`
+    `/members?registered_directory=${getVaspDirectory(directory as DirectoryType)}`
   );
   return response;
 };
