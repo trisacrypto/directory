7d6a58ff5c00171109e14155f65a432aa6ca2199
Cletus Razakou <masskoder@gmail.com>
2023-07-04 18:03:46 -0400

    refact: improve tthe component

--- web/gds-user-ui/src/modules/dashboard/member/MemberTable.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/MemberTable.tsx
@@ -2,82 +2,22 @@ import {
   Table,
   TableCaption,
   Tbody,
-  Td,
   Th,
   Thead,
   Heading,
   Tr,
   Button,
   HStack,
-  chakra,
   useColorModeValue
 } from '@chakra-ui/react';
-import UnverifiedMember from './UnverifiedMember';
 import FormLayout from 'layouts/FormLayout';
 
 import React from 'react';
 import { Trans } from '@lingui/macro';
-import { BsEye } from 'react-icons/bs';
-
+import { MemberTableRows } from './Components/MemberTableRows';
 interface MemberTableProps {
   data: any;
 }
-
-interface TableRowsProps {
-  rows: any;
-}
-
-interface TableRowProps {
-  row: any;
-}
-
-const TableRow: React.FC<TableRowProps> = (row: any) => {
-  return (
-    <Tr>
-      {row.length > 0 ? (
-        <>
-          <Td>
-            <chakra.span display="block"></chakra.span>
-            <chakra.span display="block" fontSize="sm" color="gray.700"></chakra.span>
-          </Td>
-          <Td></Td>
-          <Td></Td>
-          <Td></Td>
-          <Td></Td>
-          <Td paddingY={0}>
-            <HStack width="100%" justifyContent="center" alignItems="center">
-              <Button
-                color="blue"
-                as={'a'}
-                href={``}
-                bg={'transparent'}
-                _hover={{
-                  bg: 'transparent'
-                }}
-                _focus={{
-                  bg: 'transparent'
-                }}>
-                <BsEye fontSize="24px" />
-              </Button>
-            </HStack>
-          </Td>
-        </>
-      ) : (
-        <Td colSpan={6}>
-          <UnverifiedMember />
-        </Td>
-      )}
-    </Tr>
-  );
-};
-
-const TableRows: React.FC<TableRowsProps> = (data) => {
-  return (
-    <>
-      <TableRow row={data} />
-    </>
-  );
-};
 
 const MemberTable: React.FC<MemberTableProps> = (data) => {
   console.log('data', data);
@@ -128,7 +68,7 @@ const MemberTable: React.FC<MemberTableProps> = (data) => {
           </Tr>
         </Thead>
         <Tbody>
-          <TableRows rows={data} />
+          <MemberTableRows rows={data} />
         </Tbody>
       </Table>
     </FormLayout>
