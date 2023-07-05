cd51f7bf5e778c6ceadb81643dc38963678b0347
Cletus Razakou <masskoder@gmail.com>
2023-07-05 16:38:44 -0400

    fix conflict

--- web/gds-user-ui/src/modules/dashboard/member/components/MemberTableHeader.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/components/MemberTableHeader.tsx
@@ -1,25 +1,27 @@
-import { Heading, HStack, useColorModeValue, Button } from '@chakra-ui/react';
+import { Heading, HStack, useColorModeValue, Button, Stack } from '@chakra-ui/react';
 import { Trans } from '@lingui/macro';
 const MemberTableHeader = () => {
   const exportHandler = () => {
     console.log('modalHandler');
   };
   return (
-    <HStack justify={'space-between'} mb={'10'}>
-      <Heading size="md" color={'black'}>
-        <Trans>Member List</Trans>
-      </Heading>
-      <Button
-        minW="100px"
-        onClick={exportHandler}
-        bg={useColorModeValue('black', 'white')}
-        _hover={{
-          bg: useColorModeValue('black', 'white')
-        }}
-        color={useColorModeValue('white', 'black')}>
-        <Trans>Export</Trans>
-      </Button>
-    </HStack>
+    <Stack width={'100%'}>
+      <HStack justify={'space-between'} mb={'10'}>
+        <Heading size="md" color={'black'}>
+          <Trans>Member List</Trans>
+        </Heading>
+        <Button
+          minW="100px"
+          onClick={exportHandler}
+          bg={useColorModeValue('black', 'white')}
+          _hover={{
+            bg: useColorModeValue('black', 'white')
+          }}
+          color={useColorModeValue('white', 'black')}>
+          <Trans>Export</Trans>
+        </Button>
+      </HStack>
+    </Stack>
   );
 };
 
