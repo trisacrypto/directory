3477b1dba590141a64f0a22381bc700cfbebc614
daniellemaxwell <danielle@rotational.io>
2023-07-05 10:16:25 -0400

    Adds notification message to member page

--- web/gds-user-ui/src/modules/dashboard/member/index.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/index.tsx
@@ -1,8 +1,9 @@
-import { Heading } from '@chakra-ui/react';
+import { Heading, Text } from '@chakra-ui/react';
 import MemberTable from './MemberTable';
 import { Trans } from '@lingui/macro';
 import Loader from 'components/Loader';
 import { useFetchMembers } from './hook/useFetchMembers';
+import Card from 'components/ui/Card';
 
 const MemberPage: React.FC = () => {
   const { members, isFetchingMembers, error } = useFetchMembers();
@@ -14,6 +15,15 @@ const MemberPage: React.FC = () => {
       <Heading marginBottom="69px">
         <Trans>TRISA Member Directory</Trans>
       </Heading>
+      <Card maxW="100%" marginBottom={6}>
+        <Card.Body>
+          <Text>
+            <Trans>
+              The TRISA Member Directory is for informational purposes and is meant to foster collaboration between members. It is available to verified TRISA contacts only. If you cannot view the list, please complete the registration process. You can view the member list after verification is complete.
+            </Trans>
+          </Text>
+        </Card.Body>
+      </Card>
 
       {error && <p>error </p>}
       {members && <MemberTable data={members} />}
