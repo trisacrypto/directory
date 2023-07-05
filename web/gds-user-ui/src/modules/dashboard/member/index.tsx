c0c5acc834ca21c910ec1b4cf92327018cebee9b
Cletus Razakou <masskoder@gmail.com>
2023-07-05 16:51:52 -0400

    refact: add suspence to member page

--- web/gds-user-ui/src/modules/dashboard/member/index.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/index.tsx
@@ -1,7 +1,8 @@
 import { Heading } from '@chakra-ui/react';
-
+import { Suspense } from 'react';
 import MemberTable from './components/MemberTable';
 import { Trans } from '@lingui/macro';
+import Loader from 'components/Loader';
 import DirectoryNotification from './components/DirectoryNotification';
 import FormLayout from 'layouts/FormLayout';
 import MemberSelectNetwork from './components/memberNetworkSelect';
@@ -12,12 +13,14 @@ const MemberPage: React.FC = () => {
       <Heading marginBottom="69px">
         <Trans>TRISA Member Directory</Trans>
       </Heading>
-      <DirectoryNotification />
-      <FormLayout overflowX={'scroll'}>
-        <MemberTableHeader />
-        <MemberSelectNetwork />
-        <MemberTable />
-      </FormLayout>
+      <Suspense fallback={<Loader />}>
+        <DirectoryNotification />
+        <FormLayout overflowX={'scroll'}>
+          <MemberTableHeader />
+          <MemberSelectNetwork />
+          <MemberTable />
+        </FormLayout>
+      </Suspense>
     </>
   );
 };
