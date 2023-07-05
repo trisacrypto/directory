1e1c7a7761a668dabd2c757c9ae77414de03254f
daniellemaxwell <danielle@rotational.io>
2023-07-05 09:18:37 -0400

    Reverts changes to member page

--- web/gds-user-ui/src/modules/dashboard/member/index.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/index.tsx
@@ -3,24 +3,24 @@ import MemberTable from './MemberTable';
 import { Trans } from '@lingui/macro';
 import Loader from 'components/Loader';
 import { useFetchMembers } from './hook/useFetchMembers';
-import { mainnetMembersMockValue } from './__mocks__';
+// import { mainnetMembersMockValue } from './__mocks__';
 
 const MemberPage: React.FC = () => {
-  const { /* members,  */isFetchingMembers, /* error */ } = useFetchMembers();
+  const { members, isFetchingMembers, error } = useFetchMembers();
 
-  const vasps = mainnetMembersMockValue.vasps;
-  console.log('vasps', vasps);
+  // const vasps = mainnetMembersMockValue.vasps;
+  // console.log('vasps', vasps);
 
   if (isFetchingMembers) return <Loader />;
-  // console.log('members', members);
+  console.log('members', members);
   return (
     <>
       <Heading marginBottom="69px">
         <Trans>TRISA Member Directory</Trans>
       </Heading>
 
-      {/* {error && <p>error </p>} */}
-      {/* members && */ <MemberTable data={vasps} />}
+      {error && <p>error </p>}
+      { members && <MemberTable data={members} />}
     </>
   );
 };
