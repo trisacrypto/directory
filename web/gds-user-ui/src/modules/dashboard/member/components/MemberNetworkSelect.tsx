e98f1273df72dcdd965a50f869e2fd949448f7c0
Danielle <danielle@rotational.io>
2023-07-07 13:14:51 -0400

    Sc 18427/network logic (#1074)
    * Adds onchange to select
    
    * Adds spec
    
    * Sets default value
    
    ---------
    
    Co-authored-by: Cletus Razakou <masskoder@gmail.com>

--- web/gds-user-ui/src/modules/dashboard/member/components/MemberNetworkSelect.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/components/MemberNetworkSelect.tsx
@@ -1,21 +1,26 @@
 import { FormControl, FormLabel, Select } from '@chakra-ui/react';
 import { Trans } from '@lingui/macro';
-// import { Dispatch } from '@reduxjs/toolkit';
+import { Dispatch } from '@reduxjs/toolkit';
 import React from 'react';
-// import { setMemberNetwork } from '../member.slice';
+import { useDispatch } from 'react-redux';
+import { setMemberNetwork } from '../member.slice';
+import Store from 'application/store';
 
 const MemberSelectNetwork = () => {
-  // const dispatch: Dispatch = useDispatch();
-  //  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-  //    dispatch(setMemberNetwork(event.target.value));
-  //  };
+  const dispatch: Dispatch = useDispatch();
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  dispatch(setMemberNetwork(event.target.value));
+};
+
+// Set the default value of the network to the value in the store.
+const { network } = Store.getState().members;
 
   return (
     <FormControl>
       <FormLabel>
         <Trans>Select Network</Trans>
       </FormLabel>
-      <Select>
+      <Select defaultValue={network} onChange={handleChange} data-testid="select-network">
         <option value="mainnet">MainNet</option>
         <option value="testnet">TestNet</option>
       </Select>
