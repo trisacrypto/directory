9580669f1c6c3e5087f0ff2314deca579539dee4
daniellemaxwell <danielle@rotational.io>
2023-07-07 10:34:27 -0400

    Adds onchange to select

--- web/gds-user-ui/src/modules/dashboard/member/components/MemberNetworkSelect.tsx
+++ web/gds-user-ui/src/modules/dashboard/member/components/MemberNetworkSelect.tsx
@@ -1,21 +1,22 @@
 import { FormControl, FormLabel, Select } from '@chakra-ui/react';
 import { Trans } from '@lingui/macro';
-// import { Dispatch } from '@reduxjs/toolkit';
+import { Dispatch } from '@reduxjs/toolkit';
 import React from 'react';
-// import { setMemberNetwork } from '../member.slice';
+import { useDispatch } from 'react-redux';
+import { setMemberNetwork } from '../member.slice';
 
 const MemberSelectNetwork = () => {
-  // const dispatch: Dispatch = useDispatch();
-  //  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-  //    dispatch(setMemberNetwork(event.target.value));
-  //  };
+  const dispatch: Dispatch = useDispatch();
+  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  dispatch(setMemberNetwork(event.target.value));
+};
 
   return (
     <FormControl>
       <FormLabel>
         <Trans>Select Network</Trans>
       </FormLabel>
-      <Select>
+      <Select data-testid="select-network" onChange={handleChange}>
         <option value="mainnet">MainNet</option>
         <option value="testnet">TestNet</option>
       </Select>
