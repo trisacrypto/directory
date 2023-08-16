8d85914add0d710ae80f309215cefb93911731ac
Cletus Razakou <masskoder@gmail.com>
2023-08-16 11:44:03 -0400

    fix: pending registration

--- web/gds-user-ui/src/modules/dashboard/overview/service.ts
+++ web/gds-user-ui/src/modules/dashboard/overview/service.ts
@@ -1,6 +1,6 @@
 import axiosInstance from 'utils/axios';
 import { getCookie } from 'utils/cookies';
-export const getMetrics = async () => {
+export const getOverview = async () => {
   const response = await axiosInstance.get(`/overview`, {
     headers: {
       Authorization: `Bearer ${getCookie('access_token')}`
