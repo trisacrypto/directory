295db0b3e8c5a3c1bbd252be46bc9f73800f3d8a
Patrick Deziel <deziel.patrick@gmail.com>
2023-11-06 14:17:50 -0600

    Fix build

--- pkg/models/v1/generate.go
+++ pkg/models/v1/generate.go
@@ -1,3 +1,3 @@
 package models
 
-//go:generate protoc -I=$GOPATH/trisa/proto -I=$GOPATH/directory/proto --go_out=. --go_opt=module=github.com/trisacrypto/directory/pkg/models/v1 --go-grpc_out=. --go-grpc_opt=module=github.com/trisacrypto/directory/pkg/models/v1 gds/models/v1/models.proto
+//go:generate protoc -I=$GOPATH/src/github.com/trisacrypto/trisa/proto -I=$GOPATH/src/github.com/trisacrypto/directory/proto --go_out=. --go_opt=module=github.com/trisacrypto/directory/pkg/models/v1 --go-grpc_out=. --go-grpc_opt=module=github.com/trisacrypto/directory/pkg/models/v1 gds/models/v1/models.proto
