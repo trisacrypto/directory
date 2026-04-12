6cb3e8d0522d1bda08c95a4f217f6238740b46ef
Benjamin Bengfort <benjamin@rotational.io>
2026-04-12 14:08:21 -0500

    [FIX] Remove Sentry from UIs (#1186)

--- web/gds-admin-ui/src/sentry.js
+++ web/gds-admin-ui/src/sentry.js
@@ -1,49 +1,5 @@
-import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
-
-import config from './config';
-
-const defaultTracingOrigins = ['localhost', /^\//];
-
 const initSentry = () => {
-    // ensure environment variables app version and git revision are set
-    if (!config.appVersion) {
-        // eslint-disable-next-line no-console
-        console.log('App version is not set in environment variables');
-    }
-    if (!config.gitVersion) {
-        // eslint-disable-next-line no-console
-        console.log('Git revision is not set in environment variables');
-    }
-    console.log(`AppVersion: ${config.appVersion || ''} - GitRevision: ${config.gitVersion || ''}`); // eslint-disable-line no-console
-
-    if (process.env.REACT_APP_SENTRY_DSN) {
-        let tracingOrigins = defaultTracingOrigins;
-        if (process.env.REACT_APP_GDS_API_ENDPOINT) {
-            const origin = new URL(process.env.REACT_APP_GDS_API_ENDPOINT);
-            tracingOrigins = [origin.host];
-        }
-
-        const environment = process.env.REACT_APP_SENTRY_ENVIRONMENT
-            ? process.env.REACT_APP_SENTRY_ENVIRONMENT
-            : process.env.NODE_ENV;
-
-        Sentry.init({
-            dsn: process.env.REACT_APP_SENTRY_DSN,
-            integrations: [new BrowserTracing({ tracingOrigins })],
-            environment,
-            tracesSampleRate: 1.0,
-            release: config?.appVersion || '',
-            // mute session expired errors
-            ignoreErrors: ['Session expired'],
-        });
-
-        // eslint-disable-next-line no-console
-        console.log('Sentry tracing initialized');
-    } else {
-        // eslint-disable-next-line no-console
-        console.log('no Sentry configuration available');
-    }
+  console.log('sentry is disabled in the GDS');
 };
 
 export default initSentry;
--- web/gds-user-ui/src/application/config/sentry.ts
+++ web/gds-user-ui/src/application/config/sentry.ts
@@ -1,43 +1,5 @@
-import * as Sentry from '@sentry/react';
-import { BrowserTracing } from '@sentry/tracing';
-import { getAppVersionNumber } from '.';
-
-const defaultTracingOrigins = ['localhost', /^\//];
-const isProdEnv = process.env.NODE_ENV !== 'development';
 const initSentry = () => {
-  if (process.env.REACT_APP_SENTRY_DSN && isProdEnv) {
-    let tracingOrigins = defaultTracingOrigins;
-    if (process.env.REACT_APP_TRISA_BASE_URL) {
-      const origin = new URL(process.env.REACT_APP_TRISA_BASE_URL);
-      tracingOrigins = [origin.host];
-    }
-
-    const environment = process.env.REACT_APP_SENTRY_ENVIRONMENT
-      ? process.env.REACT_APP_SENTRY_ENVIRONMENT
-      : process.env.NODE_ENV;
-
-    Sentry.init({
-      dsn: process.env.REACT_APP_SENTRY_DSN,
-      environment,
-      integrations: [
-        new BrowserTracing({
-          tracingOrigins
-        })
-      ],
-
-      // Set tracesSampleRate to 1.0 to capture 100%
-      // of transactions for performance monitoring.
-      // We recommend adjusting this value in production
-      tracesSampleRate: 1.0,
-      release: getAppVersionNumber()
-    });
-
-    // eslint-disable-next-line no-console
-    console.log('Sentry tracing initialized');
-  } else {
-    // eslint-disable-next-line no-console
-    console.log('no Sentry configuration available');
-  }
+  console.log('sentry is disabled in the GDS');
 };
 
 export default initSentry;
